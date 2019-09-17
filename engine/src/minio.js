34e4a6359074cc0ae7c291511bdf03a8f499f517
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-09-17 19:46:15 +0200

    Parse minio config values which arent strings

--- engine/src/minio.js
+++ engine/src/minio.js
@@ -4,8 +4,8 @@ const { get } = require('./config');
 
 module.exports = new Minio.Client({
   endPoint: get('s3.endpoint'),
-  useSSL: get('s3.tls_enabled'),
-  port: get('s3.port'),
+  useSSL: get('s3.tls_enabled') === 'true',
+  port: parseInt(get('s3.port'), 10),
   accessKey: get('s3.access_key'),
   secretKey: get('s3.secret_key'),
 });
