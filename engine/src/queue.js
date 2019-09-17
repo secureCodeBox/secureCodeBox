9e4954ff005bbd629d56389bf1343ae318593e44
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-09-17 19:46:33 +0200

    Store the actual scan objects outside the redis list

--- engine/src/queue.js
+++ engine/src/queue.js
@@ -2,16 +2,22 @@ const redis = require('./redis');
 const uuid = require('uuid/v4');
 
 async function lookForJob(jobType, tenant) {
-  const res = await redis.lpop(`${tenant}:${jobType}`);
+  const scanId = await redis.lpop(`${tenant}:${jobType}`);
 
-  return JSON.parse(res);
+  if (scanId) {
+    const scanJob = await redis.get(`job:${scanId}`);
+    return JSON.parse(scanJob);
+  }
+
+  return null;
 }
 module.exports.lookForJob = lookForJob;
 
 async function createScanJob(jobType, tenant, parameters) {
   const id = uuid();
-  await redis.lpush(
-    `${tenant}:${jobType}`,
+  await redis.lpush(`${tenant}:${jobType}`, id);
+  await redis.set(
+    `job:${id}`,
     JSON.stringify({ id, parameters, createdAt: new Date() })
   );
 
