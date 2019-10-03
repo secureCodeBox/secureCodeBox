0fb6d8ef03ed7cf5a8c60def06aa539f57154227
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-10-03 19:23:30 +0200

    Initial version of the ssh_scan parser

---
+++ integrations/ssh_scan/parser/src/parser.js
@@ -0,0 +1,50 @@
+const uuid = require('uuid/v4');
+const get = require('lodash.get');
+
+async function parse(fileContent) {
+  const hosts = fileContent;
+
+  return hosts
+    .flatMap(host => {
+      if (host.error) {
+        return undefined;
+      }
+
+      const location = get(host, ['hostname']) || get(host, ['ip']);
+
+      return {
+        id: uuid(),
+        name: 'SSH Compliance',
+        description: 'SSH Compliance Information',
+        category: 'SSH Service',
+        osi_layer: 'APPLICATION',
+        severity: 'INFORMATIONAL',
+        reference: {},
+        hint: '',
+        location: location,
+        attributes: {
+          hostname: get(host, ['hostname']),
+          server_banner: get(host, ['server_banner']) || null,
+          ssh_version: get(host, ['ssh_version']),
+          os_cpe: get(host, ['os_cpe']),
+          ssh_lib_cpe: get(host, ['ssh_lib_cpe']),
+          compliance_policy: get(host, ['compliance', 'policy']),
+          compliant: get(host, ['compliance', 'compliant']),
+          grade: get(host, ['compliance', 'grade']),
+          references: get(host, ['compliance', 'references']),
+          auth_methods: get(host, ['auth_methods']),
+          key_algorithms: get(host, ['key_algorithms']),
+          encryption_algorithms: get(host, [
+            'encryption_algorithms_server_to_client',
+          ]),
+          mac_algorithms: get(host, ['mac_algorithms_server_to_client']),
+          compression_algorithms: get(host, [
+            'compression_algorithms_server_to_client',
+          ]),
+        },
+      };
+    })
+    .filter(Boolean);
+}
+
+module.exports.parse = parse;
---
+++ integrations/ssh_scan/parser/src/parser.test.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+const { parse } = require('./parser');
+
+test('ssh-scan parser parses errored result (no ssh server) to zero findings', async () => {
+  const hosts = JSON.parse(await readFile(
+    __dirname + '/__testFiles__/localhost.json',
+    {
+      encoding: 'utf8',
+    }
+  ));
+
+  const res = await parse(hosts);
+
+  expect(res).toEqual([]);
+});
+
+test('ssh-scan parser parses a proper result to proper findings', async () => {
+  const hosts = JSON.parse(await readFile(
+    __dirname + '/__testFiles__/securecodebox.io.json',
+    {
+      encoding: 'utf8',
+    }
+  ));
+
+  const res = await parse(hosts);
+
+  expect(res).toEqual([
+    {
+      attributes: {
+        auth_methods: ['publickey'],
+        compliance_policy: 'Mozilla Modern',
+        compliant: false,
+        compression_algorithms: ['none', 'zlib@openssh.com'],
+        encryption_algorithms: [
+          'chacha20-poly1305@openssh.com',
+          'aes128-ctr',
+          'aes192-ctr',
+          'aes256-ctr',
+          'aes128-gcm@openssh.com',
+          'aes256-gcm@openssh.com',
+        ],
+        grade: 'C',
+        hostname: 'securecodebox.io',
+        key_algorithms: [
+          'curve25519-sha256@libssh.org',
+          'ecdh-sha2-nistp256',
+          'ecdh-sha2-nistp384',
+          'ecdh-sha2-nistp521',
+          'diffie-hellman-group-exchange-sha256',
+          'diffie-hellman-group14-sha1',
+        ],
+        mac_algorithms: [
+          'umac-64-etm@openssh.com',
+          'umac-128-etm@openssh.com',
+          'hmac-sha2-256-etm@openssh.com',
+          'hmac-sha2-512-etm@openssh.com',
+          'hmac-sha1-etm@openssh.com',
+          'umac-64@openssh.com',
+          'umac-128@openssh.com',
+          'hmac-sha2-256',
+          'hmac-sha2-512',
+          'hmac-sha1',
+        ],
+        os_cpe: 'o:canonical:ubuntu:16.04',
+        references: ['https://wiki.mozilla.org/Security/Guidelines/OpenSSH'],
+        server_banner: 'SSH-2.0-OpenSSH_7.2p2 Ubuntu-4ubuntu2.4',
+        ssh_lib_cpe: 'a:openssh:openssh:7.2p2',
+        ssh_version: 2,
+      },
+      category: 'SSH Service',
+      description: 'SSH Compliance Information',
+      hint: '',
+      id: '49bf7fd3-8512-4d73-a28f-608e493cd726',
+      location: 'securecodebox.io',
+      name: 'SSH Compliance',
+      osi_layer: 'APPLICATION',
+      reference: {},
+      severity: 'INFORMATIONAL',
+    },
+  ]);
+});
