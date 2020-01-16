31dc04c37e5ca85cd6c1446f14ac8a2915ba7720
J12934 <13718901+J12934@users.noreply.github.com>
2020-01-16 16:34:01 +0100

    Correct expected findings

--- test/wpscan.test.js
+++ test/wpscan.test.js
@@ -1,86 +1,38 @@
 const { startSecurityTest, Time } = require('./sdk');
 
 test(
-    'finds a few informational severity findigns for www.iteraplan.de',
-    async () => {
-        const securityTest = await startSecurityTest({
-            context: 'iteraplan wp',
-            metaData: {},
-            name: 'wordpress',
-            target: {
-                name: 'iteraplan',
-                location: 'www.iteraplan.de',
-                attributes: {},
-            },
-        });
+  'finds a few findings findings for an older wordpress installation',
+  async () => {
+    const securityTest = await startSecurityTest({
+      context: 'old wordpress',
+      metaData: {},
+      name: 'wordpress',
+      target: {
+        name: 'old-wordpress',
+        location: 'old-wordpress',
+        attributes: {},
+      },
+    });
 
-        const { report } = securityTest;
+    const { report } = securityTest;
 
-        const findings = report.findings.map(
-            ({ description, category, name, osi_layer, severity }) => ({
-                description,
-                category,
-                name,
-                osi_layer,
-                severity,
-            })
-        );
+    const [finding, ...otherFindings] = report.findings;
 
-        expect(findings).toContainEqual({
-            description: 'CMS Wordpress Information',
-            category: 'CMS Wordpress',
-            name: 'CMS Wordpress',
-            osi_layer: 'APPLICATION',
-            severity: 'INFORMATIONAL',
-        });
+    console.log(report.findings);
 
-        expect(findings).toContainEqual({
-            description: ' Username & Hash Extract',
-            category: 'Plugin',
-            name: 'W3 Total Cache 0.9.2.4 ',
-            osi_layer: 'APPLICATION',
-            severity: 'INFORMATIONAL',
-        });
+    expect(finding).toMatchObject({
+      name: 'CMS Wordpress',
+      category: 'CMS Wordpress',
+      description: 'CMS Wordpress Information',
+      location: 'http://old-wordpress/',
+      severity: 'INFORMATIONAL',
+      osi_layer: 'APPLICATION',
+      attributes: {
+        version: '4.0.29',
+      },
+    });
 
-        expect(findings).toContainEqual({
-            description: ' Remote Code Execution',
-            category: 'Plugin',
-            name: 'W3 Total Cache ',
-            osi_layer: 'APPLICATION',
-            severity: 'INFORMATIONAL',
-        });
-
-        expect(findings).toContainEqual({
-            description: ' Edge Mode Enabling CSRF',
-            category: 'Plugin',
-            name: 'W3 Total Cache 0.9.4 ',
-            osi_layer: 'APPLICATION',
-            severity: 'INFORMATIONAL',
-        });
-
-        expect(findings).toContainEqual({
-            description: ' Cross',
-            category: 'Plugin',
-            name: 'W3 Total Cache <= 0.9.4 ',
-            osi_layer: 'APPLICATION',
-            severity: 'INFORMATIONAL',
-        });
-
-        expect(findings).toContainEqual({
-            description: ' Debug Mode XSS',
-            category: 'Plugin',
-            name: 'W3 Total Cache <= 0.9.4 ',
-            osi_layer: 'APPLICATION',
-            severity: 'INFORMATIONAL',
-        });
-
-        expect(findings).toContainEqual({
-            description: ' Authenticated Reflected Cross',
-            category: 'Plugin',
-            name: 'W3 Total Cache <= 0.9.4.1 ',
-            osi_layer: 'APPLICATION',
-            severity: 'INFORMATIONAL',
-        });
-    },
-    7 * Time.Minute
+    expect(otherFindings).toEqual([]);
+  },
+  7 * Time.Minute
 );
