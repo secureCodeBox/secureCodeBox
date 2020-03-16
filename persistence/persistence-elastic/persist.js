6e4fb0bff3747a4e766f171de56bf3c556357498
J12934 <13718901+J12934@users.noreply.github.com>
2020-03-16 19:39:26 +0100

    Remove unused and complicated error handling code

--- persistence/persistence-elastic/persist.js
+++ persistence/persistence-elastic/persist.js
@@ -54,25 +54,8 @@ async function persist({ getFindings, scan, now = new Date() }) {
     const { body: bulkResponse } = await client.bulk({ refresh: true, body });
 
     if (bulkResponse.errors) {
-      const erroredDocuments = [];
-      // The items array has the same order of the dataset we just indexed.
-      // The presence of the `error` key indicates that the operation
-      // that we did for the document has failed.
-      bulkResponse.items.forEach((action, i) => {
-        const operation = Object.keys(action)[0];
-        if (action[operation].error) {
-          erroredDocuments.push({
-            // If the status is 429 it means that you can retry the document,
-            // otherwise it's very likely a mapping error, and you should
-            // fix the document before to try it again.
-            status: action[operation].status,
-            error: action[operation].error,
-            operation: body[i * 2],
-            document: body[i * 2 + 1],
-          });
-        }
-      });
-      console.log(erroredDocuments);
+      console.error('Bulk Request had errors:');
+      console.log(bulkResponse);
     }
   }
 }
