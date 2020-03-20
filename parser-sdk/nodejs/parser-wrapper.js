ad2eb166d3238cc95b454897ecec57de8ff68a69
J12934 <13718901+J12934@users.noreply.github.com>
2020-03-20 20:27:27 +0100

    Let the parser automatically set the findings counts in the scans status

--- parser-sdk/nodejs/parser-wrapper.js
+++ parser-sdk/nodejs/parser-wrapper.js
@@ -1,6 +1,73 @@
 const axios = require('axios');
 const { parse } = require('./parser/parser');
 const uuid = require('uuid/v4');
+const k8s = require('@kubernetes/client-node');
+
+function severityCount(findings, severity) {
+  return findings.filter(
+    ({ severity: findingSeverity }) =>
+      findingSeverity.toUpperCase() === severity
+  ).length;
+}
+
+async function updateScanStatus(findings) {
+  const kc = new k8s.KubeConfig();
+  kc.loadFromCluster();
+  const k8sApi = kc.makeApiClient(k8s.CustomObjectsApi);
+  const scanName = process.env['SCAN_NAME'];
+  const namespace = process.env['NAMESPACE'];
+
+  try {
+    const findingCategories = new Map();
+    for (const { category } of findings) {
+      if (findingCategories.has(category)) {
+        findingCategories.set(category, findingCategories.get(category) + 1);
+      } else {
+        findingCategories.set(category, 1);
+      }
+    }
+    console.log(findingCategories);
+    console.log({
+      status: {
+        findingCount: findings.length,
+        findingSeverities: {
+          informationalCount: severityCount(findings, 'INFORMATIONAL'),
+          lowCount: severityCount(findings, 'LOW'),
+          mediumCount: severityCount(findings, 'MEDIUM'),
+          highCount: severityCount(findings, 'HIGH'),
+        },
+        findingCategories: Object.fromEntries(findingCategories.entries()),
+      },
+    });
+
+    await k8sApi.patchNamespacedCustomObjectStatus(
+      'scans.experimental.securecodebox.io',
+      'v1',
+      namespace,
+      'scans',
+      scanName,
+      {
+        status: {
+          findingCount: findings.length,
+          findingSeverities: {
+            informationalCount: severityCount(findings, 'INFORMATIONAL'),
+            lowCount: severityCount(findings, 'LOW'),
+            mediumCount: severityCount(findings, 'MEDIUM'),
+            highCount: severityCount(findings, 'HIGH'),
+          },
+          findingCategories: Object.fromEntries(findingCategories.entries()),
+        },
+      },
+      { headers: { 'content-type': 'application/merge-patch+json' } }
+    );
+    console.log('Updated status successfully');
+    // console.log(res);
+  } catch (err) {
+    console.error('Failed to update Scan Status via the kubernetes api');
+    console.error(err);
+    process.exit(1);
+  }
+}
 
 async function main() {
   const resultFileUrl = process.argv[2];
@@ -9,7 +76,7 @@ async function main() {
   const { data } = await axios.get(resultFileUrl);
 
   const findings = await parse(data);
-  console.log(`Transformed raw result file into ${findings.length} findings.`);
+  console.log(`Transformed raw result file into ${findings.length} findings`);
 
   console.log('Adding UUIDs to the findings');
   const findingsWithIds = findings.map(finding => {
@@ -19,11 +86,13 @@ async function main() {
     };
   });
 
-  console.log(`Submitting results to the engine.`);
+  await updateScanStatus(findings);
+
+  console.log(`Uploading results to the file storage service`);
 
   await axios.put(resultUploadUrl, findingsWithIds);
 
-  console.log(`Completed parser.`);
+  console.log(`Completed parser`);
 }
 
 main();
