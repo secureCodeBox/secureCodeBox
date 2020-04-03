4a47e72a54cd26edb31a174a501398672fe05d90
Jannik Hollenbach <13718901+J12934@users.noreply.github.com>
2020-04-03 17:20:14 +0200

    Add proper(ish) kube-hunter parser

--- integrations/kube-hunter/parser/parser.js
+++ integrations/kube-hunter/parser/parser.js
@@ -1,5 +1,39 @@
-async function parse() {
-  return [];
+async function parse({ vulnerabilities = [], nodes = [] }) {
+  return vulnerabilities.map(vulnerability => {
+    const reference = {}
+
+    if ( vulnerability.vid !== "None") {
+      reference.id = vulnerability.vid
+      reference.source = `https://aquasecurity.github.io/kube-hunter/kb/${vulnerability.vid}`
+    }
+
+    let location = vulnerability.location;
+    if (location.startsWith('Local to Pod')) {
+      // This is a pod specific vulnarability.
+      // As this does not fit the secureCodeBox model to well we will scope this to the first "Node/Master" type node of the cluster.
+      // This is subject to change.
+
+      for (const node of nodes) {
+        if (node.type === "Node/Master") {
+          location = node.location
+          break;
+        }
+      }
+    }
+
+    return {
+      name: vulnerability.vulnerability,
+      description: vulnerability.description,
+      location,
+      severity: vulnerability.severity.toUpperCase(),
+      category: vulnerability.category,
+      reference,
+      attributes: {
+        evidence: vulnerability.evidence,
+        kubeHunterRule: vulnerability.hunter,
+      }
+    };
+  });
 }
 
 module.exports.parse = parse;
--- integrations/kube-hunter/parser/parser.test.js
+++ integrations/kube-hunter/parser/parser.test.js
@@ -1,3 +1,17 @@
-test('passes', async () => {
-  expect(true).toBe(true);
+const fs = require('fs');
+const util = require('util');
+
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+const readFile = util.promisify(fs.readFile);
+
+const { parse } = require('./parser');
+
+test('parses result from kind-1.18-in-cluster-scan correctly', async () => {
+  const fileContent = JSON.parse(
+    await readFile(__dirname + '/__testFiles__/kind-1.18-in-cluster-scan.json', {
+      encoding: 'utf8',
+    })
+  );
+
+  expect(await parse(fileContent)).toMatchSnapshot();
 });
