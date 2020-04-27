439f067e91f706d3f976ac287e3ff74f388bcb11
Jannik Hollenbach <13718901+J12934@users.noreply.github.com>
2020-04-27 12:16:23 +0200

    Autoformat

--- test/nikto.test.js
+++ test/nikto.test.js
@@ -1,15 +1,15 @@
-const { startSecurityTest, Time } = require('./sdk');
+const { startSecurityTest, Time } = require("./sdk");
 
 test(
-  'finds open 3000 port of juiceshop',
+  "finds open 3000 port of juiceshop",
   async () => {
     const securityTest = await startSecurityTest({
-      context: 'JuiceShopPortScan',
+      context: "JuiceShopPortScan",
       metaData: {},
-      name: 'nikto',
+      name: "nikto",
       target: {
-        name: 'JuiceShop Container',
-        location: 'juice-shop',
+        name: "JuiceShop Container",
+        location: "juice-shop",
         attributes: {
           NIKTO_PORTS: "3000",
           COMBINED_NMAP_NIKTO_PORTS: "80, 443, 3000, 8080, 8443",
@@ -19,30 +19,34 @@ test(
 
     const { report } = securityTest;
 
-    const [finding1, finding2, finding3, ...otherFindings] = report.findings.map(
-      ({ name, osi_layer, severity }) => ({
-        name,
-        osi_layer,
-        severity,
-      })
-    );
+    const [
+      finding1,
+      finding2,
+      finding3,
+      ...otherFindings
+    ] = report.findings.map(({ name, osi_layer, severity }) => ({
+      name,
+      osi_layer,
+      severity,
+    }));
 
     expect(finding1).toMatchObject({
-      name: 'Retrieved x-powered-by header: Express',
-      osi_layer: 'APPLICATION',
-      severity: 'INFORMATIONAL',
+      name: "Retrieved x-powered-by header: Express",
+      osi_layer: "APPLICATION",
+      severity: "INFORMATIONAL",
     });
 
     expect(finding2).toMatchObject({
-      name: 'Retrieved access-control-allow-origin header: *',                                                                                               
-      osi_layer: 'APPLICATION',                                                                                                                              
-      severity: 'INFORMATIONAL'                  
+      name: "Retrieved access-control-allow-origin header: *",
+      osi_layer: "APPLICATION",
+      severity: "INFORMATIONAL",
     });
 
     expect(finding3).toMatchObject({
-      name: 'The X-XSS-Protection header is not defined. This header can hint to the user agent to protect against some forms of XSS',
-      osi_layer: 'APPLICATION',
-      severity: 'INFORMATIONAL'
+      name:
+        "The X-XSS-Protection header is not defined. This header can hint to the user agent to protect against some forms of XSS",
+      osi_layer: "APPLICATION",
+      severity: "INFORMATIONAL",
     });
   },
   5 * Time.Minute
