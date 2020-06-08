0099dc3ecadcaedbdb33e3afcecbc92e9e090b35
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-06-08 12:50:07 +0200

    Add dummy scanner for hook integration tests
    To perform integration tests to ensure the functionality of the
    read-only and read-write hooks we need to have a dummy scanner with no
    dependencies to a real scanner or parser.

---
+++ integrations/read-write-hook-test/parser/parser.js
@@ -0,0 +1,46 @@
+async function parse(fileContent) {
+  return transformToFindings();
+}
+
+function transformToFindings() {
+  const portFindings = {
+    name: "Test read-write-hook service",
+    description: `Port is using protocol.`,
+    category: 'Open Port',
+    location: `tcp://rw-hook-test:80`,
+    osi_layer: 'NETWORK',
+    severity: 'INFORMATIONAL',
+    attributes: {
+      port: 80,
+      state: "Open",
+      ip_address: "host ip address",
+      mac_address: "hostInfo.mac",
+      protocol: "openPort.protocol",
+      hostname: "hostInfo.hostname",
+      method: "openPort.method",
+      operating_system: "hostInfo.osNmap",
+      service: "openPort.service",
+      serviceProduct: "openPort.serviceProduct",
+      serviceVersion: "openPort.serviceVersion",
+      scripts: "openPort.scriptOutputs",
+    },
+  };
+
+  const hostFindings = {
+    name: `Host: hostname`,
+    category: 'Host',
+    description: 'Found a host',
+    location: "hostname",
+    severity: 'INFORMATIONAL',
+    osi_layer: 'NETWORK',
+    attributes: {
+      ip_address: "ip address",
+      hostname: "hostname",
+      operating_system: "osNmap",
+    },
+  };
+
+  return [...portFindings, ...hostFindings];
+}
+
+module.exports.parse = parse;
