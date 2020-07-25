1dfc86bc62fd42823a432946eb43d3a946c5b9a1
Jannik Hollenbach <13718901+J12934@users.noreply.github.com>
2020-07-25 11:29:08 +0200

    Truncate overly long "otherinfo" fields from ZAP
    These can otherwise mess up elasticsearch indexes as the fields can contain complete js / css files up to a couple of megs in size

--- scanners/zap/parser/parser.js
+++ scanners/zap/parser/parser.js
@@ -1,43 +1,54 @@
 function riskToSeverity(risk) {
   switch (parseInt(risk, 10)) {
     case 0:
-      return 'INFORMATIONAL';
+      return "INFORMATIONAL";
     case 1:
-      return 'LOW';
+      return "LOW";
     case 2:
-      return 'MEDIUM';
+      return "MEDIUM";
     default:
-      return 'HIGH';
+      return "HIGH";
   }
 }
 
-function stripHtmlTags(str)
-{
-   if ((!str) || ( str === null ) || ( str === '' ))
-       return false;
-  else
-   str = str.toString();
-  return str.replace(/<[^>]*>/g, '');
+function stripHtmlTags(str) {
+  if (!str || str === null || str === "") {
+    return false;
+  } else {
+    str = str.toString();
+  }
+  return str.replace(/<[^>]*>/g, "");
+}
+
+function truncate({ text, maxLength = 2048 }) {
+  if (!text || text.length < maxLength) {
+    return text;
+  }
+
+  return `${text.slice(0, maxLength)}...`;
 }
 
 async function parse(fileContent) {
   return fileContent.site.flatMap(
-    ({ '@name': location, '@host': host, alerts }) => {
-      return alerts.map(alert => {
+    ({ "@name": location, "@host": host, alerts }) => {
+      return alerts.map((alert) => {
         return {
           name: stripHtmlTags(alert.name),
           description: stripHtmlTags(alert.desc),
           hint: alert.hint,
           category: alert.alert || stripHtmlTags(alert.name),
           location,
-          osi_layer: 'APPLICATION',
+          osi_layer: "APPLICATION",
           severity: riskToSeverity(alert.riskcode),
           attributes: {
             host: host,
             zap_confidence: alert.confidence || null,
             zap_count: alert.count || null,
             zap_solution: stripHtmlTags(alert.solution) || null,
-            zap_otherinfo: stripHtmlTags(alert.otherinfo) || null,
+            zap_otherinfo: truncate({
+              text: stripHtmlTags(alert.otherinfo) || null,
+              maxLength: 2048,
+            }),
             zap_reference: stripHtmlTags(alert.reference) || null,
             zap_cweid: alert.cweid || null,
             zap_wascid: alert.wascid || null,
