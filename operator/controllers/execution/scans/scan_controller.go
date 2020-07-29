7bd80bad9ba8413e8a811fa89c9ce974ad6a317e
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-07-29 08:57:33 +0200

    Move containsString func into scan_controller

--- operator/controllers/execution/scans/scan_controller.go
+++ operator/controllers/execution/scans/scan_controller.go
@@ -465,3 +465,13 @@ func removeString(slice []string, s string) (result []string) {
 	}
 	return
 }
+
+// Helper functions to check and remove string from a slice of strings.
+func containsString(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
