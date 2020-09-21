2128e54aa81a3b877bd41a2b679ff91eda478d12
Jannik Hollenbach <13718901+J12934@users.noreply.github.com>
2020-09-21 14:52:31 +0200

    Better debugging of failed scans during ci

--- tests/integration/helpers.js
+++ tests/integration/helpers.js
@@ -5,6 +5,7 @@ kc.loadFromDefault();
 
 const k8sCRDApi = kc.makeApiClient(k8s.CustomObjectsApi);
 const k8sBatchApi = kc.makeApiClient(k8s.BatchV1Api);
+const k8sPodsApi = kc.makeApiClient(k8s.CoreV1Api);
 
 const namespace = "integration-tests";
 
@@ -33,19 +34,56 @@ async function getScan(name) {
   return scan;
 }
 
+async function displayAllLogsForJob(jobName) {
+  console.log(`Listing logs for Job ${jobName}:`);
+  const {
+    body: { items: pods },
+  } = await k8sPodsApi.listNamespacedPod(
+    "default",
+    true,
+    undefined,
+    undefined,
+    undefined,
+    `job-name=${jobName}`
+  );
+
+  for (const pod of pods) {
+    for (const container of pod.spec.containers) {
+      const response = await k8sPodsApi.readNamespacedPodLog(
+        pod.metadata.name,
+        "default",
+        container.name
+      );
+      console.log(`Container ${container.name}:`);
+      console.log(response.body);
+    }
+  }
+}
+
 async function logJobs() {
   try {
     const { body: jobs } = await k8sBatchApi.listNamespacedJob(namespace);
+
+    console.log("Logging spec & status of jobs in namespace");
 
     for (const job of jobs.items) {
       console.log(`Job: '${job.metadata.name}' Spec:`);
       console.dir(job.spec);
       console.log(`Job: '${job.metadata.name}' Status:`);
       console.dir(job.status);
+
+      await displayAllLogsForJob(job.metadata.name);
     }
   } catch (error) {
     console.info(`Failed to list Jobs'`);
   }
+}
+
+async function disasterRecovery(scanName) {
+  const scan = await getScan(scanName);
+  console.error("Last Scan State:");
+  console.dir(scan);
+  await logJobs();
 }
 
 /**
@@ -54,7 +92,7 @@ async function logJobs() {
  * @param {string} scanType type of the scan. Must match the name of a ScanType CRD
  * @param {string[]} parameters cli argument to be passed to the scanner
  * @param {number} timeout in seconds
- * @returns {scan.findings} returns findings { categories, severities, count } 
+ * @returns {scan.findings} returns findings { categories, severities, count }
  */
 async function scan(name, scanType, parameters = [], timeout = 180) {
   const scanDefinition = {
@@ -88,19 +126,16 @@ async function scan(name, scanType, parameters = [], timeout = 180) {
       await deleteScan(actualName);
       return status.findings;
     } else if (status && status.state === "Errored") {
-      await deleteScan(actualName);
+      console.error("Scan Errored");
+      await disasterRecovery(actualName);
+
       throw new Error(
         `Scan failed with description "${status.errorDescription}"`
       );
     }
   }
-
   console.error("Scan Timed out!");
-
-  const scan = await getScan(actualName);
-  console.log("Last Scan State:");
-  console.dir(scan);
-  await logJobs();
+  await disasterRecovery(actualName);
 
   throw new Error("timed out while waiting for scan results");
 }
