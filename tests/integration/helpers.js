ead9eb7f43ce1e7e7363138da5d1d8789f2e992f
Jannik Hollenbach <13718901+J12934@users.noreply.github.com>
2020-09-21 15:26:30 +0200

    Duration is in sec not ms 🤦‍♂️

--- tests/integration/helpers.js
+++ tests/integration/helpers.js
@@ -9,8 +9,7 @@ const k8sPodsApi = kc.makeApiClient(k8s.CoreV1Api);
 
 const namespace = "integration-tests";
 
-const sleep = (duration) =>
-  new Promise((resolve) => setTimeout(resolve, duration * 1000));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms * 1000));
 
 async function deleteScan(name) {
   await k8sCRDApi.deleteNamespacedCustomObject(
@@ -125,7 +124,7 @@ async function scan(name, scanType, parameters = [], timeout = 180) {
     if (status && status.state === "Done") {
       await deleteScan(actualName);
       // Wait a couple seconds to give kubernetes more time to update the fields
-      await sleep(2000);
+      await sleep(2);
       const { status } = await getScan(actualName);
       return status.findings;
     } else if (status && status.state === "Errored") {
