ff188414a84f41e24850c7861a86bfdb79fee6f7
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-10-07 08:50:44 +0200

    Add Dashboard for Amass

--- hooks/persistence-elastic/dashboardImporter/export-dashboards.sh
+++ hooks/persistence-elastic/dashboardImporter/export-dashboards.sh
@@ -1,4 +1,4 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 kibanaURL=${1:-"http://localhost:5601"}
 
@@ -10,3 +10,4 @@ exportDashboard() {
 
 exportDashboard "daily-summary.json" "34c734b0-6e79-11ea-bdc0-35f8aa7c4664"
 exportDashboard "wordpress-overview.json" "12b72880-fc09-11ea-a91c-5358dd402fdc"
+exportDashboard "subdomain-overview.json" "83e56080-b235-11ea-872e-c9b5d5ddb284"
