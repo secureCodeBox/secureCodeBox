11dbd750805b5f4fe80d003c398d2f3fae3e85c4
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-10-07 08:58:58 +0200

    Add SSL/TLS Dasboard for (SSLyze)

--- hooks/persistence-elastic/dashboardImporter/export-dashboards.sh
+++ hooks/persistence-elastic/dashboardImporter/export-dashboards.sh
@@ -11,3 +11,4 @@ exportDashboard() {
 exportDashboard "daily-summary.json" "34c734b0-6e79-11ea-bdc0-35f8aa7c4664"
 exportDashboard "wordpress-overview.json" "12b72880-fc09-11ea-a91c-5358dd402fdc"
 exportDashboard "subdomain-overview.json" "83e56080-b235-11ea-872e-c9b5d5ddb284"
+exportDashboard "tls-ssl-overview.json" "f5e576c0-af97-11ea-b237-7b9069d086af"
