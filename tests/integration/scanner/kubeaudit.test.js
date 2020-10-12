10dd3e70728880f61abeaa04d142cbcf3427756f
SebieF <sebastian.franz@iteratec.com>
2020-10-12 13:38:40 +0200

    Integration test done

--- tests/integration/scanner/kubeaudit.test.js
+++ tests/integration/scanner/kubeaudit.test.js
@@ -1,19 +1,16 @@
 const { scan } = require("../helpers");
 
 test(
-  "kubeaudit should find a fixed number of findings for the the juice-shop",
+  "kubeaudit should run and check our integration-tests namespace",
   async () => {
-    const { categories, severities, count } = await scan(
-      "kubeaudit-jshop",
+    const { _ } = await scan(
+      "kubeaudit-test",
       "kubeaudit",
-      ["-n", "juice-shop"],
-      15
+      ["-n", "integration-tests"],
+      20
     );
-    console.log(categories)
-    console.log(severities)
-    console.log(count)
+    
     // If we got here the scan succeded
-    // as the number of findings will depend on the cluster, we just check if it is defined at all
     expect(true).toBe(true);
   },
   5 * 60 * 1000
