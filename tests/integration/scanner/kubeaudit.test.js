396cc97449be63e49d5c914febf907a450185121
SebieF <sebastian.franz@iteratec.com>
2020-10-12 16:26:56 +0200

    New ClusterRole Service Account and fixed test

--- tests/integration/scanner/kubeaudit.test.js
+++ tests/integration/scanner/kubeaudit.test.js
@@ -1,17 +1,29 @@
 const { scan } = require("../helpers");
 
 test(
-  "kubeaudit should run and check our integration-tests namespace",
+  "kubeaudit should run and check the jshop in kubeaudit-test namespace",
   async () => {
-    await scan(
+    const { categories, severities } = await scan(
       "kubeaudit-test",
       "kubeaudit",
-      ["-n", "integration-tests"],
+      ["-n", "kubeaudit-test"],
       60
     );
-    
-    // If we got here the scan succeded
-    expect(true).toBe(true);
+
+    expect(categories).toMatchInlineSnapshot(`
+      Object {
+        "Automounted ServiceAccount Token": 1,
+        "Capability Not Dropped": 14,
+        "Non ReadOnly Root Filesystem": 1,
+        "Non Root User Not Enforced": 1,
+      }
+    `);
+    expect(severities).toMatchInlineSnapshot(`
+      Object {
+        "low": 16,
+        "medium": 1,
+      }
+    `);
   },
   5 * 60 * 1000
 );
