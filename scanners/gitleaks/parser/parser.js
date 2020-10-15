ec12c47a50a6696d73537504a85b4272b4ddc816
Paul <paul.schmelzer@iteratec.com>
2020-10-15 13:35:39 +0200

    feature/gitleaks-scanner

---
+++ scanners/gitleaks/parser/parser.js
@@ -0,0 +1,45 @@
+HIGH_TAGS = ['JWT', 'Artifactory', 'AWS', 'PrivateKey'];
+MEDIUM_TAGS = ['Hash', 'Facebook', 'Twitter', 'Github', 'LinkedIn', 'Slack', 'Google', 'Heroku',
+  'Mailchimp', 'Mailgun', 'Paypal', 'Picatic', 'Teams', 'Jenkins', 'Stripe', 'Square', 'Twilio'];
+LOW_TAGS = ['Generic', 'FileName', 'FileExtension'];
+
+async function parse (fileContent) {
+
+  return fileContent.map(finding => {
+
+    let severity = 'LOW';
+
+    if (containsTag(finding.tags, HIGH_TAGS)) {
+      severity = 'HIGH'
+    } else if (containsTag(finding.tags, MEDIUM_TAGS)) {
+      severity = 'MEDIUM'
+    }
+
+    return {
+      name: finding.rule,
+      description: 'The name of the rule which triggered the finding: ' + finding.rule,
+      osi_layer: 'APPLICATION',
+      severity: severity,
+      category: 'potential secret',
+      attributes: {
+        commit: finding.commit,
+        repo: finding.repo,
+        offender: finding.offender,
+        author: finding.author,
+        email: finding.email,
+        date: finding.date,
+        file: finding.file,
+        line_number: finding.lineNumber,
+        tags: finding.tags.split(',').map(tag => tag.trim()),
+        line: finding.line
+      }
+    }
+  });
+}
+
+function containsTag (tag, tags) {
+  let result = tags.filter(longTag => tag.includes(longTag));
+  return result.length > 0;
+}
+
+module.exports.parse = parse;
---
+++ scanners/gitleaks/parser/parser.test.js
@@ -0,0 +1,184 @@
+const fs = require("fs");
+const util = require("util");
+
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+const readFile = util.promisify(fs.readFile);
+
+const { parse } = require("./parser");
+
+test("should properly parse empty gitleaks json file", async () => {
+  const jsonContent = await readFile(
+    __dirname + "/__testFiles__/test-empty-report.json",
+    {
+      encoding: "utf8"
+    }
+  );
+  expect(await parse(JSON.parse(jsonContent))).toMatchObject([]);
+});
+
+test("should properly parse gitleaks json file", async () => {
+  const jsonContent = await readFile(
+    __dirname + "/__testFiles__/test-report.json",
+    {
+      encoding: "utf8"
+    }
+  );
+  expect(await parse(JSON.parse(jsonContent))).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "attributes": Object {
+          "author": "Max Mustermann",
+          "commit": "2a42fc73f76e3fd9d015d0a98030037a8972e3d1",
+          "date": "2019-12-11T12:45:48+01:00",
+          "email": "max.mustermann@host.de",
+          "file": ".gitlab-ci.yml",
+          "line": "    - aws --profile default configure set aws_access_key_id \\"AKIAS2QBEJFO232FJDO\\"",
+          "line_number": 67,
+          "offender": "AKIAS2QBEJFO232FJDO",
+          "repo": "web-app",
+          "tags": Array [
+            "key",
+            "AWS",
+          ],
+        },
+        "category": "potential secret",
+        "description": "The name of the rule which triggered the finding: AWS Manager ID",
+        "name": "AWS Manager ID",
+        "osi_layer": "APPLICATION",
+        "severity": "HIGH",
+      },
+      Object {
+        "attributes": Object {
+          "author": "Max Mustermann",
+          "commit": "2a42fc73f76e3fd9d015d0a98030037a8972e3d1",
+          "date": "2019-12-11T12:45:48+01:00",
+          "email": "max.mustermann@host.de",
+          "file": ".gitlab-ci.yml",
+          "line": "    - aws --profile default configure set aws_secret_access_key \\"IccA5EboL5foAY3uUyG+zh5OA3rWdpL4C1ePuUOv\\"",
+          "line_number": 68,
+          "offender": "aws_secret_access_key \\"IccA5EboL5foAY3uUyG+zh5OA3rWdpL4C1ePuUOv\\"",
+          "repo": "paul-web",
+          "tags": Array [
+            "key",
+            "AWS",
+          ],
+        },
+        "category": "potential secret",
+        "description": "The name of the rule which triggered the finding: AWS Secret Key",
+        "name": "AWS Secret Key",
+        "osi_layer": "APPLICATION",
+        "severity": "HIGH",
+      },
+      Object {
+        "attributes": Object {
+          "author": "Max Mustermann",
+          "commit": "eaf6864262dbbcbf19c972cd961121b340b9968f",
+          "date": "2020-02-18T22:28:53+01:00",
+          "email": "max.mustermann@host.de",
+          "file": "helm/multi-juicer/values.yaml",
+          "line": "      password: ERzCT4pwBDxfCKRGmfrMa8KQ8sXf8GKy",
+          "line_number": 33,
+          "offender": "password: ERzCT4pwBDxfCKRGmfrMa8KQ8sXf8GKy",
+          "repo": "multi-juicer",
+          "tags": Array [
+            "key",
+            "Generic",
+          ],
+        },
+        "category": "potential secret",
+        "description": "The name of the rule which triggered the finding: Generic credentials",
+        "name": "Generic credentials",
+        "osi_layer": "APPLICATION",
+        "severity": "LOW",
+      },
+      Object {
+        "attributes": Object {
+          "author": "Max Mustermann",
+          "commit": "eaf6864262dbbcbf19c972cd961121b340b9968f",
+          "date": "2020-02-18T22:28:53+01:00",
+          "email": "max.mustermann@host.de",
+          "file": "juice-balancer/config/config.json",
+          "line": "      \\"password\\": \\"dRzCT4pwBDxfjfeRel23mMlKQ8sX\\"",
+          "line_number": 19,
+          "offender": "password\\": \\"dRzCT4pwBDxfjfeRel23mMlKQ8sX",
+          "repo": "multi-juicer",
+          "tags": Array [
+            "key",
+            "Generic",
+          ],
+        },
+        "category": "potential secret",
+        "description": "The name of the rule which triggered the finding: Generic credentials",
+        "name": "Generic credentials",
+        "osi_layer": "APPLICATION",
+        "severity": "LOW",
+      },
+      Object {
+        "attributes": Object {
+          "author": "Max Mustermann",
+          "commit": "88cf8694d4202bb7361f6779588f566e8eae2ff2",
+          "date": "2019-01-16T19:18:54+01:00",
+          "email": "max.mustermann@host.de",
+          "file": ".env",
+          "line": "N/A",
+          "line_number": -1,
+          "offender": "Filename/path offender: .env",
+          "repo": "secureCodeBox-v2",
+          "tags": Array [
+            "key",
+            "FileName",
+          ],
+        },
+        "category": "potential secret",
+        "description": "The name of the rule which triggered the finding: File names with potential keys and credentials",
+        "name": "File names with potential keys and credentials",
+        "osi_layer": "APPLICATION",
+        "severity": "LOW",
+      },
+      Object {
+        "attributes": Object {
+          "author": "Max Mustermann",
+          "commit": "eaf6864262dbbcbf19c972cd961121b340b9968f",
+          "date": "2019-01-16T19:18:54+01:00",
+          "email": "max.mustermann@host.de",
+          "file": ".env",
+          "line": " facebook_api_key: sj20gj2ß0kofepo2ṕf02",
+          "line_number": 30,
+          "offender": "sj20gj2ß0kofepo2ṕf02",
+          "repo": "madeuprepo",
+          "tags": Array [
+            "key",
+            "Facebook",
+          ],
+        },
+        "category": "potential secret",
+        "description": "The name of the rule which triggered the finding: Facebook Secret Key",
+        "name": "Facebook Secret Key",
+        "osi_layer": "APPLICATION",
+        "severity": "MEDIUM",
+      },
+      Object {
+        "attributes": Object {
+          "author": "Max Mustermann",
+          "commit": "2a42fc73f76e3fd9d015d0a98030037a8972e3d1",
+          "date": "2019-01-16T19:18:54+01:00",
+          "email": "max.mustermann@host.de",
+          "file": "key.pem",
+          "line": " -----BEGIN PRIVATE KEY-----",
+          "line_number": 1,
+          "offender": "-----BEGIN PRIVATE KEY-----",
+          "repo": "madeuprepo",
+          "tags": Array [
+            "key",
+            "PrivateKey",
+          ],
+        },
+        "category": "potential secret",
+        "description": "The name of the rule which triggered the finding: Asymmetric Private Key",
+        "name": "Asymmetric Private Key",
+        "osi_layer": "APPLICATION",
+        "severity": "HIGH",
+      },
+    ]
+  `);
+});
