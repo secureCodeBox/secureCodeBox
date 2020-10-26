1c64da0d6c1fba80b0e89d0ed2d4e7871354ee48
SebieF <sebastian.franz@iteratec.com>
2020-10-26 11:40:01 +0100

    Adding unattended mode
    Easier use in CLI to install every available resources via --all
    1. Extracting a lot of functions to avoid duplicated code
    2. Read parameters from command line
    3. Give usage feedback if not properly used
    4. Enhanced docu

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,11 @@
 # Official installation script for the secureCodeBox
 #
 # Creates namespace, securecodebox-system, and installs the operator.
-# Then interactively installs all possible resources (scanners, demo-apps, hooks).
+# Then installs all possible resources (scanners, demo-apps, hooks).
+#
+# There exist two modes:
+# Call without parameters to install interactively
+# Call with --all to install all available resources automatically
 #
 # For more information see https://docs.securecodebox.io/
 
@@ -22,21 +26,57 @@ COLOR_RESET="\e[0m"
 BASE_DIR=$(dirname "${SCRIPT_DIRECTORY}")
 
 function print() {
-  if [[ $# = 0 ]]; then
+  if [[ $# == 0 ]]; then
     echo
-  elif [[ $# = 1 ]]; then
+  elif [[ $# == 1 ]]; then
     local message="${1}"
     echo "${message}"
-  elif [[ $# = 2 ]]; then
+  elif [[ $# == 2 ]]; then
     local color="${1}"
     local message="${2}"
     echo -e "${color}${message}${COLOR_RESET}"
   fi
 }
 
+function usage() {
+  print "SecureCodeBox Install Script"
+  print "Usage:"
+  print "./install.sh: Interactive Install"
+  print "./install.sh --all: Unattended Install, installs all available resouces"
+}
+
+function checkKubectl() {
+  print
+  print "Checking kubectl..."
+  local kube
+  kube=$(kubectl version)
+
+  if [[ $kube == *"GitVersion"* ]]; then
+    print "$COLOR_OK" "It's there!"
+  else
+    print "$COLOR_ERROR" "Kubectl not found, quitting..."
+    exit 1
+  fi
+}
+
+function createNamespaceAndInstallOperator() {
+  print
+  print "Creating namespace securecodebox-system"
+  kubectl create namespace securecodebox-system || print "Namespace already exists..."
+
+  print "Installing the operator in the securecodebox-system namespace"
+
+  if [[ $(helm -n securecodebox-system upgrade --install securecodebox-operator "$BASE_DIR"/operator/) ]]; then
+    print "$COLOR_OK" "Successfully installed the operator!"
+  else
+    print "$COLOR_ERROR" "Operator installation failed, cancelling..." && exit 1
+  fi
+}
+
 function installResources() {
   local resource_directory="$1"
   local namespace="$2"
+  local unattended="$3"
 
   local resources=()
   for path in "$resource_directory"/*; do
@@ -46,18 +86,94 @@ function installResources() {
     resources+=("${directory}")
   done
 
-  for resource in "${resources[@]}"; do
-    print "Do you want to install $resource? [y/N]"
-    read -r line
-
-    if [[ $line == *[Yy] ]]; then
+  if [[ $unattended == True ]]; then
+    for resource in "${resources[@]}"; do
       local resource_name="${resource//+([_])/-}" # Necessary because ssh_scan is called ssh-scan
-      helm upgrade --install -n "$namespace" "$resource_name" "$resource_directory"/"$resource"/ || print "$COLOR_ERROR" "Installation of '$resource' failed"
-    fi
-  done
+      helm upgrade --install -n "$namespace" "$resource_name" "$resource_directory"/"$resource"/ \
+      || print "$COLOR_ERROR" "Installation of '$resource' failed"
+    done
+
+  else
+    for resource in "${resources[@]}"; do
+      print "Do you want to install $resource? [y/N]"
+      local line
+      read -r line
+
+      if [[ $line == *[Yy] ]]; then
+        local resource_name="${resource//+([_])/-}"
+        helm upgrade --install -n "$namespace" "$resource_name" "$resource_directory"/"$resource"/ \
+        || print "$COLOR_ERROR" "Installation of '$resource' failed"
+      fi
+    done
+  fi
 
   print
   print "$COLOR_OK" "Completed to install '$resource_directory'!"
+}
+
+function interactiveInstall() {
+  print "$COLOR_HIGHLIGHT" "Welcome to the secureCodeBox!"
+  print "This interactive installation script will guide you through all the relevant installation steps in order to have you ready to scan."
+  print "Start? [y/N]"
+
+  read -r LINE
+
+  if [[ $LINE == *[Yy] ]]; then
+    print "Starting!"
+  else
+    exit
+  fi
+
+  checkKubectl
+  createNamespaceAndInstallOperator
+
+  print
+  print "Starting to install scanners..."
+  installResources "$BASE_DIR/scanners" "default" False
+
+  print
+  print "Starting to install demo-apps..."
+  print "Do you want to install the demo apps in a separate namespace? Otherwise they will be installed into the [default] namespace [y/N]"
+  read -r line
+  NAMESPACE="default"
+  if [[ $line == *[Yy] ]]; then
+    print "Please provide a name for the namespace:"
+    read -r NAMESPACE
+    kubectl create namespace "$NAMESPACE" || print "Namespace already exists or could not be created.. "
+  fi
+
+  installResources "$BASE_DIR/demo-apps" "$NAMESPACE" False
+
+  print
+  print "Starting to install hooks..."
+  installResources "$BASE_DIR/hooks" "default" False
+
+  print
+  print "$COLOR_OK" "Information about your cluster:"
+  kubectl get namespaces
+  print
+  kubectl get scantypes
+  print
+  kubectl get pods
+
+  print
+  print "$COLOR_OK" "Finished installation successfully!"
+}
+
+function unattendedInstall() {
+  checkKubectl
+  createNamespaceAndInstallOperator
+
+  print "Starting to install scanners..."
+  installResources "$BASE_DIR/scanners" "default" True
+
+  print "Starting to install demo-apps..."
+  installResources "$BASE_DIR/demo-apps" "default" True
+
+  print "Starting to install hooks..."
+  installResources "$BASE_DIR/hooks" "default" True
+
+  print "$COLOR_OK" "Finished installation successfully!"
 }
 
 print "$COLOR_HIGHLIGHT" "                                                                             "
@@ -69,69 +185,12 @@ print "$COLOR_HIGHLIGHT" " \__ \  __/ (__| |_| | | |  __/ |___| (_) | (_| |  __/
 print "$COLOR_HIGHLIGHT" " |___/\___|\___|\__,_|_|  \___|\_____\___/ \__,_|\___|____/ \___/_/\_\       "
 print "$COLOR_HIGHLIGHT" "                                                                             "
 
-print "$COLOR_HIGHLIGHT" "Welcome to the secureCodeBox!"
-print "This interactive installation script will guide you through all the relevant installation steps in order to have you ready to scan."
-print "Start? [y/N]"
-
-read -r LINE
-
-if [[ $LINE == *[Yy] ]]; then
-  print "Starting!"
-else
-  exit
+if [[ $# == 0 ]]; then
+    interactiveInstall
+  elif [[ $# == 1 ]]; then
+    if [[ $1 == "--all" ]]; then
+      unattendedInstall
+    else
+      usage
+    fi
 fi
-
-print
-print "Checking kubectl..."
-KUBE=$(kubectl version)
-
-if [[ $KUBE == *"GitVersion"* ]]; then
-  print "$COLOR_OK" "It's there!"
-else
-  print "$COLOR_ERROR" "Kubectl not found, quitting..."
-  exit 1
-fi
-
-print
-print "Creating namespace securecodebox-system"
-kubectl create namespace securecodebox-system || print "Namespace already exists..."
-
-print "Installing the operator in the securecodebox-system namespace"
-
-if [[ $(helm -n securecodebox-system upgrade --install securecodebox-operator "$BASE_DIR"/operator/) ]]; then
-  print "$COLOR_OK" "Successfully installed the operator!"
-else
-  print "$COLOR_ERROR" "Operator installation failed, cancelling..." && exit 1
-fi
-
-print
-print "Starting to install scanners..."
-installResources "$BASE_DIR/scanners" "default"
-
-print
-print "Starting to install demo-apps..."
-print "Do you want to install the demo apps in a separate namespace? Otherwise they will be installed into the [default] namespace [y/N]"
-read -r line
-NAMESPACE="default"
-if [[ $line == *[Yy] ]]; then
-  print "Please provide a name for the namespace:"
-  read -r NAMESPACE
-  kubectl create namespace "$NAMESPACE" || print "Namespace already exists or could not be created.. "
-fi
-
-installResources "$BASE_DIR/demo-apps" "$NAMESPACE"
-
-print
-print "Starting to install hooks..."
-installResources "$BASE_DIR/hooks" "default"
-
-print
-print "$COLOR_OK" "Information about your cluster:"
-kubectl get namespaces
-print
-kubectl get scantypes
-print
-kubectl get pods
-
-print
-print "$COLOR_OK" "Finished installation successfully!"
