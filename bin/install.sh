f3ef3d3b365892b7404df13d74c21965419539e4
Sebastian Franz <32578476+SebieF@users.noreply.github.com>
2020-10-26 10:55:48 +0100

    Improve docu
    Co-authored-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- bin/install.sh
+++ bin/install.sh
@@ -2,9 +2,10 @@
 
 # Official installation script for the secureCodeBox
 #
-# Creates namespace securecodebox-system and installs the operator
-# Then interactively installs all possible resources (scanners, demo-apps, hooks)
-# https://docs.securecodebox.io/
+# Creates namespace, securecodebox-system, and installs the operator.
+# Then interactively installs all possible resources (scanners, demo-apps, hooks).
+#
+# For more information see https://docs.securecodebox.io/
 
 set -eu
 shopt -s extglob
