d83bb84261dcf46e587491de29d179c2b103ec7a
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-11-11 09:40:51 +0100

    Include More Tests
    I added some more tests to check if:
    - Multiple 'anyOff' are checked
    - Not matching rules are ignored
    
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/scan-postprocessing/hook.test.js
+++ hooks/scan-postprocessing/hook.test.js
@@ -53,3 +53,114 @@ test("Should Add High Severity and Description", async () => {
     }
   ]);
 });
+
+test("Should Check Multiple 'anyOf'", async () => {
+  const findings = [
+    {
+      category: "Open Port",
+      attributes: {
+        hostname: "foobar.com",
+        port: 23,
+        state: "open"
+      },
+    },
+  ];
+
+  const rules = [{
+    matches: {
+      anyOf: [
+        {
+          category: "Open Port",
+          attributes: {
+            port: 22,
+            state: "open"
+          }
+        },
+        {
+          category: "Open Port",
+          attributes: {
+            port: 23,
+            state: "open"
+          }
+        },
+      ]
+    },
+    override: {
+      severity: "high",
+      description: "Telnet is bad"
+    }
+  }]
+
+  const getFindings = async () => findings;
+
+  const updateFindings = jest.fn();
+
+  await handle({
+    getFindings,
+    updateFindings,
+    rules: rules,
+  });
+
+  expect(updateFindings).toBeCalledWith([
+    {
+      category: "Open Port",
+      attributes: {
+        hostname: "foobar.com",
+        port: 23,
+        state: "open"
+      },
+      severity: "high",
+      description: "Telnet is bad"
+    }
+  ]);
+});
+
+test("Should Ignore not Matching Conditions", async () => {
+  const findings = [
+    {
+      category: "Open Port",
+      attributes: {
+        hostname: "foobar.com",
+        port: 23,
+        state: "open"
+      },
+    },
+  ];
+
+  const rules = [{
+    matches: {
+      anyOf: [
+        {
+          category: "Open Port",
+          attributes: {
+            port: 22,
+            state: "open"
+          }
+        },
+        {
+          category: "Open Port",
+          attributes: {
+            port: 24,
+            state: "open"
+          }
+        },
+      ]
+    },
+    override: {
+      severity: "high",
+      description: "Telnet is bad"
+    }
+  }]
+
+  const getFindings = async () => findings;
+
+  const updateFindings = jest.fn();
+
+  await handle({
+    getFindings,
+    updateFindings,
+    rules: rules,
+  });
+
+  expect(updateFindings).toBeCalledWith([]);
+});
