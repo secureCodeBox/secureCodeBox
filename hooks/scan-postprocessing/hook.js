d41021a5ca66dfdcb8a30d129ae2086cd06a49cd
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-11-11 13:17:17 +0100

    Use _.merge() instead of Object.assign()
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/scan-postprocessing/hook.js
+++ hooks/scan-postprocessing/hook.js
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
-const { isMatch, isEqual } = require("lodash")
+const { isMatch, isEqual, merge } = require("lodash")
 async function handle({
   getFindings,
   updateFindings,
@@ -47,7 +47,7 @@ function applyRules(rules, findings) {
 }
 
 function postProcessFindings(finding, rule) {
-  const newFinding = Object.assign(finding, rule.override);
+  const newFinding = merge(finding, rule.override);
   return newFinding;
 }
 
