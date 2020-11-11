4cacdd2542707f06c632803cfeff374d2ae40b2d
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-11-11 09:21:30 +0100

    Add Test Checking If One Condition Matches
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/scan-postprocessing/hook.test.js
+++ hooks/scan-postprocessing/hook.test.js
@@ -1,6 +1,55 @@
-//const { getCascadingScans } = require("./hook");
+const { handle } = require("./hook")
 
-test("Should Test Scan Postprocessing", () => {
-  throw new Error("Test not implemented");
+test("Should Add High Severity and Description", async () => {
+  const findings = [
+    {
+      category: "Open Port",
+      attributes: {
+        hostname: "foobar.com",
+        port: 23,
+        state: "open"
+      },
+    },
+  ];
+
+  const rules = [{
+    matches: {
+      anyOf: [
+        {
+          category: "Open Port",
+          attributes: {
+            port: 23,
+            state: "open"
+          }
+        },
+      ]
+    },
+    override: {
+      severity: "high",
+      description: "Telnet is bad"
+    }
+  }]
+
+  const getFindings = async () => findings;
+
+  const updateFindings = jest.fn();
+
+  await handle({
+    getFindings,
+    updateFindings,
+    rules: rules,
+  });
+
+  expect(updateFindings).toBeCalledWith([
+    {
+      category: "Open Port",
+      attributes: {
+        hostname: "foobar.com",
+        port: 23,
+        state: "open"
+      },
+      severity: "high",
+      description: "Telnet is bad"
+    }
+  ]);
 });
-
