1b905f6c79bd1d44d11ef2d7453ae608fed3e901
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-11-11 14:57:37 +0100

    Remove Unnecessary Assignment
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/finding-post-processing/hook.js
+++ hooks/finding-post-processing/hook.js
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
-const { isMatch, isEqual, merge } = require("lodash")
+const { isMatch, merge } = require("lodash")
 async function handle({
   getFindings,
   updateFindings,
