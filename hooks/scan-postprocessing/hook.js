e4a391dcc9321185711a1ce63a27dacccc4121a6
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-11-11 09:20:52 +0100

    Fix Import and postprocessing
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/scan-postprocessing/hook.js
+++ hooks/scan-postprocessing/hook.js
@@ -1,5 +1,4 @@
-import { isMatch set } from "lodash";
-
+const { isMatch } = require("lodash")
 async function handle({
   getFindings,
   updateFindings,
@@ -7,33 +6,31 @@ async function handle({
 }) {
   const findings = await getFindings();
   const newFindings = applyRules(rules, findings);
-
-  if (newFindings.length === 0) {
-    await updateFindings(newFindings);
-  }
+  await updateFindings(newFindings);
 }
 module.exports.handle = handle;
 /**
  * Goes thought the Findings and the Scan Postprocessing Rules
  * and applies the changes to the findings defined in the rules if matching
  */
-export function applyRules(rules, findings) {
+function applyRules(rules, findings) {
   const newFindings = []
   for (const rule of rules) {
     for (const finding of findings) {
       // Check if one (ore more) of the Scan Postprocessing conditions apply to the finding
-      const isRuleMatching = rule.spec.matches.anyOf.some(condition =>
+      const isRuleMatching = rule.matches.anyOf.some(condition =>
         isMatch(finding, condition)
       );
 
       if (isRuleMatching) {
-        newFindings.push(postprocessFindings(finding, rule));
+        newFindings.push(postprocessingFindings(finding, rule));
       }
     }
   }
   return newFindings
 }
 
-function postprocessFindings(finding, rule) {
+function postprocessingFindings(finding, rule) {
+  const newFinding = Object.assign(finding, rule.override);
   return newFinding;
 }
