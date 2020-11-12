4e5c93e2390aad2d6d38c1e2c1225f1683389ea8
Sebastian Franz <sebastian.franz@iteratec.com>
2020-11-12 10:44:56 +0100

    Updating sslyze Test to target unsafe-https
    Signed-off-by: Sebastian Franz <sebastian.franz@iteratec.com>

--- tests/integration/scanner/sslyze.test.js
+++ tests/integration/scanner/sslyze.test.js
@@ -1,24 +1,27 @@
 const { scan } = require("../helpers");
 
 test(
-  "Sslyze scans our website securecodebox.io",
+  "Sslyze scans the self-signed unsafe-https demo-app",
   async () => {
     const { categories, severities, count } = await scan(
-      "sslyze-securecodebox",
+      "sslyze-unsafe-https",
       "sslyze",
-      ["--regular", "securecodebox.io"],
+      ["--regular", "unsafe-https.demo-apps.svc"],
       90
     );
 
-    expect(count).toBe(1);
+    expect(count).toBe(4);
     expect(categories).toMatchInlineSnapshot(`
       Object {
+        "Invalid Certificate": 1,
+        "Outdated TLS Version": 2,
         "TLS Service Info": 1,
       }
     `);
     expect(severities).toMatchInlineSnapshot(`
       Object {
         "informational": 1,
+        "medium": 3,
       }
     `);
   },
@@ -29,11 +32,7 @@ test(
   "Invalid argument should be marked as errored",
   async () => {
     await expect(
-      scan(
-      "sslyze-invalidArg",
-      "sslyze",
-      ["--invalidArg", "example.com"],
-      90)
+      scan("sslyze-invalidArg", "sslyze", ["--invalidArg", "example.com"], 90)
     ).rejects.toThrow("HTTP request failed");
   },
   3 * 60 * 1000
