777eb2f5d40cd3263d020592e8605a13ead96642
Robert Seedorff <Robert.Seedorff@iteratec.com>
2020-12-22 08:48:15 +0100

    Fixed codeclimate issues.

--- hooks/teams-webhook/hook.js
+++ hooks/teams-webhook/hook.js
@@ -7,7 +7,9 @@ async function handle({
   vulnMngmEnabled = process.env["VULNMANAG_ENABLED"],
   vulnMngmName = process.env["VULNMANAG_NAME"],
   vulnMngmDashboardUrl = process.env["VULNMANAG_DASHBOARD_URL"],
-  vulnMngmDashboardFindingsUrl = process.env["VULNMANAG_DASHBOARD_FINDINGS_URL"],
+  vulnMngmDashboardFindingsUrl = process.env[
+    "VULNMANAG_DASHBOARD_FINDINGS_URL"
+  ],
 }) {
   const findings = await getFindings();
 
@@ -15,108 +17,104 @@ async function handle({
     enabled: vulnMngmEnabled,
     name: vulnMngmName,
     dashboardUrl: vulnMngmDashboardUrl,
-    dashboardFindingsUrl: vulnMngmDashboardFindingsUrl
+    dashboardFindingsUrl: vulnMngmDashboardFindingsUrl,
   };
 
-  console.log(`Sending ${findings.length} findings to ${webhookUrl} with config: ` + JSON.stringify(vulnerabilityManagement));
+  console.log(
+    `Sending ${findings.length} findings to ${webhookUrl} with config: ` +
+      JSON.stringify(vulnerabilityManagement)
+  );
   console.log(scan);
 
   const paylod = getMessageCardByTemplate(scan, vulnerabilityManagement);
-  console.log(JSON.stringify(paylod));
+
+  console.log(`With Payload:` + JSON.stringify(paylod));
 
   await axios.post(webhookUrl, { paylod, findings });
 }
 
 /**
- * Returns a MS Teams WebHook Payload in the classic "MessageCard" style.
- * @param {*} scan 
+ * Returns a MS Teams WebHook Message Payload in the classic "MessageCard" style.
+ * @param {*} scan
+ * @param {*} vulnerabilityManagement
  */
 function getMessageCardByTemplate(scan, vulnerabilityManagement) {
   let messageCard = null;
 
-  if (vulnerabilityManagement.enabled === "true") {
-    messageCard = {
-      "@type": "MessageCard",
-      "@context": "https://schema.org/extensions",
-      "summary": `Scan ${scan.metadata.uid}`,
-      "themeColor": "0078D7",
-      "title": "New security scan results (Type: {{scanType}}) are available!",
-      "sections": [
-        {
-          "activityTitle": `Scheduled scan: **'${scan.metadata.name}'**`,
-          "activitySubtitle": `Finished at ${scan.finishedAt}`,
-          "activityImage": "https://docs.securecodebox.io/img/Favicon.svg",
-          "startGroup": true,
-          "facts": getMessageCardFacts(scan.status.findings.severities),
-          "text": "__Findings Severity Overview:__"
-        },
-        {
-          "facts": getMessageCardFacts(scan.status.findings.categories),
-          "text": "__Findings Category Overview:__"
-        }
-      ],
-      "potentialAction": [
-        {
-          "@type": "OpenUri",
-          "name": `Open ${vulnerabilityManagement.name}`,
-          "targets": [
-            {
-              "os": "default",
-              "uri": `${vulnerabilityManagement.dashboardUrl}`
-            }
-          ]
-        },
-        {
-          "@type": "OpenUri",
-          "name": `Show Results in ${vulnerabilityManagement.name}`,
-          "targets": [
-            {
-              "os": "default",
-              "uri": `${vulnerabilityManagement.dashboardFindingsUrl}`
-            }
-          ]
-        }
-      ]
-    };
-
-  } else {
-    messageCard = {
-      "@type": "MessageCard",
-      "@context": "https://schema.org/extensions",
-      "summary": `Scan ${scan.metadata.uid}`,
-      "themeColor": "0078D7",
-      "title": "New security scan results (Type: {{scanType}}) are available!",
-      "sections": [
-        {
-          "activityTitle": `Scheduled scan: **'${scan.metadata.name}'**`,
-          "activitySubtitle": `Finished at ${scan.finishedAt}`,
-          "activityImage": "https://docs.securecodebox.io/img/Favicon.svg",
-          "startGroup": true,
-          "facts": getMessageCardFacts(scan.status.findings.severities),
-          "text": "__Findings Severity Overview:__"
-        },
-        {
-          "facts": getMessageCardFacts(scan.status.findings.categories),
-          "text": "__Findings Category Overview:__"
-        }
-      ]
-    };
-  }
+  messageCard = {
+    "@type": "MessageCard",
+    "@context": "https://schema.org/extensions",
+    summary: `Scan ${scan.metadata.uid}`,
+    themeColor: "0078D7",
+    title: "New security scan results (Type: {{scanType}}) are available!",
+    sections: [
+      {
+        activityTitle: `Scheduled scan: **'${scan.metadata.name}'**`,
+        activitySubtitle: `Finished at ${scan.finishedAt}`,
+        activityImage: "https://docs.securecodebox.io/img/Favicon.svg",
+        startGroup: true,
+        facts: getMessageCardFactsPayload(scan.status.findings.severities),
+        text: "__Findings Severity Overview:__",
+      },
+      {
+        facts: getMessageCardFactsPayload(scan.status.findings.categories),
+        text: "__Findings Category Overview:__",
+      },
+    ],
+    ...(vulnerabilityManagement.enabled === "true" && getMessageCardActionPayload(vulnerabilityManagement)),
+  };
 
   console.log("Post Payload: \n" + messageCard);
 
   return messageCard;
 }
 
-function getMessageCardFacts(facts) {
+/**
+ * Returns a MS Teams Webhook potentialAction message object, based on the given vulnerabilityManagement configuration data.
+ * @param {*} vulnerabilityManagement A vulnerabilityManagement configuration object
+ */
+function getMessageCardActionPayload(vulnerabilityManagement) {
+  const result = {
+    potentialAction: [
+      {
+        "@type": "OpenUri",
+        name: `Open ${vulnerabilityManagement.name}`,
+        targets: [
+          {
+            os: "default",
+            uri: `${vulnerabilityManagement.dashboardUrl}`,
+          },
+        ],
+      },
+      {
+        "@type": "OpenUri",
+        name: `Show Results in ${vulnerabilityManagement.name}`,
+        targets: [
+          {
+            os: "default",
+            uri: `${vulnerabilityManagement.dashboardFindingsUrl}`,
+          },
+        ],
+      },
+    ],
+  }
+
+  return result;
+}
+
+/**
+ * Returns a MS Teams Webhook facts message object, based on the given facts data.
+ * @param {*} scanData The scan data as array
+ */
+function getMessageCardFactsPayload(scanData) {
   const result = [];
-  for (var key in facts) {
+  for (var key in scanData) {
     // Wonder why this call? https://dev.to/aman_singh/what-s-the-deal-with-object-prototype-hasownproperty-call-4mbj
-    if (Object.prototype.hasOwnProperty.call(facts, key)) {
+    if (Object.prototype.hasOwnProperty.call(scanData, key)) {
       // console.log(key + " -> " + facts[key]);
       result.push({
-        "name": `${key}`,
-        "value": `${facts[key]}`
+        name: `${key}`,
+        value: `${scanData[key]}`,
       });
     }
   }
