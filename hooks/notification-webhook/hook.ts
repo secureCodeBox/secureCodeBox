dd8fc610e6028bb460b23b72a503375681316748
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-03-10 08:24:54 +0100

    Move Hook To TypeScript
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

---
+++ hooks/notification-webhook/hook.ts
@@ -0,0 +1,37 @@
+/**
+Copyright 2020 iteratec GmbH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+import * as Mustach from "mustache"
+import { isMatch } from "lodash";
+import * as axios from "axios"
+
+async function handle({ getFindings, scan, notifications = JSON.parse(process.env["NOTIFIERS"]) }) {
+  const findings: Finding[] = await getFindings();
+  notifications.forEach((notification: FindingNotification) => {
+    let matchingFindings = findings.filter(finding => matches(finding, notification.rules));
+    const notifier = NotifierFactory.create(notification.type, notification.template);
+    notifier.sendNotification(matchingFindings);
+  }
+}
+
+function matches(finding: Finding, rules: any): boolean {
+  let matches = false;
+  for (let rule of rules) {
+    if (isMatch(finding, rule)) return true;
+  }
+  return false
+}
+
+module.exports.axios = axios;
