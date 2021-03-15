24e55618d6137a43c99d47c56f61e8a8d49e861c
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-03-15 08:15:26 +0100

    Add Init() Method To Initialize The Notifier
    This Method shall init the Notifier with everything needed to send
    notifications including preparing the template.
    
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-webhook/src/Notifier.ts
+++ hooks/notification-webhook/src/Notifier.ts
@@ -1,5 +1,14 @@
 import { Finding } from "./model/Finding"
 
 export interface Notifier {
-  sendNotification(findings: Finding[]): void
+  /**
+   * Sends a Notification Message to the desired End-Point (e.g. Slack or MS Teams) 
+   * @param findings Findings that should be included in the Message
+   */
+  sendMessage(findings: Finding[]): string
+
+  /**
+   * Initializes the Notifier with the given template
+   */
+  init(): void
 }
