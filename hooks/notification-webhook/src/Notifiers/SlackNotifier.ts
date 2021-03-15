d2608fe4d5826bde33f7471f19dedb9fcb5d9ebc
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-03-15 09:06:57 +0100

    Update SlackNotifier Based on AbstractNotifier
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-webhook/src/Notifiers/SlackNotifier.ts
+++ hooks/notification-webhook/src/Notifiers/SlackNotifier.ts
@@ -1,13 +1,16 @@
+import { NotifierType } from "../NotifierType"
 import { AbstractNotifier } from "./AbstractNotifier"
+import { Finding } from "../model/Finding"
+import Mustache from 'mustache';
 
 export class SlackNotifier extends AbstractNotifier {
-  constructor(template: string) {
-    super(template)
+  protected type: NotifierType = NotifierType.SLACK
+  constructor() {
+    super();
   }
-  loadDefaultTemplate() {
-    this.template = this.template
-  }
-  sendNotification() {
 
+  sendMessage(findings: Finding[]): string {
+    console.log("Render Slack Message");
+    return Mustache.render(this.template, { scanner: "nmap" });
   }
 }
