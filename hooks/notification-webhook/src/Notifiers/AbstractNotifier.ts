d8394aa2c04aa53f983056033898f8903807950b
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-03-15 09:04:08 +0100

    Refactoring AbstractNotifier
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-webhook/src/Notifiers/AbstractNotifier.ts
+++ hooks/notification-webhook/src/Notifiers/AbstractNotifier.ts
@@ -1,16 +1,39 @@
-import { Notifier } from "../Notifier"
+import { Notifier } from "../Notifier";
+import * as fs from "fs";
+import * as util from "util";
+import { NotifierType } from "../NotifierType";
+import { Finding } from "../model/Finding";
+import axios from 'axios';
 
 export abstract class AbstractNotifier implements Notifier {
-  protected template: string
+  private readonly TEMPLATE_DIR: string = "./templates";
+  protected template: string;
+  protected abstract type: NotifierType;
 
-  constructor(template) {
-    if (null == template || "" === template) {
-      this.loadDefaultTemplate();
-    } else {
-      this.template = template;
+  constructor() { }
+
+  public async initCustomTemplate(): Promise<void> {
+    if (this.template == null || this.template === "") {
+      console.log("No Custom Template found. Loading Default Template")
+      await this.loadDefaultTemplate();
     }
   }
 
-  abstract sendNotification(): void
-  abstract loadDefaultTemplate(): void
+  public async initTemplate(templateName: string): Promise<void> {
+    if (templateName !== "") {
+      await this.loadTemplate(`${this.TEMPLATE_DIR}/${this.type}/${templateName}`)
+    }
+  }
+
+  public abstract sendMessage(findings: Finding[]): string
+
+  private async loadDefaultTemplate(): Promise<void> {
+    await this.loadTemplate(`${this.TEMPLATE_DIR}/${this.type}/messageCard.json`)
+  }
+
+  private async loadTemplate(template: string): Promise<void> {
+    const readFile = util.promisify(fs.readFile)
+    const buf = await readFile(template, "utf8");
+    this.template = buf.toString();
+  }
 }
