4a1096be5da7cc6cc0b862deed2084b6b74f59d7
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-03-17 08:22:10 +0100

    Load All Templates From Same Directory
    We decided it would be best if we would load all templates, custom as
    well as shipped templates from the same directory to reduce overhead
    
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-webhook/src/Notifiers/AbstractNotifier.ts
+++ hooks/notification-webhook/src/Notifiers/AbstractNotifier.ts
@@ -3,7 +3,6 @@ import * as fs from "fs";
 import * as util from "util";
 import { NotifierType } from "../NotifierType";
 import { Finding } from "../model/Finding";
-import { TemplateType } from "../templateType";
 
 export abstract class AbstractNotifier implements Notifier {
   private static readonly TEMPLATE_DIR: string = "./templates";
@@ -13,29 +12,15 @@ export abstract class AbstractNotifier implements Notifier {
 
   constructor() { }
 
-  public async initCustomTemplate(template: string): Promise<void> {
-    if (template == null || template === "") {
-      console.log("No Custom Template found. Loading Default Template")
-      await this.loadDefaultTemplate();
-    }
-    this.template = template;
-  }
-
-  public abstract initTemplate(templateType: string): Promise<void>;
-
   protected async loadTemplate(templateName: string) {
-    await this.loadFile(`${AbstractNotifier.TEMPLATE_DIR}/${this.type}/${templateName}.${AbstractNotifier.TEMPLATE_FILE_TYPE}`)
+    this.template = this.loadFileAsString(`${AbstractNotifier.TEMPLATE_DIR}/${this.type}/${templateName}.${AbstractNotifier.TEMPLATE_FILE_TYPE}`);
   }
 
   public abstract sendMessage(findings: Finding[]): Promise<void>
 
-  private async loadDefaultTemplate(): Promise<void> {
-    await this.loadTemplate(TemplateType.MESSAGE_CARD)
-  }
-
-  private async loadFile(template: string): Promise<void> {
+  private loadFileAsString(template: string): string {
     const readFile = util.promisify(fs.readFile)
-    const buf = await readFile(template, "utf8");
-    this.template = buf.toString();
+    const buf = fs.readFileSync(template, "utf8");
+    return buf.toString();
   }
 }
