ff836616f9ff8b1d94e65f93988c13e50db70e7b
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-03-18 09:47:46 +0100

    Remove Test Cases That Do Not Test SlackNotifier
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-webhook/src/Notifiers/SlackNotifier.test.ts
+++ hooks/notification-webhook/src/Notifiers/SlackNotifier.test.ts
@@ -114,22 +114,10 @@ test("Should Include Link To DefectDojo Project", async () => {
   expect(true).toBe(false)
 })
 
-test("Should Only Send Post Request for Matching Rules", async () => {
-  expect(true).toBe(false)
-})
-
-test("Should Apply Custom Template", async () => {
-  expect(true).toBe(false)
-})
-
 test("Should Send Minimal Template For Disabled VulnerabilityManagement", async () => {
   expect(true).toBe(false)
 })
 
 test("Should Send Minimal Template For Incomplete VulnerabilityManagement Configuration", async () => {
-  expect(true).toBe(false)
-})
-
-test("Should Only Send Post Request For High Severity Findings", async () => {
   expect(true).toBe(false)
 })
