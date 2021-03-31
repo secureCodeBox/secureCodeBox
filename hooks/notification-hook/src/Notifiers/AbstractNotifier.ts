f3841c0fcd3298e35944947b93bec0de7f55724e
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-03-31 08:45:04 +0200

    Change Templating Engine To Nunjucks
    Experimenting with different Templates we encountered the problem that
    we had to add a lot of helper methods due to mustache beeing a logic
    less templating engine.
    
    This would have meant that it would be necessary to provide new helper
    methods for new templates that want to display different information of
    scan or findings.
    
    To resolve this issue we decided to switch to the Nunjucks templating
    engine
    
    This Commit:
    - Switches template engine
    - removes now unnecessary helper methods
    - write the slack-messageCard template with nunjucks
    
    See: https://mozilla.github.io/nunjucks/
    
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-hook/src/Notifiers/AbstractNotifier.ts
+++ hooks/notification-hook/src/Notifiers/AbstractNotifier.ts
@@ -2,15 +2,15 @@ import { Notifier } from "../Notifier";
 import * as fs from "fs";
 import { NotifierType } from "../NotifierType";
 import { Finding } from "../model/Finding";
-import * as Mustache from "mustache";
 import { NotificationChannel } from "../model/NotificationChannel";
 import * as jsyaml from "js-yaml";
 import { Scan } from "../model/Scan";
 import * as path from 'path';
+import * as nunjucks from 'nunjucks';
 
 export abstract class AbstractNotifier implements Notifier {
   private static readonly TEMPLATE_DIR: string = path.join(__dirname, "../templates");
-  private static readonly TEMPLATE_FILE_TYPE = "mustache";
+  private static readonly TEMPLATE_FILE_TYPE = "njk";
   protected channel: NotificationChannel;
   protected scan: Scan;
   protected findings: Finding[];
@@ -23,24 +23,13 @@ export abstract class AbstractNotifier implements Notifier {
     this.findings = findings;
   }
 
-  protected async loadTemplate() {
-    console.log(`Try to Load Template "${this.channel.template}"`)
-    this.template = this.loadFileAsString(`${AbstractNotifier.TEMPLATE_DIR}/${this.channel.template}.${AbstractNotifier.TEMPLATE_FILE_TYPE}`);
-  }
-
   public abstract sendMessage(): Promise<void>
 
-  private loadFileAsString(template: string): string {
-    return fs.readFileSync(template, "utf8");
-  }
-
   protected renderMessage(): string {
-    this.loadTemplate();
-    const renderedTemplate = Mustache.render(this.template, {
+    nunjucks.configure(`${AbstractNotifier.TEMPLATE_DIR}`)
+    const renderedTemplate = nunjucks.render(`${this.channel.template}.${AbstractNotifier.TEMPLATE_FILE_TYPE}`, {
       "findings": this.findings,
-      "scan": this.scan,
-      "severities": this.getSeverityOverview(),
-      "categories": this.getCategoryOverview(),
+      "scan": this.scan
     });
     try {
       const templateObject = jsyaml.load(renderedTemplate);
@@ -48,31 +37,5 @@ export abstract class AbstractNotifier implements Notifier {
     } catch (e) {
       console.log(e)
     }
-  }
-
-  protected getSeverityOverview(): any {
-    try {
-      const severities = this.getDetails(this.scan.status.findings.severities);
-      return severities;
-    } catch (error) {
-      console.log(`There was an Error getting Severities from Scan: ${error}`)
-    }
-    return null;
-  }
-
-  protected getCategoryOverview(): any {
-    try {
-      const categories = this.getDetails(this.scan.status.findings.categories);
-      return categories;
-    } catch (error) {
-      console.log(`There was an Error getting Categories from Scan: ${error}`)
-    }
-    return null;
-  }
-
-  protected getDetails(data): any[] {
-    return data != null
-      ? Object.entries(data).map(([name, value]) => ({ name, value }))
-      : [];
   }
 }
