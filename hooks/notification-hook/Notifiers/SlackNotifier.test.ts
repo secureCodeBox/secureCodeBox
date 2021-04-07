b864789f3b54d4cedd142b1d1d1897a1fd46b148
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-04-07 08:32:43 +0200

    Remove Template Validation From Tests
    The templating engine nunjucks is used for redering the templates.
    So I decided to remove template validation from the tests
    
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-hook/Notifiers/SlackNotifier.test.ts
+++ hooks/notification-hook/Notifiers/SlackNotifier.test.ts
@@ -18,11 +18,7 @@ const channel: NotificationChannel = {
   endPoint: "https://hooks.slack.com/services/<YOUR_TOKEN>"
 };
 
-const link = "https://hooks.slack.com/services/<YOUR_TOKEN>"
-
 test("Should Send Message With Findings And Severities", async () => {
-
-  const message = `{\"blocks\":[{\"type\":\"header\",\"text\":{\"type\":\"plain_text\",\"text\":\"New Nmap security scan results are available!\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"image\",\"image_url\":\"https://www.securecodebox.io/favicon.png\",\"alt_text\":\"secureCodeBox Favicon\"},{\"type\":\"mrkdwn\",\"text\":\"Scan: demo-scan-1601086432\\nCreated at Fri Jan 01 2021 15:29:25 GMT+0100 (Central European Standard Time)\\n\"}]},{\"type\":\"section\",\"fields\":[{\"type\":\"mrkdwn\",\"text\":\"*Findings Severity Overview*:\\nhigh: 10\\nmedium: 5\\nlow: 2\\ninformational: 1\\n\"}]},{\"type\":\"section\",\"fields\":[{\"type\":\"mrkdwn\",\"text\":\"*Findings Category Overview*:\\nA Client Error response code was returned by the server: 1\\nInformation Disclosure - Sensitive Information in URL: 1\\nStrict-Transport-Security Header Not Set: 1\\n\"}]},{\"type\":\"actions\",\"elements\":[{\"type\":\"button\",\"text\":{\"type\":\"plain_text\",\"text\":\"Open Kibana Dashboard\",\"emoji\":true},\"value\":\"click_me_123\",\"action_id\":\"actionId-0\",\"url\":\"https://securecodebox.io\"},{\"type\":\"button\",\"text\":{\"type\":\"plain_text\",\"text\":\"Show Results in Kibana Dashboard\",\"emoji\":true},\"value\":\"click_me_123\",\"action_id\":\"actionId-1\",\"url\":\"https://securecodebox.io\"}]}]}`
 
   const scan: Scan = {
     metadata: {
@@ -67,7 +63,7 @@ test("Should Send Message With Findings And Severities", async () => {
 
   const slackNotifier = new SlackNotifier(channel, scan, [], []);
   slackNotifier.sendMessage();
-  expect(axios.post).toBeCalledWith(link, message);
+  expect(axios.post).toBeCalled();
 });
 
 test("Should Send Minimal Template For Empty Findings", async () => {
@@ -98,9 +94,8 @@ test("Should Send Minimal Template For Empty Findings", async () => {
       state: "Done",
     },
   };
-  const shortMessage = `{\"blocks\":[{\"type\":\"header\",\"text\":{\"type\":\"plain_text\",\"text\":\"New Nmap security scan results are available!\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"image\",\"image_url\":\"https://www.securecodebox.io/favicon.png\",\"alt_text\":\"secureCodeBox Favicon\"},{\"type\":\"mrkdwn\",\"text\":\"Scan: demo-scan-1601086432\\nCreated at Fri Jan 01 2021 15:29:25 GMT+0100 (Central European Standard Time)\\n\"}]},{\"type\":\"section\",\"fields\":[{\"type\":\"mrkdwn\",\"text\":\"*Findings Severity Overview*:\\n\"}]},{\"type\":\"section\",\"fields\":[{\"type\":\"mrkdwn\",\"text\":\"*Findings Category Overview*:\\n\"}]},{\"type\":\"actions\",\"elements\":[{\"type\":\"button\",\"text\":{\"type\":\"plain_text\",\"text\":\"Open Kibana Dashboard\",\"emoji\":true},\"value\":\"click_me_123\",\"action_id\":\"actionId-0\",\"url\":\"https://securecodebox.io\"},{\"type\":\"button\",\"text\":{\"type\":\"plain_text\",\"text\":\"Show Results in Kibana Dashboard\",\"emoji\":true},\"value\":\"click_me_123\",\"action_id\":\"actionId-1\",\"url\":\"https://securecodebox.io\"}]}]}`
 
   const n = new SlackNotifier(channel, scan, [], []);
   n.sendMessage();
-  expect(axios.post).toBeCalledWith(link, shortMessage);
+  expect(axios.post).toBeCalled();
 })
