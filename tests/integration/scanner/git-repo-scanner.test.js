4457a3c1df9e5d2801e16e3f37eaaa9df01e30e2
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-04-12 10:57:33 +0200

    Updated integration test for git-repo-scanner.

--- tests/integration/scanner/git-repo-scanner.test.js
+++ tests/integration/scanner/git-repo-scanner.test.js
@@ -3,11 +3,13 @@ const {scan} = require('../helpers');
 test(
   'gitleaks should find at least 1 repository in the GitHub secureCodeBox organisation',
   async () => {
-    const {categories, severities, count} = await scan(
+    // This integration tests runs about 30min because of the GitHub Public API call rate limit.
+    // If you want to speed up you need to add an valid access token like: ['--git-type', 'github', '--organization', 'secureCodeBox', '--access-token', '23476VALID2345TOKEN'],
+    const {count} = await scan(
       'git-repo-scanner-dummy-scan',
       'git-repo-scanner',
       ['--git-type', 'github', '--organization', 'secureCodeBox'],
-      180
+      90
     );
     // There must be >= 28 Repositories found in the GitHub secureCodeBox organisation.
     expect(count).toBeGreaterThanOrEqual(28);
