aa57849494312c60fc29261620fa4488f7e681b3
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-04-16 11:03:30 +0200

    Removed wrong comments

--- tests/integration/scanner/amass.test.js
+++ tests/integration/scanner/amass.test.js
@@ -1,17 +1,14 @@
 const {scan} = require('../helpers');
 
 test(
-  'amass should find at least 1 subdomain',
+  'amass should find at least 20 subdomains',
   async () => {
-    // This integration tests runs about 30min because of the GitHub Public API call rate limit.
-    // If you want to speed up you need to add an valid access token like: ['--git-type', 'github', '--organization', 'secureCodeBox', '--access-token', '23476VALID2345TOKEN'],
     const {count} = await scan(
       'amass-scanner-dummy-scan',
       'amass',
       ['-passive', '-noalts', '-norecursive', '-d', 'owasp.org'],
       90
     );
-    // There must be >= 20 subdomains found for the domain owasp.org.
     expect(count).toBeGreaterThanOrEqual(20);
   },
   3 * 60 * 1000
