c98bf29d8b9457a3c5e2d5999d37eed57f21734e
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-04-17 12:05:19 +0200

    Bugfixing CI Pipeline.

---
+++ scanners/zap-extended/scanner/tests/test_zap_extended.py
@@ -0,0 +1,17 @@
+from unittest.mock import MagicMock, Mock
+from unittest.mock import patch
+
+
+from scbzapv2.zap_configuration import ZapConfiguration
+from scbzapv2.zap_extended import ZapExtended
+
+class ZapConfigurationTests(unittest.TestCase):
+
+    def simple_check(self):
+        pass
+
+    def complex_check(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
