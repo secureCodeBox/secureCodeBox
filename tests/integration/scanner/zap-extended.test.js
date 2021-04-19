df49cedea3a00cce2cffc5781cdf33d4003d2f7a
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-04-19 12:30:58 +0200

    Fixing e-2-2 test

--- tests/integration/scanner/zap-extended.test.js
+++ tests/integration/scanner/zap-extended.test.js
@@ -12,16 +12,20 @@ test(
 
     expect(categories).toMatchInlineSnapshot(`
       Object {
-        "Content Security Policy (CSP) Header Not Set": 1,
-        "Server Leaks Version Information via \\"Server\\" HTTP Response Header Field": 1,
-        "X-Content-Type-Options Header Missing": 1,
-        "X-Frame-Options Header Not Set": 1,
+        "Content Security Policy (CSP) Header Not Set": 2,
+        "Incomplete or No Cache-control and Pragma HTTP Header Set": 1,
+        "Retrieved from Cache": 1,
+        "Server Leaks Version Information via \\"Server\\" HTTP Response Header Field": 2,
+        "Strict-Transport-Security Header Not Set": 1,
+        "X-Content-Type-Options Header Missing": 2,
+        "X-Frame-Options Header Not Set": 2,
       }
     `);
     expect(severities).toMatchInlineSnapshot(`
       Object {
-        "low": 2,
-        "medium": 2,
+        "informational": 1,
+        "low": 6,
+        "medium": 4,
       }
     `);
   },
