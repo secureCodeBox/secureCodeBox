03bafa757688c2d0bb124525559da5d02effb151
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-04-21 09:10:29 +0200

    Minor Refactoring
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

--- hooks/notification-hook/Notifiers/EMailNotifier.ts
+++ hooks/notification-hook/Notifiers/EMailNotifier.ts
@@ -8,21 +8,31 @@ export class EMailNotifier extends AbstractNotifier {
   protected type: NotifierType.EMAIL;
 
   public async sendMessage(): Promise<void> {
-    const transporter = createTransport(this.getSMTPConfig());
-    const message = JSON.parse(this.renderMessage());
-    message.to = this.channel.endPoint;
-    message.from = this.getSMTPConfig().from;
+    const message = this.prepareMessage();
+    const smtpConfig = this.getSMTPConfig();
+    await this.sendMail(message, smtpConfig);
+  }
+
+  private getSMTPConfig(): any {
+    return jsyaml.load(process.env[EMailNotifier.SMTP_CONFIG]);
+  }
+
+  protected async sendMail(message: any, smtpConfig: any) {
+    const transporter = createTransport(smtpConfig);
     try {
       const info = await transporter.sendMail(message);
       console.log(info);
     } catch (e) {
       console.log(`There was an error sending the email: ${e}`)
     } finally {
-      transporter.close()
+      transporter.close();
     }
   }
 
-  private getSMTPConfig(): any {
-    return jsyaml.load(process.env[EMailNotifier.SMTP_CONFIG]);
+  private prepareMessage(): any {
+    const message = JSON.parse(this.renderMessage());
+    message.to = this.channel.endPoint;
+    message.from = this.getSMTPConfig().from;
+    return message;
   }
 }
