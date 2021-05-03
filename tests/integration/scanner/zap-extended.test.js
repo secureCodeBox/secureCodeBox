6ae5ea0116d12b335e339b4e8615fca3b07f7873
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-05-03 04:33:37 +0200

    Added juiceshop ZAP Extended Integration test.

--- tests/integration/scanner/zap-extended.test.js
+++ tests/integration/scanner/zap-extended.test.js
@@ -1,12 +1,40 @@
 const { scan } = require("../helpers");
 
 test(
-  "ZAP-extended scan without config YAML against a plain nginx container should only find couple findings",
+  "ZAP-extended scan without config YAML against a plain 'nginx container' should only find couple findings",
   async () => {
     const { categories, severities } = await scan(
       "zap-extended-scan-nginx-demo",
       "zap-extended-scan",
       ["-t", "http://nginx.demo-apps.svc"],
+      60 * 6
+    );
+
+    expect(categories).toMatchInlineSnapshot(`
+      Object {
+        "Content Security Policy (CSP) Header Not Set": 1,
+        "Server Leaks Version Information via \\"Server\\" HTTP Response Header Field": 1,
+        "X-Content-Type-Options Header Missing": 1,
+        "X-Frame-Options Header Not Set": 1,
+      }
+    `);
+    expect(severities).toMatchInlineSnapshot(`
+      Object {
+        "low": 2,
+        "medium": 2,
+      }
+    `);
+  },
+  6 * 60 * 1000
+);
+
+test(
+  "ZAP-extended scan without config YAML against 'bodgeit' container should only find couple findings",
+  async () => {
+    const { categories, severities } = await scan(
+      "zap-extended-scan-bodgeit-demo",
+      "zap-extended-scan",
+      ["-t", "http://bodgeit.demo-apps.svc:8080/bodgeit/"],
       60 * 6
     );
 
@@ -33,12 +61,12 @@ test(
 );
 
 test(
-  "ZAP-extended scan without config YAML against bodgeit container should only find couple findings",
+  "ZAP-extended scan without config YAML against 'juiceshop' should only find couple findings",
   async () => {
     const { categories, severities } = await scan(
-      "zap-extended-scan-bodgeit-demo",
+      "zap-extended-scan-juiceshop-demo",
       "zap-extended-scan",
-      ["-t", "http://bodgeit.default.svc:8080/bodgeit/"],
+      ["-t", "http://juiceshop.demo-apps.svc:3000/"],
       60 * 6
     );
 
