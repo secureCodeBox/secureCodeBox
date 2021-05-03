1ab787bbc69ef7084b84153525fe369bf8b2a57e
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-05-03 03:54:55 +0200

    Bugfixed DNS error.

--- scanners/zap-extended/scanner/scbzapv2/__main__.py
+++ scanners/zap-extended/scanner/scbzapv2/__main__.py
@@ -3,6 +3,7 @@ import json
 import logging
 import sys
 import time
+import errno
 
 from pathlib import Path
 from zapv2 import ZAPv2
--- tests/integration/scanner/zap-extended.test.js
+++ tests/integration/scanner/zap-extended.test.js
@@ -31,3 +31,35 @@ test(
   },
   6 * 60 * 1000
 );
+
+test(
+  "ZAP-extended scan without config YAML against bodgeit container should only find couple findings",
+  async () => {
+    const { categories, severities } = await scan(
+      "zap-extended-scan-bodgeit-demo",
+      "zap-extended-scan",
+      ["-t", "http://bodgeit.default.svc:8080/bodgeit/"],
+      60 * 6
+    );
+
+    expect(categories).toMatchInlineSnapshot(`
+      Object {
+        "Content Security Policy (CSP) Header Not Set": 2,
+        "Incomplete or No Cache-control and Pragma HTTP Header Set": 1,
+        "Retrieved from Cache": 1,
+        "Server Leaks Version Information via \\"Server\\" HTTP Response Header Field": 2,
+        "Strict-Transport-Security Header Not Set": 1,
+        "X-Content-Type-Options Header Missing": 2,
+        "X-Frame-Options Header Not Set": 2,
+      }
+    `);
+    expect(severities).toMatchInlineSnapshot(`
+      Object {
+        "informational": 1,
+        "low": 6, 
+        "medium": 4,
+      }
+    `);
+  },
+  6 * 60 * 1000
+);
