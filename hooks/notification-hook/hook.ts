dca33ef12bb66662961568587b20bc52452c3c23
Sven Strittmatter <sven.strittmatter@iteratec.com>
2021-05-07 11:30:35 +0200

    Use SPDX compliant license headers
    This patch changes the origin full text license headers with the
    short SPDX tag form.
    
    The reason for that: It is standardized and can be automated with
    reuse tool.
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- hooks/notification-hook/hook.ts
+++ hooks/notification-hook/hook.ts
@@ -1,18 +1,6 @@
-/**
-Copyright 2020 iteratec GmbH
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
+// SPDX-FileCopyrightText: 2020 iteratec GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
 import { isMatch } from "lodash";
 import { Finding } from "./model/Finding";
 import { NotificationChannel } from "./model/NotificationChannel";
@@ -38,8 +26,8 @@ export async function handle({ getFindings, scan }) {
 
 /**
  * Checks if a finding is matching a set of rules
- * @param finding 
- * @param rules 
+ * @param finding
+ * @param rules
  * @returns False if not matching, True if no rules specified or matching
  */
 export function matches(finding: Finding, rules: any[]): boolean {
