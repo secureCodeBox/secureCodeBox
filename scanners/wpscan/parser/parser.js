2c9d0683f97ebb799d0f44a9c90b8975e0e6d3a4
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2021-05-17 16:27:36 +0200

    Auto format

--- scanners/wpscan/parser/parser.js
+++ scanners/wpscan/parser/parser.js
@@ -2,14 +2,13 @@
  * Convert the WPScan file / json into secureCodeBox Findings
  */
 async function parse(scanResults) {
+  const wpscanVersion = scanResults.banner.version;
+  const wpscanRequestsDone = scanResults.requests_done;
 
-  const wpscanVersion       = scanResults.banner.version;
-  const wpscanRequestsDone  = scanResults.requests_done;
-  
   const targetUrl = scanResults.target_url;
-  const targetIp  = scanResults.target_ip;
+  const targetIp = scanResults.target_ip;
 
-  const wp            = scanResults.version
+  const wp = scanResults.version;
 
   const findings = [];
 
@@ -30,19 +29,19 @@ async function parse(scanResults) {
       wpscan_requests: wpscanRequestsDone,
       wp_version: wp.number,
       wp_release_date: wp.release_date,
-      wp_release_status: wp.status, 
+      wp_release_status: wp.status,
       wp_interesting_entries: wp.interesting_entries,
       wp_found_by: wp.found_by,
       wp_confirmed_by: wp.confirmed_by,
       wp_vulnerabilities: wp.vulnerabilities
-    },
+    }
   });
 
   // add all interesting findings as INFORMATIONAL
   for (const interestingFinding of scanResults.interesting_findings) {
     //console.log(interestingFinding);
     findings.push({
-      name: "WordPress finding '"+ interestingFinding.type + "'",
+      name: "WordPress finding '" + interestingFinding.type + "'",
       description: interestingFinding.to_s,
       category: "WordPress " + interestingFinding.type,
       location: interestingFinding.url,
@@ -51,11 +50,11 @@ async function parse(scanResults) {
       confidence: interestingFinding.confidence,
       reference: {},
       attributes: {
-	hostname: targetUrl,
+        hostname: targetUrl,
         wp_interesting_entries: interestingFinding.interesting_entries,
         wp_found_by: interestingFinding.found_by,
         wp_confirmed_by: interestingFinding.confirmed_by
-      },
+      }
     });
   }
 
