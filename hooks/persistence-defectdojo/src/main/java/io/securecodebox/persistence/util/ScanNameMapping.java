95e5688bdd595b7ee2b9afb5a39547b399ce5e69
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-05-22 12:35:46 +0200

    Merged and fixed DD ScanType Mapping

--- hooks/persistence-defectdojo/src/main/java/io/securecodebox/persistence/util/ScanNameMapping.java
+++ hooks/persistence-defectdojo/src/main/java/io/securecodebox/persistence/util/ScanNameMapping.java
@@ -21,17 +21,16 @@ import io.securecodebox.persistence.defectdojo.ScanType;
 import lombok.NonNull;
 
 public enum ScanNameMapping {
-  NMAP("nmap", ScanType.NMAP_SCAN),
+  NMAP("nmap", ScanType.NMAP_XML_SCAN),
   ZAP_BASELINE("zap-baseline-scan", ScanType.ZAP_SCAN),
   ZAP_API_SCAN("zap-api-scan", ScanType.ZAP_SCAN),
   ZAP_FULL_SCAN("zap-full-scan", ScanType.ZAP_SCAN),
-  ZAP_EXTENDED_BASELINE("zap-advanced-scan", ScanType.ZAP_SCAN),
-  SSLYZE("sslyze", ScanType.SS_LYZE_3_SCAN_JSON),
+  ZAP_ADVANCED_SCAN("zap-advanced-scan", ScanType.ZAP_SCAN),
+  SSLYZE("sslyze", ScanType.SSLYZE_3_JSON_SCAN),
   TRIVY("trivy", ScanType.TRIVY_SCAN),
   GITLEAKS("gitleaks", ScanType.GITLEAKS_SCAN),
-  // WPSCAN("wpscan", ScanType.WPSCAN),
-  // NIKTO("nikto", ScanType.NIKTO_SCAN),
-  // SSH("ssh-scan, ScanType.?),
+  NIKTO("nikto", ScanType.NIKTO_SCAN), 
+  GENERIC(null, ScanType.SECURECODEBOX_FINDINGS_IMPORT)
   ;
 
   /**
