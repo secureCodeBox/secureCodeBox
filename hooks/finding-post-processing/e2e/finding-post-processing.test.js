4abb7fb6c5f74f57941f67a74e76073bd6e44528
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2021-06-14 15:11:07 +0200

    Add Makefile POC For finding-post-processing hook
    Signed-off-by: Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>

---
+++ hooks/finding-post-processing/e2e/finding-post-processing.test.js
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2020 iteratec GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
+
+const { scan } = require("./helpers");
+
+jest.retryTimes(3);
+
+test(
+  "Finding Post Processing after test-scan",
+  async () => {
+    const { severities, count } = await scan(
+      "finding-post-processing",
+      "test-scan",
+      [],
+      90
+    );
+
+    expect(count).toBe(2);
+    expect(severities.high).toBe(1);
+  },
+  3 * 60 * 1000
+);
