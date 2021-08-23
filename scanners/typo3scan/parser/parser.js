0eb1ef005dd5c45f692e03cf5e5023e0f2cf052c
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2021-08-23 16:05:55 +0200

    Fixed parser.js and added unit tests
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- scanners/typo3scan/parser/parser.js
+++ scanners/typo3scan/parser/parser.js
@@ -4,65 +4,61 @@
 
 async function parse(fileContent) {
     let findings = JSON.parse(fileContent)
+    let results = ''
 
-    if (typeof fileContent === "string") {
-        findings = fileContent
-            .split("\n")
-            .map((vulnJson) => JSON.parse(vulnJson));
-    }
-    findings = findings[0] // Trimming the begining of the JS Object
-    let domain = Object.keys(findings)[0]; // Getting the first key (and the only key)
+    Object.keys(findings).forEach(key => {
+        let domain = key;
+        let domain_findings = findings[domain]
+        let vulns = domain_findings.Vulnerabilities
 
-    let parsed_findings = findings[domain]
+        // Parsing Vulnerabilities
+        let parsed_vulnerabilities = vulns.map((vuln) => {
+            return {
+                name: vuln.Type,
+                description: `Vulnerability of type ${vuln.Type} found`,
+                category: "Vulnerability",
+                location: domain,
+                osi_layer: "APPLICATION",
+                severity: "HIGH",
+                attributes: {
+                    typo3_version: domain_findings.Version,
+                    advisory: vuln.Advisory,
+                    subcomponent: vuln.Subcomponent,
+                    versions_affected: vuln.Affected,
+                    advisory_url: vuln['Advisory URL'],
 
-    // Parsing Vulnerabilities
-    let vulns = parsed_findings.Vulnerabilities
-    let parsed_vulnerabilities = vulns.map((vuln) => {
-        return {
-            name: vuln.Type,
-            description: `Vulnerability of type ${vuln.Type} found`,
-            category: "Vulnerability",
-            location: domain,
-            osi_layer: "APPLICATION",
-            severity: "HIGH",
-            attributes: {
-                Typo3_Version: parsed_findings.Version,
-                Advisory: vuln.Advisory,
-                Subcomponent: vuln.Subcomponent,
-                Versions_affected: vuln.Affected,
-                Advisory_URL: vuln['Advisory URL'],
+                }
+            };
+        });
+        // Parsing Extenstions
+        let extensions = domain_findings.Extensions
+        let parsed_extensions = extensions.map((ext) => {
 
+            // Check if extension has vulnerabilities : if yes severity = HIGH
+            let severity = 'INFORMATIONAL'
+            if (ext.Vulnerabilities.length > 0) {
+                severity = 'HIGH'
             }
-        };
+            return {
+                name: ext.Name,
+                description: `Extension ${ext.Name} (${ext.Title}) found`,
+                category: "Extension",
+                location: ext.Url,
+                osi_layer: "APPLICATION",
+                severity: severity,
+                attributes: {
+                    typo3_version: domain_findings.Version,
+                    repository: ext.Repo,
+                    extension_Version: ext.Version,
+                    version_file: ext['Version File'],
+                    vulnerabilities: ext.Vulnerabilities
+                }
+            };
+        });
+
+        results = parsed_vulnerabilities.concat(parsed_extensions)
+
     });
-
-    // Parsing Extenstions
-    let extensions = parsed_findings.Extensions
-    let parsed_extensions = extensions.map((ext) => {
-
-        // Check if extension has vulnerabilities : if yes severity = HIGH
-        let severity = 'INFORMATIONAL'
-        if ( ext.Vulnerabilities.length > 0) {
-            severity = 'HIGH'
-        }
-        return {
-            name: ext.Name,
-            description: `Extension ${ext.Name} (${ext.Title}) found`,
-            category: "Extension",
-            location: ext.Url,
-            osi_layer: "APPLICATION",
-            severity: severity,
-            attributes: {
-                Typo3_Version: parsed_findings.Version,
-                Repository: ext.Repo,
-                Extension_Version: ext.Version,
-                Version_File: ext['Version File'],
-                Vulnerabilities: ext.Vulnerabilities
-            }
-        };
-    });
-
-    let results = parsed_vulnerabilities.concat(parsed_extensions)
     return results
 }
 module.exports.parse = parse;
--- scanners/typo3scan/parser/parser.test.js
+++ scanners/typo3scan/parser/parser.test.js
@@ -10,29 +10,31 @@ const readFile = util.promisify(fs.readFile);
 const { parse } = require("./parser");
 
 const {
-    validateParser,
+  validateParser,
 } = require("@securecodebox/parser-sdk-nodejs/parser-utils");
 
-test("example parser parses empty json files to zero findings", async () => {
-    const fileContent = await readFile(__dirname + "/__testFiles__/empty.jsonl", {
-        encoding: "utf8",
-    });
+test("parser parses large json result without vulnuerable extensions successfully", async () => {
+  const fileContent = await readFile(
+    __dirname + "/__testFiles__/localhost.json",
+    {
+      encoding: "utf8",
+    }
+  );
 
-    const findings = await parse(fileContent);
-    await expect(validateParser(findings)).resolves.toBeUndefined();
-    expect(findings).toEqual([]);
+  const findings = await parse(fileContent);
+  await expect(validateParser(findings)).resolves.toBeUndefined();
+  expect(findings).toMatchSnapshot();
 });
 
-test("example parser parses large json result successfully", async () => {
-    const fileContent = await readFile(
-      __dirname + "/__testFiles__/localhost.jsonl",
-      {
-        encoding: "utf8",
-      }
-    );
-  
-    const findings = await parse(fileContent);
-    await expect(validateParser(findings)).resolves.toBeUndefined();
-    expect(findings).toMatchSnapshot();
-  });
-  // TODO how to run these tests locally ("npx jest parser.test.js" ?? )
+test("parser parses large json result with vulnuerable extensions successfully", async () => {
+  const fileContent = await readFile(
+    __dirname + "/__testFiles__/localhost_vuln_extensions.json",
+    {
+      encoding: "utf8",
+    }
+  );
+
+  const findings = await parse(fileContent);
+  await expect(validateParser(findings)).resolves.toBeUndefined();
+  expect(findings).toMatchSnapshot();
+});
