898bfab199b073221bc1967a8391e48cb1644fd4
Max Maass <max.maass@iteratec.com>
2021-10-13 17:48:01 +0200

    First version of parser + unit tests
    Signed-off-by: Max Maass <max.maass@iteratec.com>

--- scanners/semgrep/parser/parser.js
+++ scanners/semgrep/parser/parser.js
@@ -8,7 +8,7 @@ const severityMap = new Map([
   ["error", "HIGH"]
 ])
 async function parse(fileContent) {
-  const results = fileContent;
+  const results = JSON.parse(fileContent);
   return results.results.flatMap(result => {
     // Assemble location as path to file and line range
     const location = result.path + ":" + result.start.line + "-" + result.end.line;
@@ -20,30 +20,35 @@ async function parse(fileContent) {
     const description = result.extra.message || "(No description provided in semgrep rule - when using a custom rule, set the 'message' key)"
     
     // Category of the finding - use either result.extra.metadata.category, or a placeholder
-    // TODO: What is a good placeholder if none is set in the rule? null / empty string / "semgrep"?
-    const category = result.extra.metadata.category || null
+    const category = result.extra.metadata.category || "semgrep-result"
 
     // severity of the issue: translate semgrep severity levels (INFO, WARNING, ERROR) to those of SCB (INFORMATIONAL, LOW, MEDIUM, HIGH)
-    // TODO: What to return if the severity is not set, or not set to a valid value? null / empty string / ...?
-    const severity = severityMap.has(result.extra.severity.toLowerCase()) ? severityMap.get(result.extra.severity.toLowerCase()) : null
+    const severity = severityMap.has(result.extra.severity.toLowerCase()) ? severityMap.get(result.extra.severity.toLowerCase()) : "INFORMATIONAL"
 
     const attributes = {
+      // Common weakness enumeration, if available
+      "cwe": result.extra.metadata.cwe || null,
+      // OWASP category, if available
+      "owasp_category": result.extra.metadata.owasp || null,
+      // References given in the rule
+      "references": result.extra.metadata.references || null,
+      // Link to the semgrep rule
+      "rule_source": result.extra.metadata.source || null,
+      // Which line of code matched?
+      // TODO: Do we actually want to record this? There are also secret-detector rules for semgrep, 
+      // so maybe you don't actually want the plaintext match to be recorded unencrypted in some S3 bucket?
+      "matching_lines": result.extra.lines,
     }
+
     return {
       "name": name,
       "location": location,
       "description": description,
       "category": category,
       "severity": severity,
+      "attributes": attributes
     }
   })
 }
 
 module.exports.parse = parse;
-// Debugging:
-const buffer = require("./__testFiles__/minimal-metadata.json");
-parse(buffer).then((result) => {console.log(JSON.stringify(result, null, 2))});
-
-
-// Semgrep: INFO / WARNING / ERROR
-// SCB: INFORMATIONAL / LOW / MEDIUM / HIGH
