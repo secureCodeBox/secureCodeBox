21148e104ab655339cd50a0361ed876df2bc5249
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-02-01 21:37:35 +0100

    #754 Extract thew hole generation code into a shell script
    Because it is shell script anyway and editing shell script in
    Makefiles is tedious I decided to extract the code into a separate
    shell script. So we can invoke this from the make targets.
    
    AFAICS there is lots of duplicatedcode for hooks and parser. Seems
    to be nearly the same as for the scanners. So we may reuse thisscript.
    
    This will elad into a cleaner Makefile and less code to maintain.
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

---
+++ bin/generate-scanner-docs.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+USAGE="$(basename "${0}") path/to/scanner/Chart.yaml path/to/.helm-docs"
+CHART_FILE="${1}"
+HELM_DOCS_DIR="${2}"
+
+if [[ -z "${CHART_FILE}" ]]; then
+  echo >&2 "No chart file file given as first parameter!"
+  echo >&2 "${USAGE}"
+  exit 1
+fi
+
+if [[ -z "${HELM_DOCS_DIR}" ]]; then
+  echo >&2 "No helm docs dir given as second parameter!"
+  echo >&2 "${USAGE}"
+  exit 1
+fi
+
+function generate_docs() {
+  local output_file base_template docs_template dockerhub_template
+  output_file="${1}"
+  base_template="${2}"
+  docs_template="${3}"
+  dockerhub_template="${4}"
+  helm-docs --template-files="${base_template}" \
+          --template-files="${docs_template}" \
+          --template-files="${dockerhub_template}" \
+          --output-file="${output_file}"
+}
+
+function main() {
+  echo "Generating docs for ${CHART_FILE}..."
+
+  local dir docs_dir parser_dir scanner_dir
+
+  dir="$(dirname "${CHART_FILE}")"
+  docs_dir="${dir}/docs"
+  parser_dir="${dir}/parser"
+  scanner_dir="${dir}/scanner"
+
+  if [ -d "${docs_dir}" ]; then
+    echo "Docs Folder found at: ${docs_dir}"
+
+    if [ -d "${parser_dir}" ]; then
+      echo "Parser found at: ${parser_dir}"
+
+      cd "${dir}" && generate_docs "${docs_dir}/README.DockerHub-Parser.md" \
+        "${HELM_DOCS_DIR}/templates.gotmpl" \
+        "${dir}/.helm-docs.gotmpl" \
+        "${HELM_DOCS_DIR}/README.DockerHub-Parser.md.gotmpl"
+    else
+      echo "No parser found!"
+    fi
+
+    if [ -d "${scanner_dir}" ]; then
+      echo "Scanner found at: ${scanner_dir}"
+
+      cd "${dir}" && generate_docs "${docs_dir}/README.DockerHub-Scanner.md" \
+        "${HELM_DOCS_DIR}/templates.gotmpl" \
+        "${dir}/.helm-docs.gotmpl" \
+        "${HELM_DOCS_DIR}/README.DockerHub-Scanner.md.gotmpl"
+    else
+      echo "No scanner found!"
+    fi
+
+    cd "${dir}" && generate_docs "${docs_dir}/README.ArtifactHub.md" \
+      "${HELM_DOCS_DIR}/templates.gotmpl" \
+      "${dir}/.helm-docs.gotmpl" \
+      "${HELM_DOCS_DIR}/README.ArtifactHub.md.gotmpl" \
+
+  else
+    echo "Ignoring docs creation process for ${CHART_FILE} because docs folder found at: ${docs_dir}"
+  fi
+}
+
+main
