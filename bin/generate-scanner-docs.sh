6ed965e87e95c2c57a74b66cc3faa87479eaed6d
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-02-02 13:00:00 +0100

    #754 Add chart search root as parameter
    So we do not need to cd around.
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- bin/generate-scanner-docs.sh
+++ bin/generate-scanner-docs.sh
@@ -32,15 +32,20 @@ if [[ -z "${HELM_DOCS_DIR}" ]]; then
 fi
 
 function generate_docs() {
-  local output_file base_template docs_template dockerhub_template
-  output_file="${1}"
-  base_template="${2}"
-  docs_template="${3}"
-  dockerhub_template="${4}"
-  helm-docs --template-files="${base_template}" \
-          --template-files="${docs_template}" \
-          --template-files="${dockerhub_template}" \
-          --output-file="${output_file}"
+  local chart_search_root output_file base_template docs_template dockerhub_template
+
+  chart_search_root="${1}"
+  output_file="${2}"
+  base_template="${3}"
+  docs_template="${4}"
+  dockerhub_template="${5}"
+
+  helm-docs --log-level debug \
+    --chart-search-root="${chart_search_root}" \
+    --output-file="${output_file}" \
+    --template-files="${base_template}" \
+    --template-files="${docs_template}" \
+    --template-files="${dockerhub_template}"
 }
 
 function main() {
@@ -61,7 +66,8 @@ function main() {
   if [ -d "${parser_dir}" ]; then
     log "Parser found at: '${parser_dir}'. Generating parser doc..."
 
-    cd "${dir}" && generate_docs "${docs_dir}/README.DockerHub-Parser.md" \
+    generate_docs "${scanner_dir}" \
+      "${docs_dir}/README.DockerHub-Parser.md" \
       "${HELM_DOCS_DIR}/templates.gotmpl" \
       "${scanner_dir}/.helm-docs.gotmpl" \
       "${HELM_DOCS_DIR}/README.DockerHub-Parser.md.gotmpl"
@@ -72,7 +78,8 @@ function main() {
   if [ -d "${sub_scanner_dir}" ]; then
     log "Scanner found at: '${sub_scanner_dir}'. Generating scanner doc..."
 
-    cd "${dir}" && generate_docs "${docs_dir}/README.DockerHub-Scanner.md" \
+    generate_docs "${scanner_dir}" \
+      "${docs_dir}/README.DockerHub-Scanner.md" \
       "${HELM_DOCS_DIR}/templates.gotmpl" \
       "${scanner_dir}/.helm-docs.gotmpl" \
       "${HELM_DOCS_DIR}/README.DockerHub-Scanner.md.gotmpl"
@@ -80,7 +87,9 @@ function main() {
     log "No scanner found at '${sub_scanner_dir}'! Skipping scanner doc."
   fi
 
-  cd "${dir}" && generate_docs "${docs_dir}/README.ArtifactHub.md" \
+  log "Generating main doc..."
+  generate_docs "${scanner_dir}" \
+    "${docs_dir}/README.ArtifactHub.md" \
     "${HELM_DOCS_DIR}/templates.gotmpl" \
     "${scanner_dir}/.helm-docs.gotmpl" \
     "${HELM_DOCS_DIR}/README.ArtifactHub.md.gotmpl"
