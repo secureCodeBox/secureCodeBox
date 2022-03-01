1e74ed6038b0fbe602fbdfa4054d9a7919ccb7d8
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2022-03-01 17:05:17 +0100

    Added Operator test to check that findings get updated
    Findings summary should be updated for the scheduled scan
    when a child scan is triggered.
    
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- operator/controllers/execution/scheduledscan_controller_test.go
+++ operator/controllers/execution/scheduledscan_controller_test.go
@@ -5,59 +5,56 @@
 package controllers
 
 import (
-	"fmt"
-	"testing"
+	"context"
+	"reflect"
 
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	executionv1 "github.com/secureCodeBox/secureCodeBox/operator/apis/execution/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	//+kubebuilder:scaffold:imports
 )
 
-type testData struct {
-	in                   map[string]string
-	expectedMapKeyLength int
-}
+var _ = Describe("Scheduledscan controller", func() {
+	Context("If multiple scheduled scans triggers", func() {
+		It("Update Finding Summary of scan with the results of the latest successful Scan", func() {
+			ctx := context.Background()
+			namespace := "scantype-multiple-scheduled-scan-triggerd-test"
 
-// Tests that getAnnotationsForScan drops all annotations not prefixed with "*.securecodebox.io/*"
-func TestGetAnnotationsForScan(t *testing.T) {
-	tests := []testData{
-		{
-			in: map[string]string{
-				"foobar": "bar",
-			},
-			expectedMapKeyLength: 0,
-		},
-		{
-			in: map[string]string{
-				"foobar.securecodebox.io/bar": "bar",
-			},
-			expectedMapKeyLength: 1,
-		},
-		{
-			in: map[string]string{
-				"barfoo.securecodebox.io/bar": "bar",
-				"foo":                         "bar",
-			},
-			expectedMapKeyLength: 1,
-		},
-		{
-			in: map[string]string{
-				"barfoo.securecodebox.io/bar": "bar",
-				"barfoo.securecodebox.io/foo": "bar",
-			},
-			expectedMapKeyLength: 2,
-		},
-	}
+			createNamespace(ctx, namespace)
+			createScanType(ctx, namespace)
+			scheduledScan := createScheduledScan(ctx, namespace, true)
 
-	for _, test := range tests {
-		scheduledScan := executionv1.ScheduledScan{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:        "foobar",
-				Annotations: test.in,
-			},
-		}
-		actual := getAnnotationsForScan(scheduledScan)
-		if len(actual) != test.expectedMapKeyLength {
-			t.Error(fmt.Errorf("getAnnotationsForScan should only copy over annotations following the pattern '*.securecodebox.io', but map: %v returned a map with %d keys (%d expected)", test.in, len(actual), test.expectedMapKeyLength))
-		}
-	}
-}
+			var scanlist executionv1.ScanList
+			// ensure that the ScheduledScan has been triggered
+			waitForScheduledScanToBeTriggered(ctx, namespace)
+			k8sClient.List(ctx, &scanlist, client.InNamespace(namespace))
+
+			Expect(scanlist.Items).Should(HaveLen(1))
+
+			scan := scanlist.Items[0]
+			scan.Status.State = "Done"
+
+			scan.Status.Findings = executionv1.FindingStats{
+				Count:             42,
+				FindingSeverities: executionv1.FindingSeverities{High: 42},
+				FindingCategories: map[string]uint64{"Open Port": 42},
+			}
+
+			k8sClient.Status().Update(ctx, &scan)
+
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: "test-scan", Namespace: namespace}, &scheduledScan)
+				if errors.IsNotFound(err) {
+					panic("ScheduledScan should be present for this check!")
+				}
+				return (scheduledScan.Status.Findings.Count == 42 &&
+					scheduledScan.Status.Findings.FindingSeverities == executionv1.FindingSeverities{High: 42} &&
+					reflect.DeepEqual(scheduledScan.Status.Findings.FindingCategories, map[string]uint64{"Open Port": 42}))
+			}, timeout, interval).Should(BeTrue())
+
+		})
+	})
+})
