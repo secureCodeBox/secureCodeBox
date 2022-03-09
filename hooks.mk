13fedab81599d46ef67874553ed700b304b3b7cd
Rami Souai <rami.souai@iteratec.com>
2022-03-09 18:10:56 +0100

    Unify make integration-tests for hooks to avoid inheriting make commands
    Signed-off-by: Rami Souai <rami.souai@iteratec.com>

--- hooks.mk
+++ hooks.mk
@@ -42,4 +42,4 @@ deploy:
 integration-tests:
 	@echo ".: 🩺 Starting integration test in kind namespace 'integration-tests'."
 	kubectl -n integration-tests delete scans --all
-	cd .. && npm ci && cd $(name)/${hook-prefix} && npx --yes --package jest@$(JEST_VERSION) jest --verbose --ci --colors --coverage --passWithNoTests *.test.*
+	cd .. && npm ci && cd $(name)/${hook-prefix} && npm run test --package jest@$(JEST_VERSION)
