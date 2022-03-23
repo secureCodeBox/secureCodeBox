226b700817401a23c1f1b1702df0868b52bb404d
Robert Seedorff <Robert.Seedorff@iteratec.com>
2022-03-23 20:15:10 +0100

    Added new integration-tests for new scanTypes

--- scanners/trivy/integration-tests/trivy.test.js
+++ scanners/trivy/integration-tests/trivy.test.js
@@ -7,10 +7,10 @@ const {scan} = require("../../helpers");
 jest.retryTimes(3);
 
 test(
-  "trivy scans vulnerable juiceshop demo target",
+  "trivy image scan for a vulnerable juiceshop demo target",
   async () => {
     const { categories, severities, count } = await scan(
-      "trivy-juice-shop",
+      "trivy-juice-test",
       "trivy-image",
       ["bkimminich/juice-shop:v10.2.0"],
       90
@@ -22,6 +22,64 @@ test(
     expect(severities["high"]).toBeGreaterThanOrEqual(20);
     expect(severities["medium"]).toBeGreaterThanOrEqual(10);
     expect(severities["low"]).toBeGreaterThanOrEqual(1);
+  },
+  3 * 60 * 1000
+);
+
+test(
+  "trivy filesystem scan with exiting files should not fail",
+  async () => {
+    const { categories, severities, count } = await scan(
+      "trivy-filesystem-test",
+      "trivy-filesystem",
+      ["/repo/"],
+      90,
+      // volumes
+      [{
+        "name": "test-dir",
+        "emptyDir": {}
+      }],
+      // volumeMounts
+      [{
+          "mountPath": "/repo/",
+          "name": "test-dir"
+      }],
+      // initContainers
+      [{
+          "name": "init-git",
+          "image": "bitnami/git",
+          "command": ["bash", 
+                      "-c", 
+                      // Bash script to create a git repo with a demo file
+                      `cd /repo && \\
+                      git clone https://github.com/knqyf263/trivy-ci-test`],
+          "volumeMounts": [{
+              "mountPath": "/repo/",
+              "name": "test-dir"
+          }]
+      }]
+    );
+
+    expect(count).toBeGreaterThanOrEqual(9);
+    expect(severities["high"]).toBeGreaterThanOrEqual(2);
+    expect(severities["medium"]).toBeGreaterThanOrEqual(1);
+  },
+  3 * 60 * 1000
+);
+
+test(
+  "trivy repo scan with exiting repo should not fail",
+  async () => {
+    const { categories, severities, count } = await scan(
+      "trivy-repo-test",
+      "trivy-repo",
+      ["https://github.com/knqyf263/trivy-ci-test"],
+      90
+    );
+
+    expect(count).toBeGreaterThanOrEqual(9);
+    expect(severities["high"]).toBeGreaterThanOrEqual(2);
+    expect(severities["medium"]).toBeGreaterThanOrEqual(1);
   },
   3 * 60 * 1000
 );
