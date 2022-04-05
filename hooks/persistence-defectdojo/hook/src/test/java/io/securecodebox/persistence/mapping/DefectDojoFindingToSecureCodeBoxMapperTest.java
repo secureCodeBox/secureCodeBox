2599d46d1029476a176b6e92f87abdb5c926cfe2
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2022-04-05 17:34:11 +0200

    Embed original DefectDojo finding in synced back findings
    See #1055
    
    This might be extended / improved in future by reducing the number of api requests performed by this feature. At the moment the hook needs to do 1 api request for every duplicate findings. The DefectDojo API would already support to prefetch duplicate findings in the list api request but this is not yet supported by our DefectDojo Java API client. Also might be pretty tricky to do with the current way the client is written.
    
    Co-authored-by: Max Maass <max.maass@iteratec.com>
    Co-authored-by: Sebastian Franz <sebastian.franz@iteratec.com>
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@iteratec.com>

--- hooks/persistence-defectdojo/hook/src/test/java/io/securecodebox/persistence/mapping/DefectDojoFindingToSecureCodeBoxMapperTest.java
+++ hooks/persistence-defectdojo/hook/src/test/java/io/securecodebox/persistence/mapping/DefectDojoFindingToSecureCodeBoxMapperTest.java
@@ -8,7 +8,9 @@ import io.securecodebox.persistence.defectdojo.config.DefectDojoConfig;
 import io.securecodebox.persistence.defectdojo.models.Endpoint;
 import io.securecodebox.persistence.defectdojo.models.Finding;
 import io.securecodebox.persistence.defectdojo.service.EndpointService;
+import io.securecodebox.persistence.defectdojo.service.FindingService;
 import io.securecodebox.persistence.models.SecureCodeBoxFinding;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,16 +33,17 @@ class DefectDojoFindingToSecureCodeBoxMapperTest {
   @Mock
   EndpointService endpointService;
 
+  @Mock
+  FindingService findingService;
+
+  Finding exampleFinding;
+
   @BeforeEach
   public void setup(){
     var config = new DefectDojoConfig("http://example.defectdojo.com", "placeholder", "placeholder", 1000);
-    this.mapper = new DefectDojoFindingToSecureCodeBoxMapper(config, endpointService);
-  }
+    this.mapper = new DefectDojoFindingToSecureCodeBoxMapper(config, endpointService, findingService);
 
-  @Test
-  public void shouldMapBasicFindings(){
-    // Typical ZAP Finding in DefectDojo
-    var ddFinding = Finding.builder()
+    this.exampleFinding = Finding.builder()
       .title("Content Security Policy (CSP) Header Not Set")
       .severity(Finding.Severity.Medium)
       .description("Content Security Policy (CSP) is an added layer of security that helps to detect and mitigate certain types of attacks, including Cross Site Scripting (XSS) and data injection attacks. These attacks are used for everything from data theft to site defacement or distribution of malware. CSP provides a set of standard HTTP headers that allow website owners to declare approved sources of content that browsers should be allowed to load on that page â covered types are JavaScript, CSS, HTML frames, fonts, images and embeddable objects such as Java applets, ActiveX, audio and video files.\n\nReference: https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Introducing_Content_Security_Policyhttps://cheatsheetseries.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.htmlhttp://www.w3.org/TR/CSP/http://w3c.github.io/webappsec/specs/content-security-policy/csp-specification.dev.htmlhttp://www.html5rocks.com/en/tutorials/security/content-security-policy/http://caniuse.com/#feat=contentsecuritypolicyhttp://content-security-policy.com/\n\nURL: http://juice-shop.securecodebox-test.svc:3000/sitemap.xml\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/coupons_2013.md.bak\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/encrypt.pyc\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/suspicious_errors.yml\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/package.json.bak\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/eastere.gg\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/quarantine\nMethod: GET\n")
@@ -52,6 +55,12 @@ class DefectDojoFindingToSecureCodeBoxMapperTest {
       .test(42L)
       .impact("No impact provided")
       .build();
+  }
+
+  @Test
+  public void shouldMapBasicFindings(){
+    // Typical ZAP Finding in DefectDojo
+    var ddFinding = exampleFinding;
 
     when(endpointService.get(1337L)).thenReturn(Endpoint.builder().protocol("http").host("juice-shop.securecodebox-test.svc:3000").build());
 
@@ -80,6 +89,82 @@ class DefectDojoFindingToSecureCodeBoxMapperTest {
     assertEquals(
       "2020-04-15T20:08:18Z",
       actualFinding.getParsedAt()
+    );
+  }
+
+  @Test
+  public void shouldIncludeOriginalDuplicateFindingInAttributes(){
+    // Typical ZAP Finding in DefectDojo
+    var ddFinding = exampleFinding;
+
+    ddFinding.setDuplicate(true);
+    ddFinding.setDuplicateFinding(7L);
+
+    var originalFinding = Finding.builder()
+      .id(7L)
+      .title("Content Security Policy (CSP) Header Not Set")
+      .severity(Finding.Severity.Medium)
+      .description("Content Security Policy (CSP) is an added layer of security that helps to detect and mitigate certain types of attacks, including Cross Site Scripting (XSS) and data injection attacks. These attacks are used for everything from data theft to site defacement or distribution of malware. CSP provides a set of standard HTTP headers that allow website owners to declare approved sources of content that browsers should be allowed to load on that page â covered types are JavaScript, CSS, HTML frames, fonts, images and embeddable objects such as Java applets, ActiveX, audio and video files.\n\nReference: https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Introducing_Content_Security_Policyhttps://cheatsheetseries.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.htmlhttp://www.w3.org/TR/CSP/http://w3c.github.io/webappsec/specs/content-security-policy/csp-specification.dev.htmlhttp://www.html5rocks.com/en/tutorials/security/content-security-policy/http://caniuse.com/#feat=contentsecuritypolicyhttp://content-security-policy.com/\n\nURL: http://juice-shop.securecodebox-test.svc:3000/sitemap.xml\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/coupons_2013.md.bak\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/encrypt.pyc\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/suspicious_errors.yml\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/package.json.bak\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/eastere.gg\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/quarantine\nMethod: GET\n")
+      .mitigation("Ensure that your web server, application server, load balancer, etc. is configured to set the Content-Security-Policy header, to achieve optimal browser support: \"Content-Security-Policy\" for Chrome 25+, Firefox 23+ and Safari 7+, \"X-Content-Security-Policy\" for Firefox 4.0+ and Internet Explorer 10+, and \"X-WebKit-CSP\" for Chrome 14+ and Safari 6+.")
+      // Random ids...
+      .createdAt(LocalDateTime.ofInstant(Instant.parse("2020-02-15T20:08:18.000Z"), ZoneId.systemDefault()))
+      .endpoints(List.of(1337L, 42L, 3L))
+      .foundBy(List.of(3L))
+      .test(40L)
+      .impact("No impact provided")
+      .build();
+
+    when(endpointService.get(1337L)).thenReturn(Endpoint.builder().protocol("http").host("juice-shop.securecodebox-test.svc:3000").build());
+    when(findingService.get(7L)).thenReturn(originalFinding);
+
+    var actualFinding = this.mapper.fromDefectDojoFinding(ddFinding);
+
+    assertEquals(
+      7L,
+      actualFinding.getAttributes().get("defectdojo.org/original-finding-id")
+    );
+    assertEquals(
+      "Content Security Policy (CSP) Header Not Set",
+      ((SecureCodeBoxFinding) actualFinding.getAttributes().get("defectdojo.org/original-finding")).getName()
+    );
+    assertNotNull(
+      actualFinding.getAttributes().get("defectdojo.org/original-finding")
+    );
+  }
+
+  @Test
+  public void shouldNotBeStuckInARecursiveLoop(){
+    // Typical ZAP Finding in DefectDojo
+    var ddFinding = exampleFinding;
+
+    ddFinding.setDuplicate(true);
+    ddFinding.setDuplicateFinding(7L);
+
+    var originalFinding = Finding.builder()
+      .id(7L)
+      .title("Content Security Policy (CSP) Header Not Set")
+      .severity(Finding.Severity.Medium)
+      .description("Content Security Policy (CSP) is an added layer of security that helps to detect and mitigate certain types of attacks, including Cross Site Scripting (XSS) and data injection attacks. These attacks are used for everything from data theft to site defacement or distribution of malware. CSP provides a set of standard HTTP headers that allow website owners to declare approved sources of content that browsers should be allowed to load on that page â covered types are JavaScript, CSS, HTML frames, fonts, images and embeddable objects such as Java applets, ActiveX, audio and video files.\n\nReference: https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Introducing_Content_Security_Policyhttps://cheatsheetseries.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.htmlhttp://www.w3.org/TR/CSP/http://w3c.github.io/webappsec/specs/content-security-policy/csp-specification.dev.htmlhttp://www.html5rocks.com/en/tutorials/security/content-security-policy/http://caniuse.com/#feat=contentsecuritypolicyhttp://content-security-policy.com/\n\nURL: http://juice-shop.securecodebox-test.svc:3000/sitemap.xml\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/coupons_2013.md.bak\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/encrypt.pyc\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/suspicious_errors.yml\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/package.json.bak\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/eastere.gg\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000\nMethod: GET\n\nURL: http://juice-shop.securecodebox-test.svc:3000/ftp/quarantine\nMethod: GET\n")
+      .mitigation("Ensure that your web server, application server, load balancer, etc. is configured to set the Content-Security-Policy header, to achieve optimal browser support: \"Content-Security-Policy\" for Chrome 25+, Firefox 23+ and Safari 7+, \"X-Content-Security-Policy\" for Firefox 4.0+ and Internet Explorer 10+, and \"X-WebKit-CSP\" for Chrome 14+ and Safari 6+.")
+      // Random ids...
+      .createdAt(LocalDateTime.ofInstant(Instant.parse("2020-02-15T20:08:18.000Z"), ZoneId.systemDefault()))
+      .endpoints(List.of(1337L, 42L, 3L))
+      .foundBy(List.of(3L))
+      .test(40L)
+      .impact("No impact provided")
+      .duplicate(true)
+      .duplicateFinding(2L)
+      .build();
+
+    when(findingService.get(7L)).thenReturn(originalFinding);
+
+    var exception = Assertions.assertThrows(RuntimeException.class, () -> {
+      this.mapper.fromDefectDojoFinding(ddFinding);
+    });
+
+    assertEquals(
+      "Duplicate finding does not point to the actual original finding, as the original finding (id: 7) is also a duplicate. This should never happen.",
+      exception.getMessage()
     );
   }
 
