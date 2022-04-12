60d02c28cba883077734e6466f339e3570010557
Mateusz Majcher <m.majcher91@gmail.com>
2022-04-12 15:35:17 +0200

    Authenticated generic webhook
    Signed-off-by: Mateusz Majcher <m.majcher91@gmail.com>

--- hooks/generic-webhook/hook/hook.js
+++ hooks/generic-webhook/hook/hook.js
@@ -6,12 +6,18 @@ async function handle({
   getFindings,
   scan,
   webhookUrl = process.env["WEBHOOK_URL"],
+  webhookUser = process.env["WEBHOOK_USER"],
+  webhookPassword = process.env["WEBHOOK_PASSWORD"],
   axios = require('axios')
 }) {
   const findings = await getFindings();
 
   console.log(`Sending ${findings.length} findings to ${webhookUrl}`);
 
-  await axios.post(webhookUrl, { scan, findings });
+  if (webhookUser && webhookPassword){
+    await axios.post(webhookUrl, {scan, findings }, {auth: {username: webhookUser, password: webhookPassword}});
+  }else{
+    await axios.post(webhookUrl, {scan, findings });
+  }
 }
 module.exports.handle = handle;
