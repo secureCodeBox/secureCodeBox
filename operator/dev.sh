cd57709c6e9af52e6d2d61ca16ac88394612ad8c
Max Maass <max.maass@iteratec.com>
2022-04-28 10:54:03 +0200

    Delete accidentally commited debugging script
    Signed-off-by: Max Maass <max.maass@iteratec.com>

--- operator/dev.sh
+++
@@ -1,7 +0,0 @@
-docker run --name $MINIO_DOCKER_NAME -d --rm -p $S3_ENDPOINT:9000 -p 127.0.0.1:9001:9001 -e MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY -e MINIO_SECRET_KEY=$MINIO_SECRET_KEY minio/minio server /data --console-address ":9001"
-sleep 5
-docker run --rm --link $MINIO_DOCKER_NAME:minio -e MINIO_BUCKET=$S3_BUCKET --entrypoint sh minio/mc -c "\
-  mc config host add myminio http://$S3_ENDPOINT \$MINIO_ENV_MINIO_ACCESS_KEY \$MINIO_ENV_MINIO_SECRET_KEY && \
-  sleep 5 && \
-  mc mb myminio/\$MINIO_BUCKET \
-  "
