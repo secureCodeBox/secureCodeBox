7914bc91cfd361257cb023704e7353999bdd960e
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2022-05-12 17:00:36 +0200

    Removed the nginx test instead of just commenting it out
    This keeps the visibilty of the code.
    This can simply later on be reverted if the nginx test proves to be necessary
    
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- scanners/zap-advanced/integration-tests/zap-advanced.test.js
+++ scanners/zap-advanced/integration-tests/zap-advanced.test.js
@@ -5,23 +5,7 @@
 const {scan} = require("../../helpers");
 
 jest.retryTimes(3);
-/*
-test(
-  "ZAP-advanced scan without config YAML against a plain 'nginx container' should only find couple findings",
-  async () => {
-    const { count } = await scan(
-      "zap-advanced-scan-nginx-demo",
-      "zap-advanced-scan",
-      ["-t", "http://nginx.demo-targets.svc"],
-      60 * 15
-    );
 
-    // There must be at least one finding
-    expect(count).toBeGreaterThanOrEqual(1);
-  },
-  60 * 16 * 1000
-);
-*/
 test(
   "ZAP-advanced scan without config YAML against 'bodgeit' container should only find couple findings",
   async () => {
