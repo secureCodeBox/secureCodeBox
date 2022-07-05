0070f04366ce65887d43e3db8f440a8e9c5d47e4
Simon Hülkenberg <simon.huelkenberg@iteratec.com>
2022-07-05 13:35:55 +0200

    Add exit code 4 to if
    Signed-off-by: Simon Hülkenberg <simon.huelkenberg@iteratec.com>

--- scanners/wpscan/scanner/wrapper.sh
+++ scanners/wpscan/scanner/wrapper.sh
@@ -7,10 +7,10 @@ wpscan_exit=$?
 echo "wpscan exited with $wpscan_exit"
 
 # wpscan returns a non zero exit code when it finds vulnerabilitys
-# exit codes 1, 2 and 3 are errors, 5 means vulnerabilitys were found
+# exit codes 1, 2 ,3 and 4 are errors, 5 means vulnerabilitys were found
 # see https://github.com/wpscanteam/CMSScanner/blob/master/lib/cms_scanner/exit_code.rb
 fake_exit_code=0
-if [[ $wpscan_exit -eq 1 ]] || [[ $wpscan_exit -eq 2 ]] || [[ $wpscan_exit -eq 3 ]]
+if [[ $wpscan_exit -eq 1 ]] || [[ $wpscan_exit -eq 2 ]] || [[ $wpscan_exit -eq 3 ]] || [[ $wpscan_exit -eq 4 ]]
 then
     fake_exit_code=$wpscan_exit
 fi
