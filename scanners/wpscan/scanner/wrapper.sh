63a85e86a7a44c06a7f3138015c0439698cf8867
Simon Hülkenberg <simon.huelkenberg@iteratec.com>
2022-07-05 13:10:15 +0200

    Add explanation for wrapper
    Signed-off-by: Simon Hülkenberg <simon.huelkenberg@iteratec.com>

--- scanners/wpscan/scanner/wrapper.sh
+++ scanners/wpscan/scanner/wrapper.sh
@@ -4,4 +4,7 @@
 
 wpscan $@
 echo "wpscan exited with $?"
+
+# wpscan returns a non zero exit code when it finds vulnerabilitys
+# see https://github.com/wpscanteam/CMSScanner/blob/master/lib/cms_scanner/exit_code.rb
 exit 0
