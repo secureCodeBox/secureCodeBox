526f6a6113dae391bd47a83f6f2ae20657d19b24
Simon Hülkenberg <simon.huelkenberg@iteratec.com>
2022-07-05 13:32:17 +0200

    Pass through exit code on error
    Signed-off-by: Simon Hülkenberg <simon.huelkenberg@iteratec.com>

--- scanners/wpscan/scanner/wrapper.sh
+++ scanners/wpscan/scanner/wrapper.sh
@@ -3,8 +3,16 @@
 # SPDX-License-Identifier: Apache-2.0
 
 wpscan $@
-echo "wpscan exited with $?"
+wpscan_exit=$?
+echo "wpscan exited with $wpscan_exit"
 
 # wpscan returns a non zero exit code when it finds vulnerabilitys
+# exit codes 1, 2 and 3 are errors, 5 means vulnerabilitys were found
 # see https://github.com/wpscanteam/CMSScanner/blob/master/lib/cms_scanner/exit_code.rb
-exit 0
+fake_exit_code=0
+if [[ $wpscan_exit -eq 1 ]] || [[ $wpscan_exit -eq 2 ]] || [[ $wpscan_exit -eq 3 ]]
+then
+    fake_exit_code=$wpscan_exit
+fi
+
+exit $fake_exit_code
