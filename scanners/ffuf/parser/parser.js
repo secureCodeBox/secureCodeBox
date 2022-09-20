6006bb3f542b9c906f4b29164ec532ba0580553d
Rebecca Falke <rebecca.falke@iteratec.com>
2022-09-20 09:30:50 +0200

    added more attributes to parser

--- scanners/ffuf/parser/parser.js
+++ scanners/ffuf/parser/parser.js
@@ -9,42 +9,25 @@ async function parse(fileContent) {
   }
   return fileContent.results.map(result => {
     return {
-      name: 'todo rn - what to put here??',
+      name: 'Webserver Content',
       description: `Content ${result.input?.FUZZ} was found on the webserver ${result.host}.`, // todo rn: what if no FUZZ keyword is used??
       osi_layer: 'APPLICATION',
       severity: 'LOW', // todo rn: how to determine if the finding contains a HIGH, e.g. Spring Actuator endpoints?
       category: 'Webserver Content',
       attributes: {
-        httpStatus: result.status
+        httpStatus: result.status,
+        length: result.length,
+        words: result.words,
+        lines: result.lines,
+        contentType: result["content-type"],
+        redirectlocation: result.redirectlocation,
+        duration: result.duration,
+        resultFile: result.resultfile,
+        hostname: result.host,
       },
-      location: result.url
+      location: result.url,
     }
   });
-
-  /*{
-    "id": "e18cdc5e-6b49-4346-b623-28a4e878e154",
-    "name": "Open mysql Port",
-    "description": "Port 3306 is open using tcp protocol.",
-    "category": "Open Port",
-    "parsed_at": "2021-06-22T12:27:28.153Z",
-    "identified_at": "2021-06-22T12:26:54.378Z",
-    "severity": "INFORMATIONAL",
-    "attributes": {
-    "port": 3306,
-      "state": "open",
-      "ip_address": "198.51.100.42",
-      "mac_address": null,
-      "protocol": "tcp",
-      "hostname": "example.com",
-      "method": "table",
-      "operating_system": null,
-      "service": "mysql",
-      "serviceProduct": null,
-      "serviceVersion": null,
-      "scripts": null
-  },
-    "location": "tcp://127.0.0.1:3306"
-  }*/
 }
 
 module.exports.parse = parse;
