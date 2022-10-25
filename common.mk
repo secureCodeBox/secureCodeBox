e8a5eff721cc7297b86db8ec2beff6651a7af9ce
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2022-10-25 12:01:02 +0200

    refactor(jest): enforce the use of version 27.0.6
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- common.mk
+++ common.mk
@@ -73,7 +73,7 @@ IMG_NS ?= securecodebox
 GIT_TAG ?= $$(git rev-parse --short HEAD)
 BASE_IMG_TAG ?= sha-$(GIT_TAG)
 IMG_TAG ?= "sha-$(GIT_TAG)"
-JEST_VERSION ?= latest
+JEST_VERSION ?= 27.0.6
 KIND_CLUSTER_NAME ?= kind
 
 parser-prefix = parser
