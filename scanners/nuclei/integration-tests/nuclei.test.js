ab925d34867e36591df1d1941345021bae915b2c
Rami Souai <rami.souai@iteratec.com>
2022-10-27 16:18:03 +0200

    \#1343 Use Full Qualified Domain Name for target in nuclei integration-test scan
    Since Nuclei uses a custom resolver it does not benefit from domain search specs in kubedns
    
    Signed-off-by: Rami Souai <rami.souai@iteratec.com>

--- scanners/nuclei/integration-tests/nuclei.test.js
+++ scanners/nuclei/integration-tests/nuclei.test.js
@@ -6,27 +6,25 @@ const {scan} = require("../../helpers");
 
 jest.retryTimes(3);
 
-// test(
-//   "Nuclei scan for a vulnerable bodgeit demo target",
-//   async () => {
-//     const { categories, severities, count } = await scan(
-//       "nuclei-juiceshop",
-//       "nuclei",
-//       ["-no-interactsh", "-u", "http://bodgeit.demo-targets.svc:8080"],
-//       180
-//     );
-
-//     expect(count).toBeGreaterThanOrEqual(4);
-//     // expect(categories["Image Vulnerability"]).toBeGreaterThanOrEqual(10);
-//     // expect(categories["NPM Package Vulnerability"]).toBeGreaterThanOrEqual(30);
-//     // expect(severities["high"]).toBeGreaterThanOrEqual(20);
-//     // expect(severities["medium"]).toBeGreaterThanOrEqual(10);
-//     expect(severities["low"]).toBeGreaterThanOrEqual(1);
-//   },
-//   3 * 60 * 1000
-// );
-
 test(
+  "Nuclei scan for a vulnerable bodgeit demo target",
+  async () => {
+    const { categories, severities, count } = await scan(
+      "nuclei-bodgeit",
+      "nuclei",
+      ["-no-interactsh", "-u", "http://bodgeit.demo-targets.svc.cluster.local:8080"],
+      180
+    );
+
+    expect(count).toBeGreaterThanOrEqual(20);
+    expect(severities["informational"]).toBeGreaterThanOrEqual(20);
+    expect(categories["http-missing-security-headers"]).toBeGreaterThanOrEqual(15);
+    expect(categories["tomcat-detect"]).toBe(1);
+  },
+  3 * 60 * 1000
+);
+
+test.skip(
   "Nuclei scan for a vulnerable demo target",
   async () => {
     const { categories, severities, count } = await scan(
