b2a059015b42cbb3433d34146a9fe428f01074fd
Felix Hörsting <felix.hoersting@iteratec.com>
2022-11-21 11:21:09 +0100

    Included ZAP Advanced integration tests
    Signed-off-by: Felix Hörsting <felix.hoersting@iteratec.com>

--- scanners/zap/integration-tests/zap.test.js
+++ scanners/zap/integration-tests/zap.test.js
@@ -5,6 +5,58 @@
 const {scan} = require("../../helpers");
 
 test(
+  "zap automation scan without config against 'bodgeit' should only find couple findings",
+  async () => {
+    const { count } = await scan(
+      "zap-automation-bodgeit",
+      "zap-automation-scan",
+      ["-autorun", "/home/securecodebox/scb-automation/3-automation.yaml"],
+      60 * 30,
+      // volumes
+      [{
+        "name": "zap-automation-bodgeit",
+        "configMap": {"name": "zap-automation-bodgeit"}
+      }],
+      // volumeMounts
+      [{
+          "name": "zap-automation-bodgeit",
+          "mountPath": "/home/securecodebox/scb-automation/3-automation.yaml",
+          "subPath": "3-automation.yaml"
+      }],
+    );
+    // There must be at least one finding
+    expect(count).toBeGreaterThanOrEqual(1);
+  },
+  60 * 5 * 1000
+);
+
+test(
+  "zap automation scan without config against 'swagger-petstore' should only find couple findings",
+  async () => {
+    const { count } = await scan(
+      "zap-automation-petstore",
+      "zap-automation-scan",
+      ["-autorun", "/home/securecodebox/scb-automation/4-automation.yaml"],
+      60 * 30,
+      // volumes
+      [{
+        "name": "zap-automation-petstore",
+        "configMap": {"name": "zap-automation-petstore"}
+      }],
+      // volumeMounts
+      [{
+          "name": "zap-automation-petstore",
+          "mountPath": "/home/securecodebox/scb-automation/4-automation.yaml",
+          "subPath": "4-automation.yaml"
+      }],
+    );
+    // There must be at least one finding
+    expect(count).toBeGreaterThanOrEqual(1);
+  },
+  60 * 5 * 1000
+);
+
+test(
   "zap automation scan against a plain nginx container should only find a couple of findings",
   async () => {
     const { count } = await scan(
