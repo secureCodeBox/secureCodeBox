d0f6f24c8874f4bdf5d95d69d53e587935d26e85
Simon Hülkenberg <simon.huelkenberg@iteratec.com>
2023-01-20 15:44:38 +0100

    Add initcontianer that extracts matching imagepullsecrets #1189
    Signed-off-by: Simon Hülkenberg <simon.huelkenberg@iteratec.com>

---
+++ auto-discovery/kubernetes/secretExtractionInitContainer/test_secret_extraction.py
@@ -0,0 +1,53 @@
+import unittest
+
+from secret_extraction import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_get_raw_secrets(self):
+        actual = get_raw_secrets('test_secrets')
+
+        with open('test_secrets/secret_1/.dockerconfigjson') as file:
+            expected = [json.load(file)]
+
+        with open('test_secrets/secret_2/.dockerconfigjson') as file:
+            expected.append(json.load(file))
+
+        self.assertEqual(expected, actual)
+
+    def test_get_correct_secret(self):
+        with open('test_secrets/secret_1/.dockerconfigjson') as file:
+            secret_list = [json.load(file)]
+
+        with open('test_secrets/secret_2/.dockerconfigjson') as file:
+            secret_list.append(json.load(file))
+
+        actual = get_correct_secret('localhost:5000:12345/some/image@654654', secret_list)
+
+        # testuser:testpassword base64 encoded
+        expected = {'auth': 'dGVzdHVzZXI6dGVzdHBhc3N3b3Jk'}
+
+        self.assertEqual(expected, actual)
+
+    def test_get_user_and_password_given_auth_string(self):
+        secret = {'auth': 'dGVzdHVzZXI6dGVzdHBhc3N3b3Jk'}
+        actual = get_user_and_password(secret)
+
+        # testuser, testpassword base64 encoded
+        expected = ('dGVzdHVzZXI=', 'dGVzdHBhc3N3b3Jk')
+
+        self.assertEqual(expected, actual)
+
+    def test_get_and_password_given_username_and_password_as_separate_string(self):
+        secret = {
+            'username': 'dGVzdHVzZXI=',
+            'password': 'dGVzdHBhc3N3b3Jk'
+        }
+        actual = get_user_and_password(secret)
+
+        expected = (secret['username'], secret['password'])
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
