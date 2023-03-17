d7aef74b2c5e8ea9399579ceed560a0e0a3c6a42
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2023-03-17 14:02:37 +0100

    Auto format with prettier
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@iteratec.com>

--- scanners/doggo/parser/parser.js
+++ scanners/doggo/parser/parser.js
@@ -21,23 +21,22 @@ async function parse(fileContent) {
 }
 
 function transformToFindings(targets) {
-
   // Code to transform the scanner results to scb findings
-  const targetFindings = targets.map(target => {
+  const targetFindings = targets.map((target) => {
     let finding = {
       name: `DNS Zone: ${target.name} | Type: ${target.type}`,
       description: `DNS record type ${target.type} found for zone ${target.name} with address "${target.address}".`,
       category: "DNS Information",
       location: target.name.slice(0, target.name.length - 1),
-      osi_layer: 'NETWORK',
-      severity: 'INFORMATIONAL',
+      osi_layer: "NETWORK",
+      severity: "INFORMATIONAL",
       attributes: {
         doggy_dns_type: target.type,
         doggy_dns_address: target.address,
         doggy_dns_status: target.status,
         doggy_dns_rtt: target.rtt,
-        doggy_dns_nameserver: target.nameserver
-      }
+        doggy_dns_nameserver: target.nameserver,
+      },
     };
     return finding;
   });
@@ -52,16 +51,26 @@ function transformToFindings(targets) {
 function parseResultFile(fileContent) {
   let targetList = [];
 
-  for(const rawTarget of fileContent) {
+  for (const rawTarget of fileContent) {
     // Code to transform raw target findings to usable js format
-    if (Object.keys(rawTarget).length > 0 && rawTarget.answers !== null && rawTarget.answers !== undefined && rawTarget.answers.length > 0) { //Check for empty target
-      for(const rawAnswers of rawTarget.answers) {
-        if (Object.keys(rawAnswers).length > 0 && rawAnswers.name !== null && rawAnswers.name !== undefined) { //Check for empty answers
+    if (
+      Object.keys(rawTarget).length > 0 &&
+      rawTarget.answers !== null &&
+      rawTarget.answers !== undefined &&
+      rawTarget.answers.length > 0
+    ) {
+      //Check for empty target
+      for (const rawAnswers of rawTarget.answers) {
+        if (
+          Object.keys(rawAnswers).length > 0 &&
+          rawAnswers.name !== null &&
+          rawAnswers.name !== undefined
+        ) {
+          //Check for empty answers
           targetList.push(rawAnswers);
         }
       }
     }
-
   }
   return targetList;
 }
