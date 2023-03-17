816dfdc472a25eb275d7be5ad7705f03c64b8325
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2023-03-17 10:40:50 +0100

    Auto format test file

--- auto-discovery/kubernetes/pull-secret-extractor/integration-test/pull-secret-extraction.test.js
+++ auto-discovery/kubernetes/pull-secret-extractor/integration-test/pull-secret-extraction.test.js
@@ -6,15 +6,17 @@ kc.loadFromDefault();
 const k8sPodsApi = kc.makeApiClient(k8s.CoreV1Api);
 
 test(
-    "Test if initcontainer creates correct secrets",
-    async () => {
-        let secret;
-        try {
-            secret = await k8sPodsApi.readNamespacedSecret("test-secret", "integration-test");
-        } finally {
-            expect(secret).toBeDefined();
-        }
-
-    },
-    60*1000
-)
+  "Test if initcontainer creates correct secrets",
+  async () => {
+    let secret;
+    try {
+      secret = await k8sPodsApi.readNamespacedSecret(
+        "test-secret",
+        "integration-test"
+      );
+    } finally {
+      expect(secret).toBeDefined();
+    }
+  },
+  60 * 1000
+);
