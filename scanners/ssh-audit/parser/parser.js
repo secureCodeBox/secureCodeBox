6024d0ee99d2035ed3dc75f29253487c4047da57
Sofia Lohr <sofia.lohr@iteratec.com>
2023-04-26 16:46:25 +0200

    #1454 Implement findings format

--- scanners/ssh-audit/parser/parser.js
+++ scanners/ssh-audit/parser/parser.js
@@ -1,18 +1,22 @@
 const templates = {
     del: {
-        kex:{
-            findingTemplate: {
-                description: "Good / encouraged SSH compression algorithms are missing",
-                name: "Missing SSH compression algorithms"
-              }
+        kex: {
+            description: "Good / encouraged SSH compression algorithms are missing",
+            name: "Missing SSH compression algorithms"
+        },
+        key: {
+            description: "Good / encouraged SSH compression algorithms are missing",
+            name: "Missing SSH compression algorithms"
+        },
+        mac: {
+            description: "Good / encouraged SSH compression algorithms are missing",
+            name: "Missing SSH compression algorithms"
         }
     },
-    chg:{
-        kex:{
-            findingTemplate: {
-                description: "Change Keks",
-                name: "Change Keks"
-              }
+    chg: {
+        kex: {
+            description: "Change Keks",
+            name: "Change Keks"
         }
     }
 }
@@ -24,29 +28,52 @@ const templates = {
  */
 function transformRecommendationToFinding(
     recommendation, {
-      hostname,
-      ipAddress
+        hostname,
+        ipAddress
     },
     identified_at
-  ) {
+) {
     for (const rule of policyViolationFindingRules) {
-      if (rule.policyViolationPrefix.test(recommendation)) {
-        return createPolicyViolationFinding({
-          name: rule.findingTemplate.name,
-          description: rule.findingTemplate.description,
-          identified_at: identified_at,
-          recommendation,
-          host: {
-            hostname,
-            ipAddress
-          }
-        });
-      }
+        if (rule.policyViolationPrefix.test(recommendation)) {
+            return createPolicyViolationFinding({
+                name: rule.findingTemplate.name,
+                description: rule.findingTemplate.description,
+                identified_at: identified_at,
+                recommendation,
+                host: {
+                    hostname,
+                    ipAddress
+                }
+            });
+        }
     }
-  }
+}
 
-async function parse({ target, banner, enc, kex, key, mac, compression, fingerprints }) {
+async function parse({ target, banner, enc, kex, key, mac, compression, fingerprints, recommendations }) {
     const identified_at = new Date().toISOString();
+    const recommendationsArray = Object.entries(recommendations)
+    const policyViolationFindings = [];
+    recommendationsArray.map(([recommendationSeverityLevel, value]) => {
+        // SSH audit has critical and warnings as recommendations. 
+        // These are HIGH and MEDIUM severities, respectively
+        var severity = null;
+        if (recommendationSeverityLevel == "critical") severity = 'HIGH'
+        if (recommendationSeverityLevel == "warning") severity = 'MEDIUM'
+
+        Object.entries(value).map(([recommendationAction, algorithms]) => {
+            Object.entries(algorithms).map(([algorithmType, algorithmNames]) => {
+                const findingTemplate = templates[recommendationAction][algorithmType] || null;
+                if (findingTemplate != null) {
+                    findingTemplate['severity'] = severity
+                    findingTemplate['category'] = "SSH Policy Violation"
+                    policyViolationFindings.push(findingTemplate)
+                    console.log(findingTemplate)
+                }
+            })
+        })
+    })
+
+
     const serviceFinding = {
         name: "SSH Service",
         description: "SSH Service Information",
@@ -72,7 +99,264 @@ async function parse({ target, banner, enc, kex, key, mac, compression, fingerpr
             fingerprints: fingerprints //ask
         }
     };
-    return [serviceFinding];
+    return [serviceFinding, ...policyViolationFindings];
+}
+const test = {
+    "banner": {
+        "comments": null,
+        "protocol": [
+            2,
+            0
+        ],
+        "raw": "SSH-2.0-OpenSSH_7.4",
+        "software": "OpenSSH_7.4"
+    },
+    "compression": [
+        "none",
+        "zlib@openssh.com"
+    ],
+    "cves": [
+        {
+            "cvssv2": 7.0,
+            "description": "privilege escalation via supplemental groups",
+            "name": "CVE-2021-41617"
+        },
+        {
+            "cvssv2": 7.8,
+            "description": "command injection via anomalous argument transfers",
+            "name": "CVE-2020-15778"
+        },
+        {
+            "cvssv2": 5.3,
+            "description": "username enumeration via GS2",
+            "name": "CVE-2018-15919"
+        },
+        {
+            "cvssv2": 5.3,
+            "description": "enumerate usernames due to timing discrepancies",
+            "name": "CVE-2018-15473"
+        },
+        {
+            "cvssv2": 5.3,
+            "description": "enumerate usernames via challenge response",
+            "name": "CVE-2016-20012"
+        }
+    ],
+    "enc": [
+        "chacha20-poly1305@openssh.com",
+        "aes128-ctr",
+        "aes192-ctr",
+        "aes256-ctr",
+        "aes128-gcm@openssh.com",
+        "aes256-gcm@openssh.com",
+        "aes256-cbc"
+    ],
+    "fingerprints": [
+        {
+            "hash": "3qsxEt/en/6s2C63H0wa4hBNb+VIR6TmOqGe8+jO2nY",
+            "hash_alg": "SHA256",
+            "hostkey": "ssh-ed25519"
+        },
+        {
+            "hash": "e3:ca:d9:ad:6d:0f:6f:fc:80:b0:5c:57:dd:00:7f:da",
+            "hash_alg": "MD5",
+            "hostkey": "ssh-ed25519"
+        },
+        {
+            "hash": "8xXJh7YPxGvEwjKQ157v85VDeFhB74S+tZvhh7AImdo",
+            "hash_alg": "SHA256",
+            "hostkey": "ssh-rsa"
+        },
+        {
+            "hash": "70:9a:76:1b:42:52:49:15:ba:26:ed:d7:84:a0:3f:a3",
+            "hash_alg": "MD5",
+            "hostkey": "ssh-rsa"
+        }
+    ],
+    "kex": [
+        {
+            "algorithm": "curve25519-sha256"
+        },
+        {
+            "algorithm": "curve25519-sha256@libssh.org"
+        },
+        {
+            "algorithm": "ecdh-sha2-nistp256"
+        },
+        {
+            "algorithm": "ecdh-sha2-nistp384"
+        },
+        {
+            "algorithm": "ecdh-sha2-nistp521"
+        },
+        {
+            "algorithm": "diffie-hellman-group-exchange-sha256",
+            "keysize": 1024
+        },
+        {
+            "algorithm": "diffie-hellman-group16-sha512"
+        },
+        {
+            "algorithm": "diffie-hellman-group18-sha512"
+        },
+        {
+            "algorithm": "diffie-hellman-group-exchange-sha1",
+            "keysize": 1024
+        },
+        {
+            "algorithm": "diffie-hellman-group14-sha256"
+        },
+        {
+            "algorithm": "diffie-hellman-group14-sha1"
+        },
+        {
+            "algorithm": "diffie-hellman-group1-sha1"
+        }
+    ],
+    "key": [
+        {
+            "algorithm": "ssh-rsa",
+            "keysize": 2048
+        },
+        {
+            "algorithm": "rsa-sha2-512",
+            "keysize": 2048
+        },
+        {
+            "algorithm": "rsa-sha2-256",
+            "keysize": 2048
+        },
+        {
+            "algorithm": "ecdsa-sha2-nistp256"
+        },
+        {
+            "algorithm": "ssh-ed25519"
+        }
+    ],
+    "mac": [
+        "umac-64-etm@openssh.com",
+        "umac-128-etm@openssh.com",
+        "hmac-sha2-256-etm@openssh.com",
+        "hmac-sha2-512-etm@openssh.com",
+        "hmac-sha1-etm@openssh.com",
+        "umac-64@openssh.com",
+        "umac-128@openssh.com",
+        "hmac-sha2-256",
+        "hmac-sha2-512",
+        "hmac-sha1"
+    ],
+    "recommendations": {
+        "critical": {
+            "chg": {
+                "kex": [
+                    {
+                        "name": "diffie-hellman-group-exchange-sha256",
+                        "notes": "increase modulus size to 3072 bits or larger"
+                    }
+                ]
+            },
+            "del": {
+                "kex": [
+                    {
+                        "name": "diffie-hellman-group14-sha1",
+                        "notes": ""
+                    },
+                    {
+                        "name": "diffie-hellman-group1-sha1",
+                        "notes": ""
+                    },
+                    {
+                        "name": "diffie-hellman-group-exchange-sha1",
+                        "notes": ""
+                    },
+                    {
+                        "name": "ecdh-sha2-nistp256",
+                        "notes": ""
+                    },
+                    {
+                        "name": "ecdh-sha2-nistp384",
+                        "notes": ""
+                    },
+                    {
+                        "name": "ecdh-sha2-nistp521",
+                        "notes": ""
+                    }
+                ],
+                "key": [
+                    {
+                        "name": "ecdsa-sha2-nistp256",
+                        "notes": ""
+                    },
+                    {
+                        "name": "ssh-rsa",
+                        "notes": ""
+                    }
+                ],
+                "mac": [
+                    {
+                        "name": "hmac-sha1",
+                        "notes": ""
+                    },
+                    {
+                        "name": "hmac-sha1-etm@openssh.com",
+                        "notes": ""
+                    }
+                ]
+            }
+        },
+        "warning": {
+            "chg": {
+                "key": [
+                    {
+                        "name": "rsa-sha2-256",
+                        "notes": "increase modulus size to 3072 bits or larger"
+                    },
+                    {
+                        "name": "rsa-sha2-512",
+                        "notes": "increase modulus size to 3072 bits or larger"
+                    }
+                ]
+            },
+            "del": {
+                "enc": [
+                    {
+                        "name": "aes256-cbc",
+                        "notes": ""
+                    }
+                ],
+                "kex": [
+                    {
+                        "name": "diffie-hellman-group14-sha256",
+                        "notes": ""
+                    }
+                ],
+                "mac": [
+                    {
+                        "name": "hmac-sha2-256",
+                        "notes": ""
+                    },
+                    {
+                        "name": "hmac-sha2-512",
+                        "notes": ""
+                    },
+                    {
+                        "name": "umac-128@openssh.com",
+                        "notes": ""
+                    },
+                    {
+                        "name": "umac-64-etm@openssh.com",
+                        "notes": ""
+                    },
+                    {
+                        "name": "umac-64@openssh.com",
+                        "notes": ""
+                    }
+                ]
+            }
+        }
+    },
+    "target": "iteratec.com:22"
 }
 
+parse(test)
 module.exports.parse = parse;
