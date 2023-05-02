ca18717bfc1d15f185bd278742910bad49b09160
Sofia Lohr <sofia.lohr@iteratec.com>
2023-05-02 15:29:18 +0200

    #1454 added algorithmlist in findings
    Signed-off-by: Sofia Lohr <sofia.lohr@iteratec.com>

--- scanners/ssh-audit/parser/parser.js
+++ scanners/ssh-audit/parser/parser.js
@@ -42,13 +42,19 @@ function transformRecommendationToFinding( recommendationSeverityLevel, value) {
         Object.entries(value).map(([recommendationAction, algorithms]) => {
             //algorithmType = kex/ key/ mac, , algorithmNames = {name+note}
             Object.entries(algorithms).map(([algorithmType, algorithmData]) => {
+                const algorithmNames = []
+                Object.entries(algorithmData).flatMap(([namee,notes])=> {algorithmNames.push(Object.values(notes))})
+                //console.log(algorithmNames.flat())
                 const findingTemplate = templates[recommendationAction][algorithmType] || null;
+
                 if (findingTemplate != null && typeof(findingTemplate) != "undefined") {
                     findingTemplate['severity'] = severity
                     findingTemplate['category'] = "SSH Policy Violation"
+                    findingTemplate['algorithms'] = algorithmNames.flat()
                     //console.log("algorithmType\n\n\n",algorithmType)
                     //console.log("algorithmNames\n\n\n",algorithmNames)
                     policyViolationFindings.push(findingTemplate)
+                    console.log(findingTemplate)
                 }
             })
         })
@@ -98,260 +104,211 @@ async function parse({ target, banner, enc, kex, key, mac, compression, fingerpr
 }
 const test = {
     "banner": {
-        "comments": null,
-        "protocol": [
-            2,
-            0
-        ],
-        "raw": "SSH-2.0-OpenSSH_7.4",
-        "software": "OpenSSH_7.4"
+      "comments": "Ubuntu-4ubuntu2.8",
+      "protocol": [
+        2,
+        0
+      ],
+      "raw": "SSH-2.0-OpenSSH_7.2p2 Ubuntu-4ubuntu2.8",
+      "software": "OpenSSH_7.2p2"
     },
     "compression": [
-        "none",
-        "zlib@openssh.com"
+      "none",
+      "zlib@openssh.com"
     ],
     "cves": [
-        {
-            "cvssv2": 7.0,
-            "description": "privilege escalation via supplemental groups",
-            "name": "CVE-2021-41617"
-        },
-        {
-            "cvssv2": 7.8,
-            "description": "command injection via anomalous argument transfers",
-            "name": "CVE-2020-15778"
-        },
-        {
-            "cvssv2": 5.3,
-            "description": "username enumeration via GS2",
-            "name": "CVE-2018-15919"
-        },
-        {
-            "cvssv2": 5.3,
-            "description": "enumerate usernames due to timing discrepancies",
-            "name": "CVE-2018-15473"
-        },
-        {
-            "cvssv2": 5.3,
-            "description": "enumerate usernames via challenge response",
-            "name": "CVE-2016-20012"
-        }
+      {
+        "cvssv2": 7,
+        "description": "privilege escalation via supplemental groups",
+        "name": "CVE-2021-41617"
+      },
+      {
+        "cvssv2": 7.8,
+        "description": "command injection via anomalous argument transfers",
+        "name": "CVE-2020-15778"
+      },
+      {
+        "cvssv2": 5.3,
+        "description": "username enumeration via GS2",
+        "name": "CVE-2018-15919"
+      },
+      {
+        "cvssv2": 5.3,
+        "description": "enumerate usernames due to timing discrepancies",
+        "name": "CVE-2018-15473"
+      },
+      {
+        "cvssv2": 5.3,
+        "description": "enumerate usernames via challenge response",
+        "name": "CVE-2016-20012"
+      },
+      {
+        "cvssv2": 7.8,
+        "description": "cause DoS via long password string (crypt CPU consumption)",
+        "name": "CVE-2016-6515"
+      },
+      {
+        "cvssv2": 7.2,
+        "description": "privilege escalation via triggering crafted environment",
+        "name": "CVE-2015-8325"
+      }
     ],
     "enc": [
-        "chacha20-poly1305@openssh.com",
-        "aes128-ctr",
-        "aes192-ctr",
-        "aes256-ctr",
-        "aes128-gcm@openssh.com",
-        "aes256-gcm@openssh.com",
-        "aes256-cbc"
+      "chacha20-poly1305@openssh.com",
+      "aes128-ctr",
+      "aes192-ctr",
+      "aes256-ctr",
+      "aes128-gcm@openssh.com",
+      "aes256-gcm@openssh.com"
     ],
     "fingerprints": [
-        {
-            "hash": "3qsxEt/en/6s2C63H0wa4hBNb+VIR6TmOqGe8+jO2nY",
-            "hash_alg": "SHA256",
-            "hostkey": "ssh-ed25519"
-        },
-        {
-            "hash": "e3:ca:d9:ad:6d:0f:6f:fc:80:b0:5c:57:dd:00:7f:da",
-            "hash_alg": "MD5",
-            "hostkey": "ssh-ed25519"
-        },
-        {
-            "hash": "8xXJh7YPxGvEwjKQ157v85VDeFhB74S+tZvhh7AImdo",
-            "hash_alg": "SHA256",
-            "hostkey": "ssh-rsa"
-        },
-        {
-            "hash": "70:9a:76:1b:42:52:49:15:ba:26:ed:d7:84:a0:3f:a3",
-            "hash_alg": "MD5",
-            "hostkey": "ssh-rsa"
-        }
+      {
+        "hash": "eLwgzyjvrpwDbDr+pDbIfUhlNANB4DPH9/0w1vGa87E",
+        "hash_alg": "SHA256",
+        "hostkey": "ssh-ed25519"
+      },
+      {
+        "hash": "c8:65:6b:d1:59:03:56:21:d9:0f:84:83:ce:ac:40:86",
+        "hash_alg": "MD5",
+        "hostkey": "ssh-ed25519"
+      },
+      {
+        "hash": "MbRX/CgQyN6/p8/ZjORurfaJqDhu4VEIWfXo0BnxaCE",
+        "hash_alg": "SHA256",
+        "hostkey": "ssh-rsa"
+      },
+      {
+        "hash": "a5:6f:62:26:81:03:b7:5e:06:48:10:04:79:4b:ac:32",
+        "hash_alg": "MD5",
+        "hostkey": "ssh-rsa"
+      }
     ],
     "kex": [
-        {
-            "algorithm": "curve25519-sha256"
-        },
-        {
-            "algorithm": "curve25519-sha256@libssh.org"
-        },
-        {
-            "algorithm": "ecdh-sha2-nistp256"
-        },
-        {
-            "algorithm": "ecdh-sha2-nistp384"
-        },
-        {
-            "algorithm": "ecdh-sha2-nistp521"
-        },
-        {
-            "algorithm": "diffie-hellman-group-exchange-sha256",
-            "keysize": 1024
-        },
-        {
-            "algorithm": "diffie-hellman-group16-sha512"
-        },
-        {
-            "algorithm": "diffie-hellman-group18-sha512"
-        },
-        {
-            "algorithm": "diffie-hellman-group-exchange-sha1",
-            "keysize": 1024
-        },
-        {
-            "algorithm": "diffie-hellman-group14-sha256"
-        },
-        {
-            "algorithm": "diffie-hellman-group14-sha1"
-        },
-        {
-            "algorithm": "diffie-hellman-group1-sha1"
-        }
+      {
+        "algorithm": "curve25519-sha256@libssh.org"
+      },
+      {
+        "algorithm": "ecdh-sha2-nistp256"
+      },
+      {
+        "algorithm": "ecdh-sha2-nistp384"
+      },
+      {
+        "algorithm": "ecdh-sha2-nistp521"
+      },
+      {
+        "algorithm": "diffie-hellman-group-exchange-sha256",
+        "keysize": 2048
+      },
+      {
+        "algorithm": "diffie-hellman-group14-sha1"
+      }
     ],
     "key": [
-        {
-            "algorithm": "ssh-rsa",
-            "keysize": 2048
-        },
-        {
-            "algorithm": "rsa-sha2-512",
-            "keysize": 2048
-        },
-        {
-            "algorithm": "rsa-sha2-256",
-            "keysize": 2048
-        },
-        {
-            "algorithm": "ecdsa-sha2-nistp256"
-        },
-        {
-            "algorithm": "ssh-ed25519"
-        }
+      {
+        "algorithm": "ssh-rsa",
+        "keysize": 2048
+      },
+      {
+        "algorithm": "rsa-sha2-512",
+        "keysize": 2048
+      },
+      {
+        "algorithm": "rsa-sha2-256",
+        "keysize": 2048
+      },
+      {
+        "algorithm": "ecdsa-sha2-nistp256"
+      },
+      {
+        "algorithm": "ssh-ed25519"
+      }
     ],
     "mac": [
-        "umac-64-etm@openssh.com",
-        "umac-128-etm@openssh.com",
-        "hmac-sha2-256-etm@openssh.com",
-        "hmac-sha2-512-etm@openssh.com",
-        "hmac-sha1-etm@openssh.com",
-        "umac-64@openssh.com",
-        "umac-128@openssh.com",
-        "hmac-sha2-256",
-        "hmac-sha2-512",
-        "hmac-sha1"
+      "umac-64-etm@openssh.com",
+      "umac-128-etm@openssh.com",
+      "hmac-sha2-256-etm@openssh.com",
+      "hmac-sha2-512-etm@openssh.com",
+      "hmac-sha1-etm@openssh.com",
+      "umac-64@openssh.com",
+      "umac-128@openssh.com",
+      "hmac-sha2-256",
+      "hmac-sha2-512",
+      "hmac-sha1"
     ],
     "recommendations": {
-        "critical": {
-            "chg": {
-                "kex": [
-                    {
-                        "name": "diffie-hellman-group-exchange-sha256",
-                        "notes": "increase modulus size to 3072 bits or larger"
-                    }
-                ]
+      "critical": {
+        "del": {
+          "kex": [
+            {
+              "name": "diffie-hellman-group14-sha1",
+              "notes": ""
             },
-            "del": {
-                "kex": [
-                    {
-                        "name": "diffie-hellman-group14-sha1",
-                        "notes": ""
-                    },
-                    {
-                        "name": "diffie-hellman-group1-sha1",
-                        "notes": ""
-                    },
-                    {
-                        "name": "diffie-hellman-group-exchange-sha1",
-                        "notes": ""
-                    },
-                    {
-                        "name": "ecdh-sha2-nistp256",
-                        "notes": ""
-                    },
-                    {
-                        "name": "ecdh-sha2-nistp384",
-                        "notes": ""
-                    },
-                    {
-                        "name": "ecdh-sha2-nistp521",
-                        "notes": ""
-                    }
-                ],
-                "key": [
-                    {
-                        "name": "ecdsa-sha2-nistp256",
-                        "notes": ""
-                    },
-                    {
-                        "name": "ssh-rsa",
-                        "notes": ""
-                    }
-                ],
-                "mac": [
-                    {
-                        "name": "hmac-sha1",
-                        "notes": ""
-                    },
-                    {
-                        "name": "hmac-sha1-etm@openssh.com",
-                        "notes": ""
-                    }
-                ]
-            }
-        },
-        "warning": {
-            "chg": {
-                "key": [
-                    {
-                        "name": "rsa-sha2-256",
-                        "notes": "increase modulus size to 3072 bits or larger"
-                    },
-                    {
-                        "name": "rsa-sha2-512",
-                        "notes": "increase modulus size to 3072 bits or larger"
-                    }
-                ]
+            {
+              "name": "ecdh-sha2-nistp256",
+              "notes": ""
             },
-            "del": {
-                "enc": [
-                    {
-                        "name": "aes256-cbc",
-                        "notes": ""
-                    }
-                ],
-                "kex": [
-                    {
-                        "name": "diffie-hellman-group14-sha256",
-                        "notes": ""
-                    }
-                ],
-                "mac": [
-                    {
-                        "name": "hmac-sha2-256",
-                        "notes": ""
-                    },
-                    {
-                        "name": "hmac-sha2-512",
-                        "notes": ""
-                    },
-                    {
-                        "name": "umac-128@openssh.com",
-                        "notes": ""
-                    },
-                    {
-                        "name": "umac-64-etm@openssh.com",
-                        "notes": ""
-                    },
-                    {
-                        "name": "umac-64@openssh.com",
-                        "notes": ""
-                    }
-                ]
+            {
+              "name": "ecdh-sha2-nistp384",
+              "notes": ""
+            },
+            {
+              "name": "ecdh-sha2-nistp521",
+              "notes": ""
             }
+          ],
+          "key": [
+            {
+              "name": "ecdsa-sha2-nistp256",
+              "notes": ""
+            },
+            {
+              "name": "ssh-rsa",
+              "notes": ""
+            }
+          ],
+          "mac": [
+            {
+              "name": "hmac-sha1",
+              "notes": ""
+            },
+            {
+              "name": "hmac-sha1-etm@openssh.com",
+              "notes": ""
+            }
+          ]
         }
+      },
+      "warning": {
+        "del": {
+          "mac": [
+            {
+              "name": "hmac-sha2-256",
+              "notes": ""
+            },
+            {
+              "name": "hmac-sha2-512",
+              "notes": ""
+            },
+            {
+              "name": "umac-128@openssh.com",
+              "notes": ""
+            },
+            {
+              "name": "umac-64-etm@openssh.com",
+              "notes": ""
+            },
+            {
+              "name": "umac-64@openssh.com",
+              "notes": ""
+            }
+          ]
+        }
+      }
     },
-    "target": "iteratec.com:22"
-}
+    "target": "dummy-ssh.default.svc:22"
+  }
 
-console.log(parse(test))
+//console.log(parse(test))
+parse(test)
 module.exports.parse = parse;
