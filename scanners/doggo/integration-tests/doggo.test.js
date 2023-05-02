9db23cefa9479ac0f1431ad51de5d6d8bfe2f7e4
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2023-05-02 11:27:31 +0200

    #1704 Doggo Doggo currently returns a 0 exit code even if the scan fails changed the test to expect empty results instead of failure included reference to the Doggo issue
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- scanners/doggo/integration-tests/doggo.test.js
+++ scanners/doggo/integration-tests/doggo.test.js
@@ -9,7 +9,7 @@ jest.retryTimes(3);
 test(
   "localhost port scan should only find a host finding",
   async () => {
-    const { categories, severities, count } = await scan(
+    const {categories, severities, count} = await scan(
       "doggo-localhost",
       "doggo",
       ["example.com"],
@@ -18,19 +18,19 @@ test(
 
     expect(count).toBeGreaterThanOrEqual(1);
     expect(categories).toMatchInlineSnapshot(`
-      Object {
+      {
         "DNS Information": 1,
       }
     `);
     expect(severities).toMatchInlineSnapshot(`
-      Object {
+      {
         "informational": 1,
       }
     `);
   },
   3 * 60 * 1000
 );
-
+/* 
 test(
   "invalid scan should be marked as errored",
   async () => {
@@ -39,6 +39,25 @@ test(
     ).rejects.toThrow(
       'Scan failed with description "Failed to run the Scan Container, check k8s Job and its logs for more details"'
     );
+  },
+  3 * 60 * 1000
+);
+*/
+// This is temporary replacement for the above test. Doggo currently returns a 0 exit code even if the scan fails.
+// This is a bug in doggo and has been reported recently in https://github.com/mr-karan/doggo/issues/68.
+// Once this is fixed, the above test should be re-enabled.
+// This test below should be removed once the above test is re-enabled
+// if the test below fails due to scan failure, then it's likely the test above should be re-enabled
+test(
+  "invalid scan should get empty results",
+  async () => {
+    await expect(
+      scan("doggo-localhost", "doggo", ["-invalidFlag", "localhost"], 90)
+    ).resolves.toMatchInlineSnapshot(`
+      {
+        "severities": {},
+      }
+    `);
   },
   3 * 60 * 1000
 );
