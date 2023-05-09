d96010f6d4e30fe0d828e4155f8c4ade0f1ce762
Sven Strittmatter <sven.strittmatter@iteratec.com>
2023-05-09 11:58:06 +0200

    Fix the usage comment to work
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- bin/add-license-header.sh
+++ bin/add-license-header.sh
@@ -5,17 +5,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 #
-# Add SPDX license header to givne list off files
+# Add SPDX license header to given list off files
 #
-# Prerequesites:
+# Prerequisites:
 # - reuse tool
 #
 # Usage:
 #   add-license-header.sh file1 file2 file3
+#
+# Example to read from linter file (IMPORTANT: You must remove all the markdown clutter to get palin list of files):
 #   cat spdx-report.md | ./add-license-header.sh
 #
 # To generate the file list use `reuse lint`. This produces a Markdown report:
-#   docker run --rm --volume $(pwd):/data fsfe/reuse lint > files.txt > spdx-report.md
+#   docker run --rm --volume $(pwd):/data fsfe/reuse lint > spdx-report.md
 #
 # See also:
 # - https://spdx.org
