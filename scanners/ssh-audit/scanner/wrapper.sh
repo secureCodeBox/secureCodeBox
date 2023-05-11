79fda16190fe271c06826d7e435d4a64046f86b6
Sofia Lohr <sofia.lohr@iteratec.com>
2023-05-11 10:33:54 +0200

    #1454 Deleted unnecessary lines Co-authored-by: Jannik Hollenbach <jannik.hollenbach@iteratec.com>
    Signed-off-by: Sofia Lohr <sofia.lohr@iteratec.com>

--- scanners/ssh-audit/scanner/wrapper.sh
+++ scanners/ssh-audit/scanner/wrapper.sh
@@ -5,11 +5,12 @@
 /home/sshaudit/ssh-audit.py -j "$@" >> /home/securecodebox/ssh-audit.json
 exit_code=$?
 
+# Exit Code 1 describes an error with the connection to the target
+# Exit Code -1 describes an unknown error
 if [ $exit_code -eq 1 ] || [ $exit_code -eq -1 ]
 then
     echo $(cat /home/securecodebox/ssh-audit.json)
     rm /home/securecodebox/ssh-audit.json
-    echo $exit_code
     exit $exit_code
 else 
     exit 0
