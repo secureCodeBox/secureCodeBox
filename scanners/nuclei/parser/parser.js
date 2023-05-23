61090c4c6759c7e6e187893bc609b1fa71cc7cde
Jannik Hollenbach <jannik@hollenbach.de>
2023-05-23 20:55:25 +0200

    Convert ip_address field in nuclei to an array

--- scanners/nuclei/parser/parser.js
+++ scanners/nuclei/parser/parser.js
@@ -61,7 +61,7 @@ async function parse(fileContent) {
       category: finding["template-id"],
       references: references.length > 0 ? references : null, 
       attributes: {
-        ip: finding.ip || null,
+        ip_addresses: finding.ip ? [finding.ip] : null,
         type: finding.type || null,
         hostname,
         path: finding.path || null,
