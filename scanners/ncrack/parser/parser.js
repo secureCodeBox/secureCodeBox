254996ad554c38c287b3ee6867b4c27b40e0554f
Jannik Hollenbach <jannik@hollenbach.de>
2023-05-23 20:50:50 +0200

    Autoformat ncrack parser

--- scanners/ncrack/parser/parser.js
+++ scanners/ncrack/parser/parser.js
@@ -2,49 +2,61 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-const xml2js = require('xml2js');
+const xml2js = require("xml2js");
 const crypto = require("crypto");
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 // eslint-disable-next-line security/detect-non-literal-fs-filename
 const readFile = util.promisify(fs.readFile);
 
-async function parse (fileContent, scan, encryptionKeyLocation = process.env['ENCRYPTION_KEY_LOCATION']) {
-  const { ncrackrun } = await transformXML(fileContent);
+async function parse(
+  fileContent,
+  scan,
+  encryptionKeyLocation = process.env["ENCRYPTION_KEY_LOCATION"]
+) {
+  const {ncrackrun} = await transformXML(fileContent);
   let publicKey = null;
   if (encryptionKeyLocation) {
-    publicKey = await readPublicKey(encryptionKeyLocation)
-      .catch(() => {
-        console.log('Public key not found on file system location: ' + encryptionKeyLocation)
-        process.exit()
-      });
+    publicKey = await readPublicKey(encryptionKeyLocation).catch(() => {
+      console.log(
+        "Public key not found on file system location: " + encryptionKeyLocation
+      );
+      process.exit();
+    });
   }
   return transformToFindings(ncrackrun, publicKey);
 }
 
-function transformToFindings (ncrackrun, publicKey) {
-  return ncrackrun.service.flatMap(({ address, port, credentials = [] }) => {
-    const { addr: ipAddress } = address[0]['$'];
-    const { protocol, portid, name: portName } = port[0]['$'];
+function transformToFindings(ncrackrun, publicKey) {
+  return ncrackrun.service.flatMap(({address, port, credentials = []}) => {
+    const {addr: ipAddress} = address[0]["$"];
+    const {protocol, portid, name: portName} = port[0]["$"];
 
-    return credentials.map(credential => {
-      let { username, password } = credential['$'];
+    return credentials.map((credential) => {
+      let {username, password} = credential["$"];
 
       if (publicKey) {
-        password = crypto.publicEncrypt({
-          key: publicKey,
-          padding: crypto.constants.RSA_PKCS1_PADDING,
-        }, Buffer.from(password)).toString("base64")
+        password = crypto
+          .publicEncrypt(
+            {
+              key: publicKey,
+              padding: crypto.constants.RSA_PKCS1_PADDING,
+            },
+            Buffer.from(password)
+          )
+          .toString("base64");
       }
 
       return {
         name: `Credentials for Service ${portName}://${ipAddress}:${portid} discovered via bruteforce.`,
-        description: '',
-        category: 'Discovered Credentials',
+        description: "",
+        category: "Discovered Credentials",
         location: `${portName}://${ipAddress}:${portid}`,
-        osi_layer: 'APPLICATION',
-        severity: 'HIGH',
-        mitigation: 'Use a more secure password or disable the service at ' + `${portName}://${ipAddress}:${portid}`,
+        osi_layer: "APPLICATION",
+        severity: "HIGH",
+        mitigation:
+          "Use a more secure password or disable the service at " +
+          `${portName}://${ipAddress}:${portid}`,
         attributes: {
           port: portid,
           ip_address: ipAddress,
@@ -58,11 +70,11 @@ function transformToFindings (ncrackrun, publicKey) {
   });
 }
 
-function transformXML (fileContent) {
+function transformXML(fileContent) {
   return new Promise((resolve, reject) => {
     xml2js.parseString(fileContent, (err, xmlInput) => {
       if (err) {
-        reject(new Error('Error converting XML to JSON in xml2js: ' + err));
+        reject(new Error("Error converting XML to JSON in xml2js: " + err));
       } else {
         resolve(xmlInput);
       }
@@ -70,8 +82,8 @@ function transformXML (fileContent) {
   });
 }
 
-async function readPublicKey (keyLocation) {
-  return readFile(keyLocation)
+async function readPublicKey(keyLocation) {
+  return readFile(keyLocation);
 }
 
 module.exports.parse = parse;
