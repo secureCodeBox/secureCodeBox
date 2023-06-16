3106e26b6b3c56a55c1bf58608bd9b6ee633ef92
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2023-06-16 11:47:25 +0200

    #150 Added a go tag to seperate between slow tests and fast tests fast tests can be called by "make test-fast" and all tests will run when command "make test" is used. This is done to keep the CI workflow as usual
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- operator/controllers/execution/scheduledscan_controller_test.go
+++ operator/controllers/execution/scheduledscan_controller_test.go
@@ -1,11 +1,14 @@
 // SPDX-FileCopyrightText: the secureCodeBox authors
 //
 // SPDX-License-Identifier: Apache-2.0
+
+//go:build fast
+// +build fast
+
 package controllers
 
 import (
 	"context"
-	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -103,26 +106,6 @@ var _ = Describe("ScheduledScan controller", func() {
 			Expect(scheduledScan.Status.Findings.Count).Should(Equal(uint64(42)))
 			Expect(scheduledScan.Status.Findings.FindingSeverities).Should(Equal(executionv1.FindingSeverities{High: 42}))
 			Expect(scheduledScan.Status.Findings.FindingCategories).Should(Equal(map[string]uint64{"Open Port": 42}))
-		})
-	})
-
-	Context("A Scan is triggred due to a Scheduled Scan with Schedule in Spec", func() {
-		It("The ScheduledScan's should be triggered according to the Schedule", func() {
-			ctx := context.Background()
-			namespace := "scantype-multiple-scheduled-scan-triggerd-test-schedule"
-
-			createNamespace(ctx, namespace)
-			createScanType(ctx, namespace)
-			scheduledScan := createScheduledScanWithSchedule(ctx, namespace, true)
-
-			var scanlist executionv1.ScanList
-
-			// ensure that the ScheduledScan has been triggered
-			waitForScheduledScanToBeTriggered(ctx, namespace, 90*time.Second)
-			k8sClient.List(ctx, &scanlist, client.InNamespace(namespace))
-
-			Expect(scheduledScan.Spec.Schedule).Should(Equal("*/1 * * * *"))
-			Expect(scanlist.Items).Should(HaveLen(1))
 		})
 	})
 })
