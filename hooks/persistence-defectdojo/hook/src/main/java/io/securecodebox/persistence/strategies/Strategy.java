0aa849bdbaec83ef8cc756d18b851262332d8be7
Heiko Kiesel <heiko.kiesel@iteratec.com>
2023-07-13 14:29:18 +0200

    Adjust Strategy classes

--- hooks/persistence-defectdojo/hook/src/main/java/io/securecodebox/persistence/strategies/Strategy.java
+++ hooks/persistence-defectdojo/hook/src/main/java/io/securecodebox/persistence/strategies/Strategy.java
@@ -4,9 +4,9 @@
 package io.securecodebox.persistence.strategies;
 
 import io.securecodebox.persistence.config.PersistenceProviderConfig;
-import io.securecodebox.persistence.defectdojo.config.DefectDojoConfig;
-import io.securecodebox.persistence.defectdojo.models.Finding;
-import io.securecodebox.persistence.defectdojo.models.ScanFile;
+import io.securecodebox.persistence.defectdojo.config.Config;
+import io.securecodebox.persistence.defectdojo.model.Finding;
+import io.securecodebox.persistence.defectdojo.model.ScanFile;
 import io.securecodebox.persistence.models.Scan;
 
 import java.util.List;
@@ -15,7 +15,7 @@ import java.util.List;
  *  Defines a general strategy pattern interface used to implement different strategies for importing results into OWASP DefectDojo.
  */
 public interface Strategy {
-  void init(DefectDojoConfig defectDojoConfig, PersistenceProviderConfig persistenceProviderConfig);
+  void init(Config defectDojoConfig, PersistenceProviderConfig persistenceProviderConfig);
 
   List<Finding> run(Scan scan, ScanFile scanResultFile) throws Exception;
 }
