4a2cbbae7ce866b4d7d089a37459034d66387fcd
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2023-08-18 14:40:36 +0200

    #1833 Reimplemented amass parser to use the sqlite database instead of the JSON output This is done since the JSON output is no longer supported
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- scanners/amass/parser/parser.js
+++ scanners/amass/parser/parser.js
@@ -2,42 +2,126 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-async function parse(fileContent) {
-  let identifiedDomains = [];
+const sqlite3 = require('sqlite3').verbose();
 
-  if (typeof fileContent === "string") {
-    identifiedDomains = fileContent
-      .split("\n")
-      .filter(Boolean)
-      .map((domainJson) => JSON.parse(domainJson));
-  } else if (typeof fileContent === "object") {
-    // If amass identifies a single result it will be automatically parsed as a json object by the sdk & underlying http lib (axios)
-    identifiedDomains = [fileContent];
-  }
 
-  return identifiedDomains.map((domain) => {
-    let timestamp;
-    if (domain.Timestamp) {
-        timestamp = new Date(domain.Timestamp).toISOString();
-    }
-    return {
-      name: domain.name,
-      identified_at: timestamp,
-      description: `Found subdomain ${domain.name}`,
-      category: "Subdomain",
-      location: domain.name,
-      osi_layer: "NETWORK",
-      severity: "INFORMATIONAL",
-      attributes: {
-        tag: domain.tag,
-        hostname: domain.name,
-        source: domain.source,
-        domain: domain.domain,
-        addresses: domain.addresses,
-        ip_addresses: domain.addresses?.map((address) => address.ip) ?? [],
-      },
-    };
+async function checkifTableExists(db){
+  const query = `select count(*) from sqlite_master m where m.name="assets" OR m.name="relations"`
+
+  return new Promise((resolve, reject) => {
+    db.get(query, [], (err, row) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(row["count(*)"] === 2);
+    });
+  });
+
+}
+
+async function openDatabase(databasePath) {
+  
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(databasePath, sqlite3.OPEN_READONLY, (err) => {
+      if (err) {
+        reject(err.message);
+        return;
+      }
+    });
+    resolve(db);
   });
 }
+
+async function parse(databasePath) {
+  const db = await openDatabase(databasePath);
+  const tableExists = await checkifTableExists(db);
+  if(!tableExists) return [];
+
+  return new Promise((resolve, reject) => {
+
+    const query = `
+    WITH relation_chain AS (
+      SELECT 
+          fqdn.content AS subdomain, 
+          ips.content AS ip,
+          cidr.content AS cidr,
+          asn.id AS asn_id,
+      asn.content AS asn
+      FROM assets fqdn
+    
+      JOIN relations r1 ON fqdn.id = r1.from_asset_id AND (r1.type = 'a_record' OR r1.type = 'aaaa_record')
+      JOIN assets ips ON r1.to_asset_id = ips.id
+    
+      JOIN relations r2 ON ips.id = r2.to_asset_id AND r2.type = 'contains'
+      JOIN assets cidr ON r2.from_asset_id = cidr.id
+    
+      JOIN relations r3 ON cidr.id = r3.to_asset_id AND r3.type = 'announces'
+      JOIN assets asn ON r3.from_asset_id = asn.id
+    
+      WHERE fqdn.type = 'FQDN'
+    )
+      
+      SELECT 
+      rc.subdomain, 
+      rc.ip, 
+      rc.cidr, 
+      rc.asn,
+      a.content AS managed_by,
+      (SELECT content FROM assets WHERE id = 1) AS domain
+      
+      FROM relation_chain rc
+      JOIN relations r ON rc.asn_id = r.from_asset_id AND r.type = 'managed_by'
+      JOIN assets a ON r.to_asset_id = a.id;`;
+
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const results = rows.map((row) => {
+        // Parse the stringified JSON values
+        const domainObj = JSON.parse(row.domain);
+        const subdomainObj = JSON.parse(row.subdomain);
+        const ipObj = JSON.parse(row.ip);
+        const cidrObj = JSON.parse(row.cidr);
+        const asnObj = JSON.parse(row.asn);
+        const managedByObj = JSON.parse(row.managed_by);
+
+        return {
+          name: subdomainObj.name,
+          identified_at: null,
+          description: `Found subdomain ${subdomainObj.name}`,
+          category: "Subdomain",
+          location: subdomainObj.name,
+          osi_layer: "NETWORK",
+          severity: "INFORMATIONAL",
+          attributes: {
+            addresses: {
+              ip: ipObj.address,
+              cidr: cidrObj.cidr,
+              asn: asnObj.number,
+              desc: managedByObj.name
+            },
+            domain: domainObj.name,
+            hostname: subdomainObj.name,
+            ip_addresses: ipObj.address,
+          },
+        };
+      });
+
+      resolve(results);
+
+      db.close((closeErr) => {
+        if (closeErr) {
+          reject(closeErr.message);
+        }
+      });
+    });
+  });
+}
+
+
 
 module.exports.parse = parse;
