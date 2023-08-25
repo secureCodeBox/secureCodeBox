d887b76cff7c5cf575e27dea70108272b562029c
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2023-08-25 11:22:20 +0200

    #1833 Updated amass parser test and snapshot to fit the change to expect filecontent instead of path of database
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- scanners/amass/parser/parser.test.js
+++ scanners/amass/parser/parser.test.js
@@ -2,35 +2,42 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+const fs = require("fs");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+
+const { parse } = require("./parser");
+
 const {
   validateParser,
 } = require("@securecodebox/parser-sdk-nodejs/parser-utils");
 
-// eslint-disable-next-line security/detect-non-literal-fs-filename
-
-const {
-  parse
-} = require("./parser");
-
 test("parser parses example.com sqlite results database successfully", async () => {
-  const databasePath = __dirname + "/__testFiles__/example.com.sqlite";
+  const fileContent = await readFile(
+    __dirname + "/__testFiles__/example.com.sqlite"
+  );
 
-  const findings = await parse(databasePath);
+  const findings = await parse(fileContent);
   await expect(validateParser(findings)).resolves.toBeUndefined();
   expect(findings).toMatchSnapshot();
 });
 
 test("parser parses sqlite results database with empty tables successfully", async () => {
-  const databasePath = __dirname + "/__testFiles__/emptyTables.sqlite";
-  const findings = await parse(databasePath);
+  const fileContent = await readFile(
+    __dirname + "/__testFiles__/emptyTables.sqlite"
+  );
+  
+  const findings = await parse(fileContent);
   await expect(validateParser(findings)).resolves.toBeUndefined();
   expect(findings).toEqual([]);
 });
 
 test("parser parses sqlite results database with no tables successfully", async () => {
-  const databasePath = __dirname + "/__testFiles__/noTables.sqlite";
+  const fileContent = await readFile(
+    __dirname + "/__testFiles__/noTables.sqlite",
+  );
 
-  const findings = await parse(databasePath);
+  const findings = await parse(fileContent);
   await expect(validateParser(findings)).resolves.toBeUndefined();
   expect(findings).toEqual([]);
 });
