724f800707414186fa1ccff2146dda6798333d8a
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2023-10-31 16:32:19 +0100

    Fixed nuclei integration-test timeout Test uses specific template id that are expected to get results, instead of all available templates Locally: time needed for test decreased from 156s to 27s
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- scanners/nuclei/integration-tests/nuclei.test.js
+++ scanners/nuclei/integration-tests/nuclei.test.js
@@ -12,12 +12,13 @@ test(
     const { categories, severities, count } = await scan(
       "nuclei-bodgeit",
       "nuclei",
-      ["-no-interactsh", "-u", "http://bodgeit.demo-targets.svc.cluster.local:8080"],
+      ["-no-interactsh", "-template-id", "http-missing-security-headers,tomcat-detect",
+       "-u", "http://bodgeit.demo-targets.svc.cluster.local:8080"],
       180
     );
 
-    expect(count).toBeGreaterThanOrEqual(15);
-    expect(severities["informational"]).toBeGreaterThanOrEqual(15);
+    expect(count).toBeGreaterThanOrEqual(10);
+    expect(severities["informational"]).toBeGreaterThanOrEqual(10);
     expect(categories["http-missing-security-headers"]).toBeGreaterThanOrEqual(8);
     expect(categories["tomcat-detect"]).toBe(1);
   },
