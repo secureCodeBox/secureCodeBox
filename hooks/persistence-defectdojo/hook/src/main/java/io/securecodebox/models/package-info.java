2792d0a37eea33ff49e0c01a80266eef8dc5bc9f
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-01-19 21:49:19 +0100

    Add package info with info that it is generated
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

---
+++ hooks/persistence-defectdojo/hook/src/main/java/io/securecodebox/models/package-info.java
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: the secureCodeBox authors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * This package contains generated API models
+ * <p>
+ *   Do not edit the files in this package!
+ * </p>
+ */
+package io.securecodebox.models;
