95d027e56224d0a56ddef328bb1bebacf7eefa20
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-02-02 17:50:02 +0100

    Generic Exceptions Should Never Be Thrown
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- hooks/persistence-defectdojo/hook/src/main/java/io/securecodebox/persistence/strategies/Strategy.java
+++ hooks/persistence-defectdojo/hook/src/main/java/io/securecodebox/persistence/strategies/Strategy.java
@@ -7,6 +7,7 @@ import io.securecodebox.persistence.config.PersistenceProviderConfig;
 import io.securecodebox.persistence.defectdojo.config.Config;
 import io.securecodebox.persistence.defectdojo.model.Finding;
 import io.securecodebox.persistence.defectdojo.model.ScanFile;
+import io.securecodebox.persistence.exceptions.DefectDojoPersistenceException;
 import io.securecodebox.persistence.models.Scan;
 
 import java.util.List;
@@ -17,5 +18,5 @@ import java.util.List;
 public interface Strategy {
   void init(Config defectDojoConfig, PersistenceProviderConfig persistenceProviderConfig);
 
-  List<Finding> run(Scan scan, ScanFile scanResultFile) throws Exception;
+  List<Finding> run(Scan scan, ScanFile scanResultFile) throws DefectDojoPersistenceException;
 }
