68b84673b4e07a94a77e5d21e129a8b2e17ceb36
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-02-13 21:01:39 +0100

    #2272 Merge Tests into One
    Found the test in the wrong package, after wondering why there are
    so much public API...
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- hooks/persistence-defectdojo/hook/src/test/java/io/securecodebox/persistence/util/DescriptionGeneratorTest.java
+++ hooks/persistence-defectdojo/hook/src/test/java/io/securecodebox/persistence/util/DescriptionGeneratorTest.java
@@ -3,29 +3,37 @@
 // SPDX-License-Identifier: Apache-2.0
 package io.securecodebox.persistence.util;
 
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.securecodebox.models.V1Scan;
 import io.securecodebox.models.V1ScanSpec;
-import org.junit.jupiter.api.Disabled;
+import io.securecodebox.models.V1ScanStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tests for {@link DescriptionGenerator}
  */
 class DescriptionGeneratorTest {
 
-  private final DescriptionGenerator sut = new DescriptionGenerator();
-
-  @Test
-  @Disabled
-  void generate() {
-    final var scan = new V1Scan();
-    assertThat(sut.generate(scan), is(""));
+  @BeforeEach
+  public void setUp() {
+    final var fixedClock = Clock.fixed(
+      Instant.ofEpochSecond(1546876203),
+      ZoneId.of("Europe/Berlin"));
+    sut.setClock(fixedClock);
   }
+
+  private final DescriptionGenerator sut = new DescriptionGenerator();
 
   @Test
   void determineDefectDojoScanName() {
@@ -33,7 +41,7 @@ class DescriptionGeneratorTest {
     spec.setScanType(ScanNameMapping.NMAP.secureCodeBoxbScanType);
     final var scan = new V1Scan();
     scan.setSpec(spec);
-    
+
     assertThat(sut.determineDefectDojoScanName(scan), is(ScanNameMapping.NMAP.defectDojoScanType.getTestType()));
   }
 
@@ -63,5 +71,82 @@ class DescriptionGeneratorTest {
       () -> assertThat(scan.getSpec(), is(not(nullValue()))),
       () -> assertThat(sut.determineDefectDojoScanName(scan), is("Generic Findings Import"))
     );
+  }
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+
+  @Test
+  void generate() {
+    final var scan = new V1Scan();
+    scan.setMetadata(new V1ObjectMeta());
+    scan.setSpec(new V1ScanSpec());
+    scan.setStatus(new V1ScanStatus());
+    scan.getSpec().setScanType("nmap");
+    scan.getMetadata().setName("test-scan");
+    scan.getMetadata().setCreationTimestamp(OffsetDateTime.parse("2010-06-30T01:20+02:00"));
+    scan.getSpec().setParameters(List.of());
+
+    assert scan.getMetadata() != null;
+
+    scan.getMetadata().setName("nmap");
+    scan.getSpec().setScanType("nmap");
+    scan.getSpec().setParameters(List.of("http://example.target"));
+
+    assertEquals(
+      String.join(
+        System.getProperty("line.separator"),
+        "# Nmap Scan",
+        "Started: 30.06.2010 01:20:00",
+        "Ended: 07.01.2019 16:50:03",
+        "ScanType: nmap",
+        "Parameters: [http://example.target]"
+      ),
+      sut.generate(scan)
+    );
+  }
+
+  @Test
+  void nullGenerate() {
+    final var scan = new V1Scan();
+    scan.setMetadata(new V1ObjectMeta());
+    scan.setSpec(new V1ScanSpec());
+    scan.setStatus(new V1ScanStatus());
+    scan.getSpec().setScanType("nmap");
+    scan.getMetadata().setName("test-scan");
+    scan.getMetadata().setCreationTimestamp(OffsetDateTime.parse("2010-06-30T01:20+02:00"));
+    scan.getSpec().setParameters(List.of());
+
+    assertEquals(String.join(
+      System.getProperty("line.separator"),
+      "# Nmap Scan",
+      "Started: 30.06.2010 01:20:00",
+      "Ended: 07.01.2019 16:50:03",
+      "ScanType: nmap",
+      "Parameters: []"
+    ), sut.generate(scan));
+  }
+
+  @Test
+  void shouldUseCurrentTimeIfEndedAtIsntSet() {
+    final var scan = new V1Scan();
+    scan.setMetadata(new V1ObjectMeta());
+    scan.setSpec(new V1ScanSpec());
+    scan.setStatus(new V1ScanStatus());
+    scan.getSpec().setScanType("nmap");
+    scan.getMetadata().setName("test-scan");
+    scan.getMetadata().setCreationTimestamp(OffsetDateTime.parse("2010-06-30T01:20+02:00"));
+    scan.getSpec().setParameters(List.of());
+
+    scan.getStatus().setFinishedAt(null);
+
+    assertEquals(String.join(
+      System.getProperty("line.separator"),
+      "# Nmap Scan",
+      "Started: 30.06.2010 01:20:00",
+      "Ended: 07.01.2019 16:50:03",
+      "ScanType: nmap",
+      "Parameters: []"
+    ), sut.generate(scan));
   }
 }
