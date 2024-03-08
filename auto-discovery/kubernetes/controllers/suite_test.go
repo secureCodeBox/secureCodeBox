d23e2c6e719b6c020ce9188cd0a5b17309273aff
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2024-03-08 15:07:44 +0100

    Resolving newer golang linter errors
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@iteratec.com>

--- auto-discovery/kubernetes/controllers/suite_test.go
+++ auto-discovery/kubernetes/controllers/suite_test.go
@@ -14,7 +14,6 @@ import (
 	. "github.com/onsi/gomega"
 
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -34,7 +33,6 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
 var ctx context.Context
