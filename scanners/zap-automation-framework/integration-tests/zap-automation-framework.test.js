acf4d6f8c5d05fdf484ecc3d3b2c703e8a9a425e
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2024-04-09 11:52:20 +0200

    #1902 Renamed integration tests for zap-automation-framework
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- scanners/zap-automation-framework/integration-tests/zap-automation-framework.test.js
+++ scanners/zap-automation-framework/integration-tests/zap-automation-framework.test.js
@@ -107,35 +107,3 @@ test.concurrent(
   },
   60 * 8 * 1000
 );
-
-/*test(
-  "zap baseline scan against a plain nginx container should only find couple findings",
-  async () => {
-    const {categories, severities} = await scan(
-      "zap-nginx-baseline",
-      "zap-baseline-scan",
-      ["-t", "http://nginx.demo-targets.svc"],
-      60 * 4
-    );
-
-    expect(categories).toMatchInlineSnapshot(`
-Object {
-  "Content Security Policy (CSP) Header Not Set": 1,
-  "In Page Banner Information Leak": 1,
-  "Missing Anti-clickjacking Header": 1,
-  "Permissions Policy Header Not Set": 1,
-  "Server Leaks Version Information via \\"Server\\" HTTP Response Header Field": 1,
-  "Storable and Cacheable Content": 1,
-  "X-Content-Type-Options Header Missing": 1,
-}
-`);
-    expect(severities).toMatchInlineSnapshot(`
-Object {
-  "informational": 1,
-  "low": 4,
-  "medium": 2,
-}
-`);
-  },
-  5 * 60 * 1000
-);*/
