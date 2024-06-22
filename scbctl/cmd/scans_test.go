72bc780763bdf85a9fc2f84a00ddd4453f398d41
Jannik Hollenbach <jannik@hollenbach.de>
2024-06-22 20:26:30 +0200

    Add more assertions and test cases
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>

--- scbctl/cmd/scans_test.go
+++ scbctl/cmd/scans_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	v1 "github.com/secureCodeBox/secureCodeBox/operator/apis/execution/v1"
+	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
@@ -27,34 +28,80 @@ func (m *MockClientProvider) GetClient(_ *genericclioptions.ConfigFlags) (client
 	return m.Client, m.namespace, m.err
 }
 
+type testcase struct {
+	name          string
+	args          []string
+	expectedError error
+	expectedScan  *expectedScan
+}
+
+type expectedScan struct {
+	name       string
+	scanType   string
+	namespace  string
+	parameters []string
+}
+
 func TestScanCommand(t *testing.T) {
 	scheme := runtime.NewScheme()
 	utilruntime.Must(v1.AddToScheme(scheme))
 
-	testcases := []struct {
-		name          string
-		args          []string
-		expectedError error
-	}{
+	testcases := []testcase{
 		{
-			name:          "Valid entry ",
+			name:          "Should create nmap scan with a single parameter",
 			args:          []string{"scan", "nmap", "--", "scanme.nmap.org"},
 			expectedError: nil,
+			expectedScan: &expectedScan{
+				name:       "nmap",
+				scanType:   "nmap",
+				namespace:  "default",
+				parameters: []string{"scanme.nmap.org"},
+			},
 		},
 		{
-			name:          "Valid entry with multiple parameters",
+			name:          "Should create nmap scan with multiple parameters",
 			args:          []string{"scan", "nmap", "--", "scanme.nmap.org", "-p", "90"},
 			expectedError: nil,
+			expectedScan: &expectedScan{
+				name:       "nmap",
+				scanType:   "nmap",
+				namespace:  "default",
+				parameters: []string{"scanme.nmap.org", "-p", "90"},
+			},
 		},
 		{
-			name:          "Valid entry with namespace",
+			name:          "Should create nmap in a custom namespace",
 			args:          []string{"scan", "--namespace", "foobar", "nmap", "--", "scanme.nmap.org"},
 			expectedError: nil,
+			expectedScan: &expectedScan{
+				name:       "nmap",
+				scanType:   "nmap",
+				namespace:  "foobar",
+				parameters: []string{"scanme.nmap.org"},
+			},
 		},
 		{
-			name:          "No scan parameters provided",
+			name:          "Flags provided after the `--` seperator should be passed as parameters, not flags",
+			args:          []string{"scan", "--namespace", "foobar", "kubeaudit", "--", "--namespace", "some-other-namespace"},
+			expectedError: nil,
+			expectedScan: &expectedScan{
+				name:       "kubeaudit",
+				scanType:   "kubeaudit",
+				namespace:  "foobar",
+				parameters: []string{"--namespace", "some-other-namespace"},
+			},
+		},
+		{
+			name:          "Should throw an error when no parameters are provided",
 			args:          []string{"scan", "nmap"},
 			expectedError: errors.New("you must use '--' to separate scan parameters"),
+			expectedScan:  nil,
+		},
+		{
+			name:          "Should throw an error when no `--` separator is used before the scan parameters",
+			args:          []string{"scan", "nmap", "scanme.nmap.org"},
+			expectedError: errors.New("you must use '--' to separate scan parameters"),
+			expectedScan:  nil,
 		},
 	}
 
@@ -75,15 +122,10 @@ func TestScanCommand(t *testing.T) {
 			rootCmd.SilenceUsage = true
 
 			err := rootCmd.Execute()
-			if tc.expectedError != nil {
-				if err == nil || err.Error() != tc.expectedError.Error() {
-					t.Errorf("expected error: %v, got: %v", tc.expectedError, err)
-				}
-			} else if err != nil {
-				t.Errorf("expected no error, but got: %v", err)
-			}
 
-			if tc.expectedError == nil {
+			assert.Equal(t, err, tc.expectedError, "error returned by scan should match")
+
+			if tc.expectedScan != nil {
 				scans := &v1.ScanList{}
 				if listErr := client.List(context.Background(), scans); listErr != nil {
 					t.Fatalf("failed to list scans: %v", listErr)
@@ -91,8 +133,14 @@ func TestScanCommand(t *testing.T) {
 				if len(scans.Items) != 1 {
 					t.Fatalf("expected 1 scan to created but got %d", len(scans.Items))
 				}
-			}
 
+				scan := scans.Items[0]
+
+				assert.Equal(t, scan.Name, tc.expectedScan.name)
+				assert.Equal(t, scan.Namespace, tc.expectedScan.namespace)
+				assert.Equal(t, scan.Spec.ScanType, tc.expectedScan.scanType)
+				assert.Equal(t, scan.Spec.Parameters, tc.expectedScan.parameters)
+			}
 		})
 	}
 }
