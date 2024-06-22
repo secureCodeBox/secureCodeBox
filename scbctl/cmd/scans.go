6ac51577a7825691bf01adff424e4f71f818c6df
Jannik Hollenbach <jannik@hollenbach.de>
2024-06-22 19:23:07 +0200

    Restructure code to allow tests to reuse the underlying command definition
    Signed-off-by: Jannik Hollenbach <jannik@hollenbach.de>

--- scbctl/cmd/scans.go
+++ scbctl/cmd/scans.go
@@ -5,92 +5,79 @@ package cmd
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"strings"
 
 	v1 "github.com/secureCodeBox/secureCodeBox/operator/apis/execution/v1"
-	kubernetes "github.com/secureCodeBox/secureCodeBox/scbctl/pkg"
+
 	"github.com/spf13/cobra"
 	metav2 "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
-var (
-	kubeconfigArgs                           = genericclioptions.NewConfigFlags(false)
-	clientProvider kubernetes.ClientProvider = &kubernetes.DefaultClientProvider{}
-	scheme                                   = runtime.NewScheme()
-)
+func NewScanCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "scan [scanType] -- [parameters...]",
+		Short: "Create a new scan",
+		Long:  `Create a new Scan custom resource in the the current namespace`,
+		Args:  cobra.MinimumNArgs(1),
+		Example: `
+		# Create a new scan
+		scbctl scan nmap -- scanme.nmap.org
+		# Create in a different namespace
+		scbctl scan --namespace foobar nmap -- -p 80 scanme.nmap.org
+		`,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
 
-func init() {
-	utilruntime.Must(v1.AddToScheme(scheme))
-	ScanCmd.Flags().StringP("namespace", "n", "", "Namespace in which to create the scan")
-}
-
-var ScanCmd = &cobra.Command{
-	Use:   "scan [name] -- [parameters...]",
-	Short: "Create a new scanner",
-	Long:  `Create a new execution (Scan) in the default namespace if no namespace is provided`,
-	Example: `
-	# Create a new scan
-	scbctl scan nmap 
-	# Create in a different namespace
-	scbctl scan --namespace foobar nmap -- scanme.nmap.org -p 90
-	`,
-	SilenceUsage: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-
-		scanName := args[0]
-		paramIndex := cmd.ArgsLenAtDash()
-		if paramIndex == -1 {
-			return errors.New("You must use '--' to separate scan parameters")
-		}
-
-		parameters := args[paramIndex:]
-
-		fmt.Println("🎬 Initializing Kubernetes client")
-
-		kubeclient, namespace, err := clientProvider.GetClient(kubeconfigArgs)
-		if err != nil {
-			return fmt.Errorf("Error initializing Kubernetes client: %s", err)
-		}
-
-		if namespaceFlag, err := cmd.Flags().GetString("namespace"); err == nil && namespaceFlag != "" {
-			namespace = namespaceFlag
-		}
-
-		fmt.Printf("🆕 Creating a new scan with name '%s' and parameters '%s'\n", scanName, strings.Join(parameters, " "))
-
-		scan := &v1.Scan{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "Scan",
-				APIVersion: "execution.securecodebox.io/v1",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      scanName,
-				Namespace: namespace,
-			},
-			Spec: v1.ScanSpec{
-				ScanType:   scanName,
-				Parameters: parameters,
-			},
-		}
-
-		fmt.Println("🔁 Launching the scan")
-
-		err = kubeclient.Create(context.TODO(), scan)
-		if err != nil {
-			if metav2.IsNotFound(err) {
-				return fmt.Errorf("failed to create Scan: namespace '%s' not found", namespace)
+			scanName := args[0]
+			paramIndex := cmd.ArgsLenAtDash()
+			if paramIndex == -1 {
+				return fmt.Errorf("you must use '--' to separate scan parameters")
 			}
-			return fmt.Errorf("Failed to create Scan: %s", err)
-		}
 
-		fmt.Printf("🚀 Successfully created a new Scan '%s'\n", args[0])
-		return nil
+			parameters := args[paramIndex:]
 
-	},
+			fmt.Println("🎬 Initializing Kubernetes client")
+
+			kubeclient, namespace, err := clientProvider.GetClient(kubeconfigArgs)
+			if err != nil {
+				return fmt.Errorf("error initializing kubernetes client: %s", err)
+			}
+
+			if namespaceFlag, err := cmd.Flags().GetString("namespace"); err == nil && namespaceFlag != "" {
+				namespace = namespaceFlag
+			}
+
+			fmt.Printf("🆕 Creating a new scan with name '%s' and parameters '%s'\n", scanName, strings.Join(parameters, " "))
+
+			scan := &v1.Scan{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Scan",
+					APIVersion: "execution.securecodebox.io/v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      scanName,
+					Namespace: namespace,
+				},
+				Spec: v1.ScanSpec{
+					ScanType:   scanName,
+					Parameters: parameters,
+				},
+			}
+
+			fmt.Println("🔁 Launching the scan")
+
+			err = kubeclient.Create(context.TODO(), scan)
+			if err != nil {
+				if metav2.IsNotFound(err) {
+					return fmt.Errorf("failed to create Scan: namespace '%s' not found", namespace)
+				}
+				return fmt.Errorf("failed to create scan: %s", err)
+			}
+
+			fmt.Printf("🚀 Successfully created a new Scan '%s'\n", args[0])
+			return nil
+		},
+	}
 }
--- scbctl/main.go
+++ scbctl/main.go
@@ -7,28 +7,19 @@ import (
 	"os"
 
 	"github.com/secureCodeBox/secureCodeBox/scbctl/cmd"
-	"github.com/spf13/cobra"
 	v1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
-var kubeconfigArgs = genericclioptions.NewConfigFlags(false)
 var scheme = runtime.NewScheme()
-
-var rootCmd = &cobra.Command{
-	Use:   "scbctl",
-	Short: "cli app to manage scans & other secureCodeBox resources",
-	Long:  ``,
-}
 
 func init() {
 	utilruntime.Must(v1.AddToScheme(scheme))
-	rootCmd.AddCommand(cmd.ScanCmd)
-	kubeconfigArgs.AddFlags(rootCmd.PersistentFlags())
 }
+
 func main() {
+	rootCmd := cmd.NewRootCommand()
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
