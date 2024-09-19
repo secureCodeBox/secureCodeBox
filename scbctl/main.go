c69d22dd90d7d24ba4a6b67ed11ec5fcf0e9353a
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2024-09-19 12:22:48 +0200

    Remove unneeded code from main.go
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@iteratec.com>

--- scbctl/main.go
+++ scbctl/main.go
@@ -7,16 +7,7 @@ import (
 	"os"
 
 	"github.com/secureCodeBox/secureCodeBox/scbctl/cmd"
-	v1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 )
-
-var scheme = runtime.NewScheme()
-
-func init() {
-	utilruntime.Must(v1.AddToScheme(scheme))
-}
 
 func main() {
 	rootCmd := cmd.NewRootCommand()
