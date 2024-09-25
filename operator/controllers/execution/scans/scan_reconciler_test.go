d6ac6b9e4df34061fd7b18c8b8ad6894fdf80718
Samreet Singh <samreet.singh@iteratec.com>
2024-09-25 13:49:56 +0200

    Added unit tests for ttlSecondsAfterFinished field
    Co-authored-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
    Signed-off-by: Samreet Singh <samreet.singh@iteratec.com>

---
+++ operator/controllers/execution/scans/scan_reconciler_test.go
@@ -0,0 +1,75 @@
+// SPDX-FileCopyrightText: the secureCodeBox authors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package scancontrollers
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	executionv1 "github.com/secureCodeBox/secureCodeBox/operator/apis/execution/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var namespace = "namespace"
+var reconciler = &ScanReconciler{}
+var _ = Describe("ScanControllers", func() {
+	Context("checkIfTTLSecondsAfterFinishedisCompleted", func() {
+		It("should return true if TTLSecondsAfterFinished is set", func() {
+			finishTime := time.Date(
+				2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+			var timeout int32 = 30
+			var scan = &executionv1.Scan{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: namespace,
+					Name:      "nmap",
+				},
+				Spec: executionv1.ScanSpec{
+					ScanType:                "nmap",
+					Parameters:              []string{"scanme.nmap.org"},
+					TTLSecondsAfterFinished: &timeout,
+				},
+				Status: executionv1.ScanStatus{
+					FinishedAt: &metav1.Time{Time: finishTime},
+				},
+			}
+			Expect(reconciler.checkIfTTLSecondsAfterFinishedisCompleted(scan)).To(BeTrue())
+		})
+
+		It("should return false if TTLSecondsAfterFinished is not set", func() {
+			var scan = &executionv1.Scan{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: namespace,
+					Name:      "nmap",
+				},
+				Spec: executionv1.ScanSpec{
+					ScanType:   "nmap",
+					Parameters: []string{"scanme.nmap.org"},
+				},
+			}
+			Expect(reconciler.checkIfTTLSecondsAfterFinishedisCompleted(scan)).To(BeFalse())
+		})
+		It("should return false if TTLSecondsAfterFinished is timed out", func() {
+			finishTime := time.Now()
+			var timeout int32 = 300
+			var scan = &executionv1.Scan{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: namespace,
+					Name:      "nmap3",
+				},
+				Spec: executionv1.ScanSpec{
+					ScanType:                "nmap",
+					Parameters:              []string{"scanme.nmap.org"},
+					TTLSecondsAfterFinished: &timeout,
+				},
+				Status: executionv1.ScanStatus{
+					FinishedAt: &metav1.Time{Time: finishTime},
+				},
+			}
+			Expect(reconciler.checkIfTTLSecondsAfterFinishedisCompleted(scan)).To(BeFalse())
+		})
+
+	})
+})
