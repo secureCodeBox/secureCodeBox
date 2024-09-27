afafcb0f55e27294b91ead2bbbabf92ba5a64a4e
ddddddO <lbfdeatq@gmail.com>
2024-09-27 22:56:18 +0900

    [nits] modify buildTree func
    Signed-off-by: ddddddO <lbfdeatq@gmail.com>

--- scbctl/cmd/cascading.go
+++ scbctl/cmd/cascading.go
@@ -57,9 +57,6 @@ func buildTree(scans []v1.Scan) *gtree.Node {
 	root := gtree.NewRoot("Scans")
 
 	scanMap := make(map[string]*v1.Scan)
-	for i := range scans {
-		scanMap[scans[i].Name] = &scans[i]
-	}
 	for _, scan := range scans {
 		scanMap[scan.Name] = &scan
 	}
