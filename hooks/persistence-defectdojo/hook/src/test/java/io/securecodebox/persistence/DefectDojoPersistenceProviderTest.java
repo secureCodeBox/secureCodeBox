0462b39210cf2130417c25b950f949e53a1a7a06
Sven Strittmatter <sven.strittmatter@iteratec.com>
2025-01-07 22:07:55 +0100

    Add Missing License Headers
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- hooks/persistence-defectdojo/hook/src/test/java/io/securecodebox/persistence/DefectDojoPersistenceProviderTest.java
+++ hooks/persistence-defectdojo/hook/src/test/java/io/securecodebox/persistence/DefectDojoPersistenceProviderTest.java
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: the secureCodeBox authors
+//
+// SPDX-License-Identifier: Apache-2.0
 package io.securecodebox.persistence;
 
 import org.junit.jupiter.params.ParameterizedTest;
