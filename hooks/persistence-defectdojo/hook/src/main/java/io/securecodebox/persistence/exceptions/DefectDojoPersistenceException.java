4dd48288fdd738876c12891be41dc4373cb8b144
Sven Strittmatter <sven.strittmatter@iteratec.com>
2025-01-07 14:48:30 +0100

    Document Constructors of Exception
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- hooks/persistence-defectdojo/hook/src/main/java/io/securecodebox/persistence/exceptions/DefectDojoPersistenceException.java
+++ hooks/persistence-defectdojo/hook/src/main/java/io/securecodebox/persistence/exceptions/DefectDojoPersistenceException.java
@@ -7,10 +7,21 @@ package io.securecodebox.persistence.exceptions;
  * The base error type of this hook
  */
 public class DefectDojoPersistenceException extends RuntimeException {
+  /**
+   * Creates an exception with a message
+   *
+   * @param message must not be {@code null} ar empty. Should be formatted to be directly printed to STDERR.
+   */
   public DefectDojoPersistenceException(String message) {
-    super(message);
+    this(message, null);
   }
 
+  /**
+   * Dedicated constructor
+   *
+   * @param message see {@link #DefectDojoPersistenceException(String}
+   * @param cause   may be {@code null} if context where the exception occurred is unnecessary.
+   */
   public DefectDojoPersistenceException(String message, Throwable cause) {
     super(message, cause);
   }
