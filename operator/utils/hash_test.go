821034fd74da050ec6335708957df56107d3ccfb
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2025-02-06 11:20:35 +0100

    Update to latest kubebuilder dependency
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@iteratec.com>

--- operator/utils/hash_test.go
+++ operator/utils/hash_test.go
@@ -53,7 +53,8 @@ var _ = Describe("ScanType Hashing", func() {
 
 		It("should hash scantype consistently", func() {
 			hashValues := HashScanType(scanType)
-			Expect(hashValues).To(Equal(uint64(0xc1cee52ba3736175)), "Should hash scantype consistently")
+			// note: this hash changes with every kubernetes release as kubernetes adds new field to their objects which causes the hashes to change.
+			Expect(hashValues).To(Equal(uint64(0xba4b605a6550aca3)), "Should hash scantype consistently")
 		})
 
 		It("should ignore non-scb annotations on the scantypes", func() {
