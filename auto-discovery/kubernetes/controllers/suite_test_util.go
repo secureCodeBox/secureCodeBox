b682db18dfb24020059d1d8c420d6a9bb699aee6
Boris Shek <boris.shek@iteratec.com>
2025-04-15 15:18:06 +0200

    Refactor auto-discovery controller suite tests to remove duplicate code
    Correct a minor typographical error in service_scan_controller.go regarding the RequeueAfter value.
    
    Signed-off-by: Boris Shek <boris.shek@iteratec.com>

--- auto-discovery/kubernetes/controllers/suite_test_util.go
+++ auto-discovery/kubernetes/controllers/suite_test_util.go
@@ -11,174 +11,70 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var Config = config.AutoDiscoveryConfig{
-	Cluster: config.ClusterConfig{
-		Name: "test-cluster",
-	},
-	ServiceAutoDiscovery: config.ServiceAutoDiscoveryConfig{
-		PassiveReconcileInterval: metav1.Duration{Duration: 1 * time.Second},
-		ScanConfigs: []config.ScanConfig{
-			{
-				Name:           "test-scan-0",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{},
-				Labels:         map[string]string{},
-				Parameters:     []string{"-p", "{{ .Host.Port }}", "{{ .Service.Name }}.{{ .Service.Namespace }}.svc"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"foo": "bar",
-					},
-				},
-			},
-			{
-				Name:           "test-scan-1",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{},
-				Labels:         map[string]string{},
-				Parameters:     []string{"-p", "{{ .Host.Port }}", "{{ .Service.Name }}.{{ .Service.Namespace }}.svc"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"foo": "bar",
-					},
-				},
+// newServiceScanConfigMock creates a mock scan configuration specifically for service auto-discovery
+func newServiceScanConfigMock(name string) config.ScanConfig {
+	return config.ScanConfig{
+		Name:           name,
+		RepeatInterval: metav1.Duration{Duration: time.Hour},
+		Annotations:    map[string]string{},
+		Labels:         map[string]string{},
+		Parameters:     []string{"-p", "{{ .Host.Port }}", "{{ .Service.Name }}.{{ .Service.Namespace }}.svc"},
+		ScanType:       "nmap",
+		HookSelector: metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"foo": "bar",
 			},
 		},
-	},
-	ContainerAutoDiscovery: config.ContainerAutoDiscoveryConfig{
-		PassiveReconcileInterval: metav1.Duration{Duration: 1 * time.Second},
-		ScanConfigs: []config.ScanConfig{
-			{
-				Name:           "test-scan",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{"testAnnotation": "{{ .Namespace.Name }}"},
-				Labels:         map[string]string{"testLabel": "{{ .Namespace.Name }}"},
-				Parameters:     []string{"-p", "{{ .Namespace.Name }}"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchExpressions: []metav1.LabelSelectorRequirement{
-						{
-							Operator: metav1.LabelSelectorOpIn,
-							Key:      "foo",
-							Values:   []string{"bar", "baz"},
-						},
-						{
-							Operator: metav1.LabelSelectorOpDoesNotExist,
-							Key:      "foo",
-						},
-					},
-				},
-			},
-			{
-				Name:           "test-scan-two",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{"testAnnotation": "{{ .Namespace.Name }}"},
-				Labels:         map[string]string{"testLabel": "{{ .Namespace.Name }}"},
-				Parameters:     []string{"-p", "{{ .Namespace.Name }}"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchExpressions: []metav1.LabelSelectorRequirement{
-						{
-							Operator: metav1.LabelSelectorOpIn,
-							Key:      "foo",
-							Values:   []string{"bar", "baz"},
-						},
-						{
-							Operator: metav1.LabelSelectorOpDoesNotExist,
-							Key:      "foo",
-						},
-					},
-				},
-			},
-		},
-	},
-	ResourceInclusion: config.ResourceInclusionConfig{
-		Mode: config.EnabledPerResource,
-	},
+	}
 }
 
-// broken config has two scans (per autodiscovery) defined with the same name which will trigger an error during controller setup
-var BrokenConfig = config.AutoDiscoveryConfig{
+// newContainerScanConfigMock creates a mock scan configuration specifically for container auto-discovery
+func newContainerScanConfigMock(name string) config.ScanConfig {
+	return config.ScanConfig{
+		Name:           name,
+		RepeatInterval: metav1.Duration{Duration: time.Hour},
+		Annotations:    map[string]string{"testAnnotation": "{{ .Namespace.Name }}"},
+		Labels:         map[string]string{"testLabel": "{{ .Namespace.Name }}"},
+		Parameters:     []string{"-p", "{{ .Namespace.Name }}"},
+		ScanType:       "nmap",
+		HookSelector: metav1.LabelSelector{
+			MatchExpressions: []metav1.LabelSelectorRequirement{
+				{
+					Operator: metav1.LabelSelectorOpIn,
+					Key:      "foo",
+					Values:   []string{"bar", "baz"},
+				},
+				{
+					Operator: metav1.LabelSelectorOpDoesNotExist,
+					Key:      "foo",
+				},
+			},
+		},
+	}
+}
+
+// AutoDiscoveryConfigMock holds the complete mock configuration
+var AutoDiscoveryConfigMock = config.AutoDiscoveryConfig{
 	Cluster: config.ClusterConfig{
 		Name: "test-cluster",
 	},
 	ServiceAutoDiscovery: config.ServiceAutoDiscoveryConfig{
 		PassiveReconcileInterval: metav1.Duration{Duration: 1 * time.Second},
 		ScanConfigs: []config.ScanConfig{
-			{
-				Name:           "test-scan",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{},
-				Labels:         map[string]string{},
-				Parameters:     []string{"-p", "{{ .Host.Port }}", "{{ .Service.Name }}.{{ .Service.Namespace }}.svc"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"foo": "bar",
-					},
-				},
-			},
-			{
-				Name:           "test-scan",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{},
-				Labels:         map[string]string{},
-				Parameters:     []string{"-p", "{{ .Host.Port }}", "{{ .Service.Name }}.{{ .Service.Namespace }}.svc"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"foo": "bar",
-					},
-				},
-			},
+			newServiceScanConfigMock("test-scan-0"),
+			newServiceScanConfigMock("test-scan-1"),
 		},
 	},
 	ContainerAutoDiscovery: config.ContainerAutoDiscoveryConfig{
 		PassiveReconcileInterval: metav1.Duration{Duration: 1 * time.Second},
+		ImagePullSecretConfig: config.ImagePullSecretConfig{
+			MapImagePullSecretsToEnvironmentVariables: true,
+			UsernameEnvironmentVariableName:           "username",
+			PasswordNameEnvironmentVariableName:       "password",
+		},
 		ScanConfigs: []config.ScanConfig{
-			{
-				Name:           "test-scan",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{"testAnnotation": "{{ .Namespace.Name }}"},
-				Labels:         map[string]string{"testLabel": "{{ .Namespace.Name }}"},
-				Parameters:     []string{"-p", "{{ .Namespace.Name }}"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchExpressions: []metav1.LabelSelectorRequirement{
-						{
-							Operator: metav1.LabelSelectorOpIn,
-							Key:      "foo",
-							Values:   []string{"bar", "baz"},
-						},
-						{
-							Operator: metav1.LabelSelectorOpDoesNotExist,
-							Key:      "foo",
-						},
-					},
-				},
-			},
-			{
-				Name:           "test-scan",
-				RepeatInterval: metav1.Duration{Duration: time.Hour},
-				Annotations:    map[string]string{"testAnnotation": "{{ .Namespace.Name }}"},
-				Labels:         map[string]string{"testLabel": "{{ .Namespace.Name }}"},
-				Parameters:     []string{"-p", "{{ .Namespace.Name }}"},
-				ScanType:       "nmap",
-				HookSelector: metav1.LabelSelector{
-					MatchExpressions: []metav1.LabelSelectorRequirement{
-						{
-							Operator: metav1.LabelSelectorOpIn,
-							Key:      "foo",
-							Values:   []string{"bar", "baz"},
-						},
-						{
-							Operator: metav1.LabelSelectorOpDoesNotExist,
-							Key:      "foo",
-						},
-					},
-				},
-			},
+			newContainerScanConfigMock("test-scan"),
+			newContainerScanConfigMock("test-scan-two"),
 		},
 	},
 	ResourceInclusion: config.ResourceInclusionConfig{
