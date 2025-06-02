e26d85ced2e676b397267d655a42e55437d99e05
Samreet Singh <samreet.singh@iteratec.com>
2025-06-02 15:14:04 +0200

    Remove ZAP-baseline and ZAP-advanced Scanner in favor of the ZAP-automation-framework
    - Deleted scanner directories
    - Removed scanner from CI, release-build and scb-bot
    
    Signed-off-by: Samreet Singh <samreet.singh@iteratec.com>

--- scanners/zap-advanced/scanner/zapclient/configuration/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: the secureCodeBox authors
-#
-# SPDX-License-Identifier: Apache-2.0
-
-__all__ = ["zap_configuration_context_users"]
-
-from .zap_configuration_context_users import ZapConfigurationContextUsers
