c2b038f94d11aad57b1edaeeb56454b548e555b0
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-06-16 19:49:46 +0200

    Migrate nmap integration test to esm
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@owasp.org>

--- scanners/nmap/integration-tests/nmap.test.js
+++ scanners/nmap/integration-tests/nmap.test.js
@@ -2,18 +2,16 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-const { scan } = require("../../../tests/integration/helpers.js");
-
-jest.retryTimes(3);
+import { scan } from "../../../tests/integration/helpers.js";
 
 test(
   "localhost port scan should only find a host finding",
   async () => {
-    const {categories, severities, count} = await scan(
+    const { categories, severities, count } = await scan(
       "nmap-localhost",
       "nmap",
       ["localhost"],
-      90
+      90,
     );
 
     expect(count).toBe(1);
@@ -28,17 +26,23 @@ test(
       }
     `);
   },
-  3 * 60 * 1000
+  {
+    timeout: 3 * 60 * 1000,
+    retry: 3,
+  },
 );
 
 test(
   "invalid port scan should be marked as errored",
   async () => {
     await expect(
-      scan("nmap-localhost", "nmap", ["-invalidFlag", "localhost"], 90)
+      scan("nmap-localhost", "nmap", ["-invalidFlag", "localhost"], 90),
     ).rejects.toThrow(
-      'Scan failed with description "Failed to run the Scan Container, check k8s Job and its logs for more details"'
+      'Scan failed with description "Failed to run the Scan Container, check k8s Job and its logs for more details"',
     );
   },
-  3 * 60 * 1000
+  {
+    timeout: 3 * 60 * 1000,
+    retry: 3,
+  },
 );
