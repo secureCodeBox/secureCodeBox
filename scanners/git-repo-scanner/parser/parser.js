a39cc5c8776cee0c4bfde64301dd90645f9dffff
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-06-22 18:12:10 +0200

    Migrate git-repo-scanner to esm
    Signed-off-by: Jannik Hollenbach <jannik.hollenbach@owasp.org>

--- scanners/git-repo-scanner/parser/parser.js
+++ scanners/git-repo-scanner/parser/parser.js
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-async function parse(fileContent) {
+export async function parse(fileContent) {
   return fileContent;
 }
-
-module.exports.parse = parse;
