4fd127e1b88966aff117cee0dceaf457d8a8791e
Joel Saß <joel.sass@iteratec.com>
2025-07-18 14:49:44 +0200

    Change to Taskfile setup and refactored JS to ESM
    Signed-off-by: Joel Saß <joel.sass@iteratec.com>

--- scanners/subfinder/parser/parser.test.js
+++ scanners/subfinder/parser/parser.test.js
@@ -2,15 +2,9 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-const { readFile } = require("fs/promises");
-const util = require("util");
-
-const {
-  validateParser,
-} = require("@securecodebox/parser-sdk-nodejs/parser-utils");
-
-const { parse } = require("./parser");
-const exp = require("node:constants");
+import { readFile } from "node:fs/promises";
+import { validateParser } from "@securecodebox/parser-sdk-nodejs/parser-utils";
+import { parse } from "./parser";
 
 test("should properly parse subfinder json file without ip output", async () => {
   const fileContent = await readFile(
