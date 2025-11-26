f7c9dbbdcf33bf3835118b651c0a62ca4f4e4201
Patrick <patrick.weiss@iteratec.com>
2025-11-26 10:20:41 +0100

    Rewrite git-repo-scanner in go
    Signed-off-by: Patrick <patrick.weiss@iteratec.com>

--- scanners/git-repo-scanner/integration-tests/git-repo-scanner.test.js
+++ scanners/git-repo-scanner/integration-tests/git-repo-scanner.test.js
@@ -8,12 +8,11 @@ test.skip(
   "git-repo-scanner should find at least 1 repository in the GitHub secureCodeBox organisation",
   async () => {
     // This integration tests runs about 30min because of the GitHub Public API call rate limit.
-    // If you want to speed up you need to add an valid access token like: ['--git-type', 'github', '--organization', 'secureCodeBox', '--access-token', '23476VALID2345TOKEN'],
+    // If you want to speed up you need to add an valid access token like: ['--git-type', 'GitHub', '--organization', 'secureCodeBox', '--access-token', '23476VALID2345TOKEN'],
     const { count } = await scan(
       "git-repo-scanner-dummy-scan",
       "git-repo-scanner",
-      ["--git-type", "github", "--organization", "secureCodeBox"],
-      90,
+      ["--git-type", "GitHub", "--organization", "secureCodeBox"],
     );
     // There must be >= 28 Repositories found in the GitHub secureCodeBox organisation.
     expect(count).toBeGreaterThanOrEqual(28);
