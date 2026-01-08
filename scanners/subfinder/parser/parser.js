63ad48552de750bf4818096ee0b22d8e42ac4426
Patrick <patrick.weiss@iteratec.com>
2026-01-08 10:10:48 +0100

    Add option to include target domain in subfinder findings
    Signed-off-by: Patrick <patrick.weiss@iteratec.com>

--- scanners/subfinder/parser/parser.js
+++ scanners/subfinder/parser/parser.js
@@ -2,32 +2,66 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-export async function parse(fileContent) {
-  if (!fileContent)
-    return [];
+const includeTargetDomain =
+  process.env["INCLUDE_TARGET_DOMAIN"]?.toLowerCase() == "true";
+
+export async function parse(fileContent, scan) {
+  if (!fileContent && !includeTargetDomain) return [];
 
   const targets = parseResultFile(fileContent);
-  return transformToFindings(targets);
+  const findings = transformToFindings(targets);
+
+  const domain = includeTargetDomain
+    ? getArgValue(scan.spec.parameters, "-d", "-domain")
+    : null;
+  if (domain) {
+    findings.push(getTargetDomainFinding(domain));
+  }
+
+  return findings;
+}
+
+function getArgValue(args, ...flags) {
+  const index = args.findIndex((arg) => flags.includes(arg));
+  return index !== -1 && index + 1 < args.length ? args[index + 1] : null;
+}
+
+function getTargetDomainFinding(domain) {
+  return {
+    name: domain,
+    identified_at: null,
+    description: `Found subdomain ${domain}`,
+    category: "Subdomain",
+    location: domain,
+    osi_layer: "NETWORK",
+    severity: "INFORMATIONAL",
+    attributes: {
+      domain: domain,
+      hostname: domain,
+      ip_address: null,
+      ip_addresses: [],
+      source: "parser",
+    },
+  };
 }
 
 function transformToFindings(targets) {
   return targets.map((item) => ({
-      "name": item.host,
-      "identified_at": null,
-      "description": `Found subdomain ${item.host}`,
-      "category": "Subdomain",
-      "location": item.host,
-      "osi_layer": "NETWORK",
-      "severity": "INFORMATIONAL",
-      "attributes": {
-        "domain": item.input,
-        "hostname": item.host,
-        "ip_address": item?.ip || null,
-        "ip_addresses": [item?.ip || null].filter(Boolean),
-        "source": item.source,
-      }
-    }
-  ));
+    name: item.host,
+    identified_at: null,
+    description: `Found subdomain ${item.host}`,
+    category: "Subdomain",
+    location: item.host,
+    osi_layer: "NETWORK",
+    severity: "INFORMATIONAL",
+    attributes: {
+      domain: item.input,
+      hostname: item.host,
+      ip_address: item?.ip || null,
+      ip_addresses: [item?.ip || null].filter(Boolean),
+      source: item.source,
+    },
+  }));
 }
 
 /**
@@ -35,7 +69,8 @@ function transformToFindings(targets) {
  * @param {*} fileContent
  */
 function parseResultFile(fileContent) {
-  return fileContent.trim()
-                    .split('\n')
-                    .map(line => JSON.parse(line));
+  return fileContent
+    .trim()
+    .split("\n")
+    .map((line) => JSON.parse(line));
 }
